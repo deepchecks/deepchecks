2fc13f052293619fb6bcf96ff02b1bfbf62b9f54
Noam Bressler <noam@deepchecks.com>
2021-10-11 11:40:17 +0300

    add dataset obj

--- mlchecks/base.py
+++ mlchecks/base.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import Callable, Dict, List, Any, Tuple
+import pandas as pd
 
 __all__ = ['CheckResult', 'Checkable', 'Check', 'CheckSuite', 'Model', 'Dataset']
 
@@ -82,22 +83,42 @@ class CheckSuite:
                 for check in self.checks]
 
 
+class Dataset(pd.DataFrame):
 
-class Dataset:
-    pass
+    def __init__(self,
+                 df: pd.DataFrame,
+                 features: List[str] = None, cat_features: List[str] = None,
+                 label: str = None, index: str = None, date: str = None,
+                 *args, **kwargs):
 
+        super().__init__(df, *args, **kwargs)
 
-class Model(ABC):
-    model_obj: Any
+        if features:
+            self._features = features
+        else:
+            self._features = [x for x in df.columns if x not in {label, index, date}]
 
-    # @abstractmethod
-    # def fit(self):
-    #     pass
+        self._label = label
+        self._index_name = index
+        self._date_name = date
 
-    # @abstractmethod
-    # def predict(self):
-    #     pass
+        if cat_features:
+            self._cat_features = cat_features
+        else:
+            self._cat_features = self.infer_categorical_features()
 
+    def infer_categorical_features(self) -> List[str]:
+        # TODO: add infer logic here
+        return []
 
-class ScikitModel(Model):
-    pass
+    def features(self) -> List[str]:
+        return self._features
+
+    def index_name(self) -> str:
+        return self._index_name
+
+    def date_name(self) -> str:
+        return self._date_name
+
+    def cat_features(self) -> List[str]:
+        return self._cat_features
