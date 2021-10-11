6b0e1c9fd14b9ca30acd20ce7bcd6460d225af5c
Nir Hutnik <nir@deepchecks.com>
2021-10-11 14:32:50 +0000

    Update suite and checks

--- mlchecks/base/suite.py
+++ mlchecks/base/suite.py
@@ -1,6 +1,7 @@
 from typing import List
-from mlchecks.base.check import BaseCheck
 
+from mlchecks.base.check import BaseCheck, CheckResult, TrainValidationBaseCheck, CompareDatasetsBaseCheck, \
+    SingleDatasetBaseCheck, ModelOnlyBaseCheck
 
 __all__ = ['CheckSuite']
 
@@ -11,13 +12,51 @@ class CheckSuite:
     def __init__(self, *checks):
         for check in checks:
             if not isinstance(check, BaseCheck):
-                raise Exception(f'CheckSuite receives only `Checkable` objects but got: {check.__class__.__name__}')
+                raise Exception(f'CheckSuite receives only `BaseCheck` objects but got: {check.__class__.__name__}')
         self.checks = checks
 
-    def run(self, model=None, train_data=None, validation_data=None):
-        return [check.run(model=model, train_data=train_data, validation_data=validation_data)
-                for check in self.checks]
+    def run(self, model=None, train_dataset=None, validation_dataset=None, check_datasets_policy: str = 'validation') \
+            -> List[CheckResult]:
+        """
+        Args:
+          model: A scikit-learn-compatible fitted estimator instance
+          train_dataset: Dataset object, representing data an estimator was fitted on
+          validation_dataset: Dataset object, representing data an estimator predicts on
+          check_datasets_policy: str, one of either ['both', 'train', 'validation'].
+                                 Determines the policy by which single dataset checks are run when two datasets are
+                                 given, one for train and the other for validation.
 
-    def run_and_decide(self, model=None, train_data=None, validation_data=None):
-        return [check.run_and_decide(model=model, train_data=train_data, validation_data=validation_data)
-                for check in self.checks]
+        Returns:
+          List[CheckResult] - All results by all initialized checks
+
+        Raises:
+             ValueError if check_datasets_policy is not of allowed types
+        """
+        if check_datasets_policy not in ['both', 'train', 'validation']:
+            raise ValueError('check_datasets_policy must be one of ["both", "train", "validation"]')
+
+        results = []
+        for check in self.checks:
+            if isinstance(check, TrainValidationBaseCheck):
+                results.append(check.run(train_dataset=train_dataset, validation_dataset=validation_dataset,
+                                         model=model))
+            elif isinstance(check, CompareDatasetsBaseCheck):
+                results.append(check.run(dataset=train_dataset, compared_dataset=validation_dataset, model=model))
+            elif isinstance(check, SingleDatasetBaseCheck):
+                if check_datasets_policy in ['both', 'train']:
+                    results.append(check.run(dataset=train_dataset))
+                if check_datasets_policy in ['both', 'validation']:
+                    results.append(check.run(dataset=validation_dataset))
+            elif isinstance(check, ModelOnlyBaseCheck):
+                results.append(check.run(model=model))
+            else:
+                raise TypeError(f'Expected check of type SingleDatasetBaseCheck, CompareDatasetsBaseCheck, '
+                                f'TrainValidationBaseCheck or ModelOnlyBaseCheck. Got  {check.__class__.__name__} '
+                                f'instead')
+
+        return results
+
+    def decide(self, model=None, train_dataset=None, validation_dataset=None, check_datasets_policy: str = 'validation'
+               ) -> List[CheckResult]:
+        #TODO: Complete
+        pass
