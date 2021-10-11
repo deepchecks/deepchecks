413e5972a3d65c63b6fb0d00cc348ce6a681e414
Matan Perlmutter <matan@deepchecks.com>
2021-10-11 16:59:47 +0300

    Add docstring

--- mlchecks/checks/overview/model_info.py
+++ mlchecks/checks/overview/model_info.py
@@ -4,6 +4,14 @@ from sklearn.base import BaseEstimator
 
 
 def model_info(model: BaseEstimator):
+    """Summarize given model parameters
+
+    Args:
+        model (BaseEstimator): A scikit-learn-compatible fitted estimator instance
+
+    Returns:
+        CheckResult: value is dictionary in format {type: <model_type>, params: <model_params_dict>}
+    """
     _type = type(model).__name__
     model_param_df = pd.DataFrame.from_dict(model.get_params(), orient='index', columns=['value'])
     model_param_df.index.name = 'parameter'
@@ -16,6 +24,9 @@ def model_info(model: BaseEstimator):
 
 
 class ModelInfo(ModelOnlyBaseCheck):
+    """
+    Class that wraps the usage of `model_info` to be used in `Suite`
+    """
     def run(self, model: BaseEstimator) -> CheckResult:
         return model_info(model)
 
