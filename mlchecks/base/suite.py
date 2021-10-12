3fab9ef430cd4be99df74c7b9408abc80e3b00c4
Itay Gabbay <itay@deepchecks.com>
2021-10-12 12:02:19 +0300

    Support of deciders, and multi-suite suites

--- mlchecks/base/suite.py
+++ mlchecks/base/suite.py
@@ -1,22 +1,28 @@
-from typing import List
+from collections import defaultdict
+from typing import List, Dict, Union
 
 from mlchecks.base.check import BaseCheck, CheckResult, TrainValidationBaseCheck, CompareDatasetsBaseCheck, \
-    SingleDatasetBaseCheck, ModelOnlyBaseCheck
+    SingleDatasetBaseCheck, ModelOnlyBaseCheck, Decidable
 
 __all__ = ['CheckSuite']
 
+from mlchecks.utils import MLChecksValueError
 
-class CheckSuite:
+
+class CheckSuite(BaseCheck):
     checks: List[BaseCheck]
+    name: str
 
-    def __init__(self, *checks):
+    def __init__(self, name, *checks, **params):
+        super().__init__()
+        self.name = name
         for check in checks:
             if not isinstance(check, BaseCheck):
                 raise Exception(f'CheckSuite receives only `BaseCheck` objects but got: {check.__class__.__name__}')
         self.checks = checks
 
-    def run(self, model=None, train_dataset=None, validation_dataset=None, check_datasets_policy: str = 'validation') \
-            -> List[CheckResult]:
+    def _run(self, model, train_dataset, validation_dataset, check_datasets_policy, decide=False) \
+            -> Dict[str, Union[Dict, List[Union[CheckResult, bool]]]]:
         """
         Args:
           model: A scikit-learn-compatible fitted estimator instance
@@ -35,28 +41,61 @@ class CheckSuite:
         if check_datasets_policy not in ['both', 'train', 'validation']:
             raise ValueError('check_datasets_policy must be one of ["both", "train", "validation"]')
 
-        results = []
+        results: Dict[str, Union[Dict, List[Union[CheckResult, bool]]]] = defaultdict(list)
         for check in self.checks:
+            check_results = []
             if isinstance(check, TrainValidationBaseCheck):
-                results.append(check.run(train_dataset=train_dataset, validation_dataset=validation_dataset,
-                                         model=model))
+                check_results.append(check.run(train_dataset=train_dataset, validation_dataset=validation_dataset,
+                                                    model=model))
             elif isinstance(check, CompareDatasetsBaseCheck):
-                results.append(check.run(dataset=train_dataset, compared_dataset=validation_dataset, model=model))
+                check_results.append(check.run(dataset=train_dataset, compared_dataset=validation_dataset,
+                                                    model=model))
             elif isinstance(check, SingleDatasetBaseCheck):
                 if check_datasets_policy in ['both', 'train']:
-                    results.append(check.run(dataset=train_dataset))
+                    check_results.append(check.run(dataset=train_dataset))
                 if check_datasets_policy in ['both', 'validation']:
-                    results.append(check.run(dataset=validation_dataset))
+                    check_results.append(check.run(dataset=validation_dataset))
             elif isinstance(check, ModelOnlyBaseCheck):
-                results.append(check.run(model=model))
+                check_results.append(check.run(model=model))
+            elif isinstance(check, CheckSuite):
+                suite_res = check._run(model, train_dataset, validation_dataset, check_datasets_policy, decide)
+                if check.name in results:
+                    raise MLChecksValueError("Each suite must have a unique name")
+                results[self.name].append(suite_res)
             else:
                 raise TypeError(f'Expected check of type SingleDatasetBaseCheck, CompareDatasetsBaseCheck, '
                                 f'TrainValidationBaseCheck or ModelOnlyBaseCheck. Got  {check.__class__.__name__} '
                                 f'instead')
 
+            if len(check_results) > 0:
+                if not decide:
+                    results[self.name].extend(check_results)
+                else:
+                    for res in check_results:
+                        if isinstance(check, Decidable):
+                            check: Decidable
+                            results[self.name].append(check.decide(res))
+
         return results
 
-    def decide(self, model=None, train_dataset=None, validation_dataset=None, check_datasets_policy: str = 'validation'
-               ) -> List[CheckResult]:
-        #TODO: Complete
-        pass
+    def run(self, model=None, train_dataset=None, validation_dataset=None, check_datasets_policy: str = 'validation') \
+            -> Dict[str, Union[Dict, List[Union[CheckResult, bool]]]]:
+        """
+        Running the suites without validating the deciders.
+        """
+        return self._run(model, train_dataset, validation_dataset, check_datasets_policy, decide=False)
+
+    def run_and_decide(self, model=None, train_dataset=None, validation_dataset=None,
+                       check_datasets_policy: str = 'validation') \
+            -> Dict[str, Union[Dict, List[Union[CheckResult, bool]]]]:
+
+        return self._run(model, train_dataset, validation_dataset, check_datasets_policy, decide=True)
+
+
+
+
+# [dataset_info, model_info, Suite("itay", mode_info)]
+# {
+#     "aaa" [sgdfg],
+#
+# }
