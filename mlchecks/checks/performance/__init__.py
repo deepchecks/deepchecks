d2e61ef84e07b61ac2a8c6a78e66eea07b52f3a4
Jonthan Liberman <jonthanliberman@ip-192-168-1-3.eu-west-1.compute.internal>
2021-10-13 11:10:44 +0300

    classification_report

--- mlchecks/checks/performance/__init__.py
+++ mlchecks/checks/performance/__init__.py
@@ -1,4 +1,4 @@
-"""Module contains check of overall classification metrics"""
+"""Module contains check of overall classification metrics."""
 from .classification_report import *
 from .confusion_matrix_report import *
 from .roc_report import *
