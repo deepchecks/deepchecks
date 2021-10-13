fcf4e4dc2fc0fd3b4cd2009da6b8c5c74dad0502
DBI <danb@deepchecks.com>
2021-10-13 16:34:35 +0300

    add tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,11 +15,10 @@ def iris():
     df = load_iris(return_X_y=False, as_frame=True)
     return pd.concat([df.data, df.target], axis=1)
 
-
 @pytest.fixture(scope='session')
 def iris_dataset(iris):
     """Return Iris dataset as Dataset object."""
-    return Dataset(iris)
+    return Dataset(iris, label='target')
 
 
 @pytest.fixture(scope='session')
@@ -62,5 +61,6 @@ def iris_dataset_single_class(iris):
     """Return Iris dataset modified to a binary label as Dataset object."""
     idx = iris.target != 2
     df = iris[idx]
-    dataset = Dataset(df)
+    dataset = Dataset(df, label="target")
     return dataset
+
