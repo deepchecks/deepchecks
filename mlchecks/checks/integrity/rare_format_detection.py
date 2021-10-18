56297616ba6936c9befbda6b5a059ceb5c2aa0be
nirchecks <nir@deepchecks.com>
2021-10-18 19:53:08 +0300

    added functionality - refine detection

--- mlchecks/checks/integrity/rare_format_detection.py
+++ mlchecks/checks/integrity/rare_format_detection.py
@@ -1,56 +1,66 @@
 """The single_feature_contribution check module."""
 import re
-from copy import deepcopy, copy
-from typing import Union, List
+from copy import deepcopy
+from typing import Union, List, Tuple
 
 import pandas as pd
 
 from mlchecks import CheckResult, Dataset, SingleDatasetBaseCheck
 from mlchecks.base.dataset import validate_dataset_or_dataframe
-from mlchecks.display import format_check_display
 
-__all__ = ['rare_format_detection', 'RareFormatDetection', 'SubStr', 'Pattern']
+# from mlchecks.display import format_check_display
 
+__all__ = ['rare_format_detection', 'RareFormatDetection', 'Pattern']
 
-class SubStr:
-    """Supporting class for regex subbing in pattern."""
-
-    def __init__(self, regex_str: str, filler: str):
-        """Initiate the SubStr class."""
-        self.regex_str = regex_str
-        self.filler = filler
-
-    def sub(self, s: str) -> str:
-        """Replace matching patterns to the regex_str by the filler."""
-        return re.sub(self.regex_str, self.filler, s)
+from mlchecks.checks.integrity.string_utils import split_and_keep, split_and_keep_by_many
 
 
 class Pattern:
     """Supporting class for creating complicated patterns for rare_format_detection."""
 
-    def __init__(self, name: str, substrs: List[SubStr]):
+    def __init__(self, name: str, substituters: Union[List[Tuple[str, str]], Tuple[str, str]], ignore: str = None,
+                 refine: bool = False, is_sequence: bool = False):
         """Initiate the Pattern class."""
         self.name = name
-        self.substrs = substrs
+        if isinstance(substituters, tuple):
+            substituters = [substituters]
+        self.substituters = substituters  # tuple or list of tuples of regex and str
+        self.ignore = ignore  # regex
+        self.refine = refine
+        self.is_sequence = is_sequence
 
     def sub(self, s: str) -> str:
         """Replace matching patterns to the regex_str by the filler."""
-        for subs in self.substrs:
-            s = subs.sub(s)
+        s = self.clean(s)
+        for subs in self.substituters:
+            s = re.sub(subs[0], subs[1], s)
         return s
+
+    def clean(self, s: str) -> str:
+        """Remove all substrings that should be ignored."""
+        if self.ignore is None:
+            return s
+        return re.sub(self.ignore, '', s)
+
+    def is_format_significant(self, fmt) -> bool:
+        """Return boolean indicating whether format includes any filler."""
+        return any(sub[1] in fmt for sub in self.substituters)
 
 
 DEFAULT_PATTERNS = [
-    Pattern('digits only format (ignoring letters)', [SubStr(r'\d', '0'), SubStr(r'[A-Z|a-z]', '')]),
-    Pattern('sequences of digits only format (ignoring letters)', [SubStr(r'\d', 'DIGIT_SEQ'),
-                                                                   SubStr(r'[A-Z|a-z]', '')]),
-    Pattern('letters only format (ignoring digits)', [SubStr(r'\d', ''), SubStr(r'[A-Z|a-z]', '')]),
-    Pattern('sequences of letters only format (ignoring letters)', [SubStr(r'\d', ''),
-                                                                    SubStr(r'[A-Z|a-z]+', 'LETTER_SEQ')]),
-    Pattern('digits and letters format', [SubStr(r'\d', '0'), SubStr(r'[A-Z|a-z]', 'X')]),
-    Pattern('digits and letters format (case sensitive)', [SubStr(r'\d', '0'), SubStr(r'[A-Z]', 'X'),
-                                                           SubStr(r'[a-z]', 'x')]),
-    Pattern('any sequence format', [SubStr(r'\d', '0'), SubStr(r'[A-Z|a-z|d]+', 'SEQ')]),
+    Pattern(name='digits only format (ignoring letters)', substituters=(r'\d', '0'), ignore=r'[A-Z|a-z]',
+            refine=True),
+    Pattern(name='sequences of digits only format (ignoring letters)', substituters=(r'\d+', '000'),
+            ignore=r'[A-Z|a-z]', refine=True, is_sequence=True),
+    Pattern(name='letters only format (ignoring digits)', substituters=(r'[A-Z|a-z]', 'X'), ignore=r'\d',
+            refine=True),
+    Pattern(name='sequences of letters only format (ignoring letters)', ignore=r'\d',
+            substituters=(r'[A-Z|a-z]+', 'XXX'), refine=True, is_sequence=True),
+    Pattern(name='digits and letters format', substituters=[(r'\d', '0'), (r'[A-Z|a-z]', 'X')]),
+    Pattern(name='digits and letters format (case sensitive)',
+            substituters=[(r'\d', '0'), (r'[A-Z]', 'X'), (r'[a-z]', 'x')]),
+    Pattern(name='digits or letters format', substituters=(r'[A-Z|a-z|d]', 'X'), refine=True, is_sequence=True),
+    Pattern(name='any sequence format', substituters=(r'[A-Z|a-z|d]+', 'XXX'), refine=True, is_sequence=True),
 ]
 
 
@@ -80,13 +90,12 @@ def rare_format_detection(dataset: Union[Dataset, pd.DataFrame], column_names: U
     res = {column_name: _detect_per_column(dataset[column_name], patterns, rarity_threshold)
            for column_name in column_names}
 
-    html = ''
+    display = []
     for key, value in res.items():
-        if value is not None and not value.empty:
-            html += f'<br>Column {key}:<br><br>{value.to_html()}<br>'
+        display.append(f'\n\nColumn {key}:')
+        display.append(value)
 
-    return CheckResult(value=res, display={'text/html': format_check_display('Rare Format Detection',
-                                                                             rare_format_detection, html)})
+    return CheckResult(value=res, header='Rare Format Detection', check=rare_format_detection, display=display)
 
 
 def _detect_per_column(column: pd.Series, patterns, rarity_threshold):
@@ -127,20 +136,74 @@ def _detect_per_column_and_pattern(column, pattern, rarity_threshold):
 
     # find rare formats by replacing every pattern with the filler.
     # For example if the pattern is \d+ and column contains dates, the formats that will be found is _._._
-    patterned_column = column.apply(lambda s: pattern.sub(s))
+    patterned_column = column.apply(pattern.sub)
 
     rare_to_common_format_ratio, rare_formats, common_formats = get_rare_vs_common_values(patterned_column,
                                                                                           rarity_threshold)
-
     if rare_to_common_format_ratio == 0: return {}
+    if not any(pattern.is_format_significant(common_format) for common_format in common_formats): return {}
 
     rare_values = column[patterned_column.isin(rare_formats)]
-    common_values_examples = [column[patterned_column == common_format].values[1] for common_format in common_formats]
+    common_values_examples = [column[patterned_column == common_format].values[0] for common_format in common_formats]
+
+    if pattern.refine is True:
+        for i, fmt in enumerate(common_formats):
+            format_samples = column[patterned_column == fmt].apply(pattern.clean).values
+            common_formats[i] = _refine_formats(fmt=fmt, substr=pattern.substituters[0][1], samples=format_samples,
+                                                is_substr_sequence=pattern.is_sequence)
+            #TODO: using pattern.substituters[0][1] right now. should be all fillers - substr should get a list
 
     return {'ratio of rare patterns to common patterns': f'{rare_to_common_format_ratio:.2%}',
             'common formats': common_formats,
             'examples for values in common formats': common_values_examples,
             'values in rare formats': list(rare_values.unique())}
+
+
+def _refine_formats(fmt: str, substr: str, samples: List[str], is_substr_sequence: bool = False) -> str:
+    """
+    Return a refined (degeneralized) pattern, based on known samples.
+
+    Example: format "XXX@XXX.XXX" was found. However, it appears that all samples of this format are in gmail.com
+    domain. This function detects that and returns a degeneralized format - "XXX@gmail.com"
+
+    Args:
+        fmt (str): string representing the format
+        substr (str): original substr of the format that replaced other characters when detecting the pattern
+        samples (List[str]): list of all known samples of current format
+        is_substr_sequence (bool): boolean representing whether the substr replaced each character (e.g. '/d') or
+                                   a whole sequence (e.g. '/d+')
+
+    Returns:
+        str: degeneralized format, based on the fmt input
+    """
+    if substr not in fmt:
+        return fmt
+
+    if is_substr_sequence is True:
+        splt_fmt = split_and_keep(s=fmt, separator=substr)
+        splt_fmt_wo_sep = list(filter(lambda x: x != substr, splt_fmt))
+        split_examples = [split_and_keep_by_many(s, splt_fmt_wo_sep) for s in samples]
+    else:
+        splt_fmt = list(fmt)
+        split_examples = [list(s) for s in samples]
+
+    new_format = []
+    example = split_examples[0]
+    for i in range(len(example)):
+        if splt_fmt[i] != substr:
+            new_format.append(splt_fmt[i])
+            continue
+
+        common_value = example[i]
+        # If separator represents the same string in all examples, use that instead of a separator:
+        if all(split_sample[i] == common_value for split_sample in split_examples[1:]):
+            new_format.append(common_value)
+        else:
+            new_format.append(splt_fmt[i])
+
+    new_format = ''.join(new_format)
+
+    return new_format
 
 
 def get_rare_vs_common_values(col: pd.Series, sharp_drop_ratio_threshold: float = 0.05):
