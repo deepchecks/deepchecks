0d710f728ab21ae4770bb993e02c7e22aa0b78e9
Jonthan Liberman <jonthanliberman@Jonthans-MacBook-Pro.local>
2021-10-25 17:27:12 +0300

    v1

--- mlchecks/checks/integrity/dominant_frequency_change.py
+++ mlchecks/checks/integrity/dominant_frequency_change.py
@@ -6,7 +6,7 @@ import numpy as np
 import pandas as pd
 
 from mlchecks import Dataset
-from mlchecks.base.check import CheckResult, TrainValidationBaseCheck
+from mlchecks.base.check import CheckResult, CompareDatasetsBaseCheck
 
 __all__ = ['dominant_frequency_change', 'DominantFrequencyChange']
 
@@ -52,12 +52,12 @@ def find_p_val(key: str, ref_hist: Dict, test_hist: Dict, ref_count: int, test_c
     return p_val
 
 
-def dominant_frequency_change(validation_dataset: Dataset, train_dataset: Dataset, p_val_thres: float = 0.0001, dominance_ratio: float = 2,  ratio_change_thres: float = 1.5):
+def dominant_frequency_change(dataset: Dataset, baseline_dataset: Dataset, p_val_thres: float = 0.0001, dominance_ratio: float = 2,  ratio_change_thres: float = 1.5):
     """Detect values highly represented in the tested and reference data and checks if their relative and absolute percentage have increased significantly
 
     Args:
-        train_dataset (Dataset): The training dataset object. Must contain an index.
-        validation_dataset (Dataset): The validation dataset object. Must contain an index.
+        dataset (Dataset): The dataset object. Must contain an index.
+        baseline_dataset (Dataset): The baseline dataset object. Must contain an index.
         p_val_thres (float = 0.0001): Maximal p-value to pass the statistical test determining if the value abundance has changed significantly (0-1).
         dominance_ratio (float = 2): Next most abundance value has to be THIS times less than the first (0-inf).
         ratio_change_thres (float = 1.5): The dominant frequency has to change by at least this ratio (0-inf).
@@ -68,41 +68,41 @@ def dominant_frequency_change(validation_dataset: Dataset, train_dataset: Datase
         MLChecksValueError: If the object is not a Dataset or DataFrame instance
 
     """
-    validation_dataset = Dataset.validate_dataset_or_dataframe(validation_dataset)
-    train_dataset = Dataset.validate_dataset_or_dataframe(train_dataset)
-    validation_dataset.validate_shared_features(train_dataset, dominant_frequency_change.__name__)
+    baseline_dataset = Dataset.validate_dataset_or_dataframe(baseline_dataset)
+    dataset = Dataset.validate_dataset_or_dataframe(dataset)
+    dataset.validate_shared_features(baseline_dataset, dominant_frequency_change.__name__)
 
-    columns = train_dataset.features()
+    columns = baseline_dataset.features()
 
-    train_f = train_dataset.data
-    val_f = validation_dataset.data
+    test_df = dataset.data
+    ref_df = baseline_dataset.data
 
-    val_len = len(val_f)
-    train_len = len(train_f)
+    ref_len = len(ref_df)
+    test_len = len(test_df)
     p_df = {}
 
     for column in columns:
-        top_val = val_f[column].value_counts()
-        top_train = train_f[column].value_counts()
+        top_ref = ref_df[column].value_counts()
+        top_test = test_df[column].value_counts()
         
-        if(top_val.iloc[0] > top_val.iloc[1] * dominance_ratio):
-            p_val = find_p_val(top_val.index[0], top_train, top_val, train_len, val_len, ratio_change_thres)
+        if(top_ref.iloc[0] > top_ref.iloc[1] * dominance_ratio):
+            p_val = find_p_val(top_ref.index[0], top_test, top_ref, test_len, ref_len, ratio_change_thres)
             if p_val < p_val_thres:
-                p_df[column] = {'value': top_val.index[0], 'p value': p_val}
-        elif(top_train.iloc[0] > top_train.iloc[1] * dominance_ratio):
-            p_val = find_p_val(top_val.index[0], top_train, top_val, train_len, val_len, ratio_change_thres)
+                p_df[column] = {'value': top_ref.index[0], 'p value': p_val}
+        elif(top_test.iloc[0] > top_test.iloc[1] * dominance_ratio):
+            p_val = find_p_val(top_test.index[0], top_test, top_ref, test_len, ref_len, ratio_change_thres)
             if p_val < p_val_thres:
-                p_df[column] = {'value': top_train.index[0], 'p value': p_val}
+                p_df[column] = {'value': top_test.index[0], 'p value': p_val}
 
     p_df = pd.DataFrame.from_dict(p_df, orient='index')
     
     return CheckResult(p_df, header='Data Sample Leakage Report',
                        check=dominant_frequency_change, display=p_df)
 
-class DominantFrequencyChange(TrainValidationBaseCheck):
+class DominantFrequencyChange(CompareDatasetsBaseCheck):
     """Finds dominant frequency change."""
 
-    def run(self, validation_dataset: Dataset, train_dataset: Dataset) -> CheckResult:
+    def run(self, dataset, baseline_dataset) -> CheckResult:
         """Run dominant_frequency_change_report check.
 
         Args:
@@ -115,4 +115,4 @@ class DominantFrequencyChange(TrainValidationBaseCheck):
             CheckResult: Detects values highly represented in the tested and reference data and checks if their
                          relative and absolute percentage have increased significantly and makes a report in a dataframe.
         """
-        return dominant_frequency_change(validation_dataset=validation_dataset, train_dataset=train_dataset, **self.params)
+        return dominant_frequency_change(dataset=dataset, baseline_dataset=baseline_dataset, **self.params)
