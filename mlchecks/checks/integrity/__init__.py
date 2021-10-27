7b8357c6d37c87b17c847a2604a12b34c82aa600
Noam Bressler <noamzbr@gmail.com>
2021-10-27 11:42:54 +0300

    Mlc 92 testing nb (#78)
    Myriad fixes for various checks, and adding the first version of the extensive testing notebook

--- mlchecks/checks/integrity/__init__.py
+++ mlchecks/checks/integrity/__init__.py
@@ -6,5 +6,6 @@ from .is_single_value import *
 from .special_chars import *
 from .string_mismatch_comparison import *
 from .rare_format_detection import *
+from .data_duplicates import *
 from .new_category import *
 from .new_label import *
--- mlchecks/suites/integrity_suite.py
+++ mlchecks/suites/integrity_suite.py
@@ -1,13 +1,38 @@
 """The predefined Integrity suite module."""
-
 from mlchecks import CheckSuite
-from mlchecks.checks import IsSingleValue, MixedNulls, MixedTypes, StringMismatch
+from mlchecks.checks.integrity import (
+    IsSingleValue,
+    MixedNulls,
+    MixedTypes,
+    StringMismatch,
+    DataDuplicates,
+    SpecialCharacters,
+    StringMismatchComparison,
+    CategoryMismatchTrainValidation
+)
 
-__all__ = ['IntegrityCheckSuite']
+__all__ = ['SingleDatasetIntegrityCheckSuite', 'ComparativeIntegrityCheckSuite', 'IntegrityCheckSuite']
 
-IntegrityCheckSuite = CheckSuite('Integrity Suite',
+SingleDatasetIntegrityCheckSuite = CheckSuite(
+    'Single Dataset Integrity Suite',
     IsSingleValue(),
     MixedNulls(),
     MixedTypes(),
-    StringMismatch()
+    StringMismatch(),
+    DataDuplicates(),
+    # RareFormatDetection(),  # Commented out until fixed
+    SpecialCharacters()
+)
+
+
+ComparativeIntegrityCheckSuite = CheckSuite(
+    'Comparative Integrity Suite',
+    StringMismatchComparison(),
+    CategoryMismatchTrainValidation()
+)
+
+IntegrityCheckSuite = CheckSuite(
+    'Integrity Suite',
+    SingleDatasetIntegrityCheckSuite,
+    ComparativeIntegrityCheckSuite
 )
