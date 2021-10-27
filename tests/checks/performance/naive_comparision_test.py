9c27d16e0f9d735b851fa439a76f954f77c00081
Jonthan Liberman <jonthanliberman@ip-192-168-1-16.eu-west-1.compute.internal>
2021-10-27 13:57:23 +0300

    v0

---
+++ tests/checks/performance/naive_comparision_test.py
@@ -0,0 +1,40 @@
+"""Contains unit tests for the confusion_matrix_report check."""
+from mlchecks.checks.performance import naive_comparision, NaiveComparision
+from mlchecks.utils import MLChecksValueError
+from hamcrest import assert_that, calling, raises, equal_to
+
+
+def test_dataset_wrong_input():
+    bad_dataset = 'wrong_input'
+    # Act & Assert
+    assert_that(calling(naive_comparision).with_args(bad_dataset, bad_dataset, None),
+                raises(MLChecksValueError,
+                       'function naive_comparision requires dataset to be of type Dataset. instead got: str'))
+
+
+def test_0(iris_split_dataset_and_model):
+    train_ds, val_ds, clf = iris_split_dataset_and_model
+    # Arrange
+    check = NaiveComparision()
+    # Act X
+    result = check.run(train_ds, val_ds, clf).value
+    # Assert
+    assert_that(result, equal_to(0))
+
+def test_1(iris_split_dataset_and_model):
+    train_ds, val_ds, clf = iris_split_dataset_and_model
+    # Arrange
+    check = NaiveComparision(native_model_order=1)
+    # Act X
+    result = check.run(train_ds, val_ds, clf).value
+    # Assert
+    assert_that(result, equal_to(0))
+
+def test_2(iris_split_dataset_and_model):
+    train_ds, val_ds, clf = iris_split_dataset_and_model
+    # Arrange
+    check = NaiveComparision(native_model_order=2)
+    # Act X
+    result = check.run(train_ds, val_ds, clf).value
+    # Assert
+    assert_that(result, equal_to(0))
