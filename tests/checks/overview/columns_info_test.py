b7bd5d5545772b202f931588beb10e2223b1e80f
Jonthan Liberman <jonthanliberman@ip-192-168-1-143.eu-west-1.compute.internal>
2021-10-31 17:44:04 +0200

    v1

---
+++ tests/checks/overview/columns_info_test.py
@@ -0,0 +1,35 @@
+"""Contains unit tests for the dataset_info check."""
+from mlchecks.base import Dataset
+from mlchecks.checks.overview import ColumnsInfo, columns_info
+from mlchecks.utils import MLChecksValueError
+
+import numpy as np
+import pandas as pd
+from hamcrest import assert_that, equal_to, calling, raises
+
+
+def test_dataset_wrong_input():
+    x = 'wrong_input'
+    # Act & Assert
+    assert_that(calling(columns_info).with_args(x),
+                raises(MLChecksValueError, 'dataset must be of type DataFrame or Dataset. instead got: str'))
+
+
+def test_dataset_info(iris_dataset):
+    num_fe = np.random.rand(200)
+    cat_fe = np.random.randint(10, size=200)
+    date = range(1635693229, 1635693429)
+    index = range(200)
+    data = {'index': index, 'date': date, 'a': cat_fe, 'b': num_fe, 'c': num_fe, 'label': cat_fe}
+    df = pd.DataFrame.from_dict(data)
+
+    dataset = Dataset(df, label='label', date='date', index='index')
+    # Arrange
+    check = ColumnsInfo()
+    # Act
+    result_ds, result_df = check.run(dataset).value, check.run(df).value,
+    # Assert
+    expected_res = {'index': 'index', 'date': 'date', 'a': 'categorical feature',
+                    'b': 'numerical feature', 'c': 'numerical feature', 'label': 'label'}
+    assert_that(result_ds, expected_res)
+    assert_that(result_df, expected_res)
