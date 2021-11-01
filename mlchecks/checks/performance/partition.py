436fae61893088c055efac67dcb1b4b7772d917d
Matan Perlmutter <matan@deepchecks.com>
2021-11-01 12:41:38 +0200

    Fix lint and docstring

--- mlchecks/checks/performance/partition.py
+++ mlchecks/checks/performance/partition.py
@@ -1,3 +1,4 @@
+"""Module of functions to partition columns into segments."""
 from typing import List, Callable
 
 import numpy as np
@@ -14,39 +15,55 @@ class MLChecksFilter:
     """Use this class to mark to not filter by equal but filter by not equal values."""
 
     def __init__(self, filter_func: Callable, label: str):
+        """Init MLChecksFilter.
+
+        Args:
+            filter_func (Callable): function which receive dataframe and return a filter on it
+            label (str): name of the filter
+        """
         self.filter_func = filter_func
         self.label = label
 
     def filter(self, dataframe):
+        """Run the filter on given dataframe."""
         return dataframe.loc[self.filter_func(dataframe)]
 
 
 def numeric_segmentation_edges(column: pd.Series, max_segments: int) -> List[MLChecksFilter]:
-    percentile_values: List = (np.nanpercentile(column.astype(float).values, np.linspace(0, 100, max_segments + 1))
-                               .tolist())
+    percentile_values = np.nanpercentile(column.to_numpy(), np.linspace(0, 100, max_segments + 1))
+    # If there are a lot of duplicate values, some of the quantiles might be equal,
+    # so filter them (with preserved order)
+    return pd.unique(percentile_values)
 
-    # if have [a,a) segments, make it [a,a+eps)
-    repeating_values = []
-    return_percentile_values = []
-    for i in range(len(percentile_values)):
-        if percentile_values[i] not in repeating_values: # Matan: Why?
-            return_percentile_values.append(percentile_values[i])
-            repeating_values.append(percentile_values[i])
 
-    return return_percentile_values
+def largest_category_index_up_to_ratio(histogram, max_segments, max_cat_proportions):
+    total_values = sum(histogram.values)
+    first_less_then_max_cat_proportions_idx = np.argwhere(
+        histogram.values.cumsum() > total_values * max_cat_proportions
+    )
+    if first_less_then_max_cat_proportions_idx.shape[0] > 0:  # if there is such a category
+        first_less_then_max_cat_proportions_idx = first_less_then_max_cat_proportions_idx[0][0]
+    else:
+        first_less_then_max_cat_proportions_idx = max_segments
+
+    # Get index of last value in histogram to show
+    return min(max_segments, histogram.size, first_less_then_max_cat_proportions_idx + 1)
 
 
 def partition_column(dataset: Dataset, column_name: str, max_segments: int, max_cat_proportions: float = 0.9)\
         -> List[MLChecksFilter]:
-    """for categorical we'll have a max of max_segments+1, for the 'Others'.
-    categories will contain at most max_cat_proportions% of the data, the rest will go to 'Others' even if less then
-    max_segments.
+    """Split column into segments.
+
+    For categorical we'll have a max of max_segments + 1, for the 'Others'. We take the largest categories which
+    cumulative percent in data is equal/larger than `max_cat_proportions`. the rest will go to 'Others' even if less
+    than max_segments.
+    For numerical we split into `max_segments` number of quantiles.
 
     Args:
         dataset (Dataset):
         column_name (str):
-        max_segments (int):
-        max_cat_proportions (float):
+        max_segments (int): maximum number of segments to split into.
+        max_cat_proportions (float): (for categorical) ratio to aggregate largest values to show.
     """
     column = dataset.data[column_name]
     if column_name not in dataset.cat_features():
@@ -66,17 +83,8 @@ def partition_column(dataset: Dataset, column_name: str, max_segments: int, max_
     else:
         # Get sorted histogram
         cat_hist_dict = column.value_counts()
-        # Find first value which accumulated sum is larger than max proportion
-        first_less_then_max_cat_proportions_idx = np.argwhere(
-            cat_hist_dict.values.cumsum() > len(column) * max_cat_proportions
-        )
-        if first_less_then_max_cat_proportions_idx.shape[0] > 0:  # if there is such a category
-            first_less_then_max_cat_proportions_idx = first_less_then_max_cat_proportions_idx[0][0]
-        else:
-            first_less_then_max_cat_proportions_idx = max_segments
-
         # Get index of last value in histogram to show
-        n_large_cats = min(max_segments, len(cat_hist_dict), first_less_then_max_cat_proportions_idx + 1)
+        n_large_cats = largest_category_index_up_to_ratio(cat_hist_dict, max_segments, max_cat_proportions)
 
         filters = []
         for i in range(n_large_cats):
@@ -84,7 +92,7 @@ def partition_column(dataset: Dataset, column_name: str, max_segments: int, max_
             filters.append(MLChecksFilter(f, cat_hist_dict.index[i]))
 
         if len(cat_hist_dict) > n_large_cats:
-            f = lambda df, values=cat_hist_dict.index[n_large_cats:]: ~df[column_name].isin(values)
+            f = lambda df, values=cat_hist_dict.index[:n_large_cats]: ~df[column_name].isin(values)
             filters.append(MLChecksFilter(f, 'Others'))
 
         return filters
--- mlchecks/checks/performance/segment_performance.py
+++ mlchecks/checks/performance/segment_performance.py
@@ -1,9 +1,8 @@
+"""Module of segment performance check."""
 from typing import Callable, Union
-
 import numpy as np
 from matplotlib.axes import Axes
-
-from mlchecks import Dataset, CheckResult
+from mlchecks import Dataset, CheckResult, SingleDatasetBaseCheck
 from mlchecks.checks.performance.partition import partition_column
 from mlchecks.metric_utils import validate_scorer, task_type_check, DEFAULT_SINGLE_METRIC, DEFAULT_METRICS_DICT
 from mlchecks.string_utils import format_number
@@ -14,11 +13,25 @@ __all__ = ['segment_performance']
 
 
 def segment_performance(dataset: Dataset, model, metric: Union[str, Callable] = None,
-                        feature_1: str = None, feature_2: str = None, num_segments: int = 10):
+                        feature_1: str = None, feature_2: str = None, max_segments: int = 10):
+    """Display performance metric segmented by 2 given features in a heatmap.
+
+    Args:
+        dataset (Dataset): a Dataset object.
+        model (BaseEstimator): A scikit-learn-compatible fitted estimator instance.
+        metric (Union[str, Callable]): Metric to show, either function or sklearn scorer name.
+        feature_1 (str): feature to segment by on y-axis.
+        feature_2 (str): feature to segment by on x-axis.
+        max_segments (int): maximal number of segments to split the a values into.
+    """
     self = segment_performance
     # Validations
     if feature_1 is None or feature_2 is None:
-        raise MLChecksValueError('Must define both `feature_1` and `feature_2`')
+        raise MLChecksValueError('Must define both "feature_1" and "feature_2"')
+    # if feature_1 == feature_2:
+    #     raise MLChecksValueError('"feature_1" must be different than "feature_2"')
+    if not isinstance(max_segments, int) or max_segments < 0:
+        raise MLChecksValueError('"num_segments" must be positive integer')
 
     Dataset.validate_dataset(dataset, self.__name__)
     dataset.validate_label(self.__name__)
@@ -32,10 +45,12 @@ def segment_performance(dataset: Dataset, model, metric: Union[str, Callable] = 
         metric_name = DEFAULT_SINGLE_METRIC[model_type]
         scorer = DEFAULT_METRICS_DICT[model_type][metric_name]
 
-    feature_1_filters = partition_column(dataset, feature_1, max_segments=num_segments)
-    feature_2_filters = partition_column(dataset, feature_2, max_segments=num_segments)
+    feature_1_filters = partition_column(dataset, feature_1, max_segments=max_segments)
+    feature_2_filters = partition_column(dataset, feature_2, max_segments=max_segments)
 
     scores = np.empty((len(feature_1_filters), len(feature_2_filters)), dtype=float)
+    counts = np.empty((len(feature_1_filters), len(feature_2_filters)), dtype=int)
+
     for i, feature_1_filter in enumerate(feature_1_filters):
         data = dataset.data
         feature_1_df = feature_1_filter.filter(data)
@@ -48,37 +63,66 @@ def segment_performance(dataset: Dataset, model, metric: Union[str, Callable] = 
             else:
                 score = scorer(model, feature_2_df[dataset.features()], feature_2_df[dataset.label_name()])
             scores[i, j] = score
+            counts[i, j] = len(feature_2_df)
 
     def display():
         ax: Axes
-        _, ax = plt.subplots(1, 1, figsize=(7, 4))
-        im = ax.imshow(np.array(scores, dtype=float))
+        plt.rcParams['font.family'] = 'sans-serif'
+        plt.rcParams['font.sans-serif'] = ['Tahoma']
+        plt.rcParams['font.monospace'] = 'Ubuntu Mono'
+
+        _, ax = plt.subplots(1, 1, figsize=(10, 7))
+        im = ax.imshow(np.array(scores, dtype=float), cmap='RdYlGn')
 
         # Create colorbar
         cbar = ax.figure.colorbar(im, ax=ax)
-        cbar.ax.set_ylabel('Score', rotation=-90, va="bottom")
+        cbar.ax.set_ylabel('Score', rotation=-90, va='bottom')
 
         x = [v.label for v in feature_2_filters]
         y = [v.label for v in feature_1_filters]
 
         # Set ticks with labels
-        ax.set_xticks(np.arange(len(x)))
-        ax.set_yticks(np.arange(len(y)))
-        ax.set_xticklabels(x)
-        ax.set_yticklabels(y)
+        ax.set_xticks(np.arange(len(x)), minor=False)
+        ax.set_yticks(np.arange(len(y)), minor=False)
+        ax.set_xticklabels(x, minor=False)
+        ax.set_yticklabels(y, minor=False)
 
         plt.xlabel(feature_2)
         plt.ylabel(feature_1)
 
         # Rotate the tick labels and set their alignment.
-        plt.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
+        plt.setp(ax.get_xticklabels(), rotation=45, ha='right', rotation_mode='anchor')
 
         # Loop over data dimensions and create text annotations.
+        q1, q2 = np.nanquantile(scores.flatten(), [0.1, 0.9])
         for i in range(len(y)):
             for j in range(len(x)):
-                if not np.isnan(scores[i, j]):
-                    ax.text(j, i, format_number(scores[i, j]), ha="center", va="center", color="w")
+                score = scores[i, j]
+                if not np.isnan(score):
+                    color = 'black' if q1 < score < q2 else 'white'
+                    text = f'{format_number(score)}\n({counts[i, j]})'
+                    ax.text(j, i, text, ha='center', va='center', color=color)
 
-        ax.set_title(f'{metric_name} by features {feature_1}/{feature_2}')
+        ax.set_title(f'{metric_name} (count) by features {feature_1}/{feature_2}')
 
     return CheckResult(scores, check=self, display=display)
+
+
+class SegmentPerformance(SingleDatasetBaseCheck):
+    """Display performance metric segmented by 2 given features in a heatmap.
+
+    Params:
+        metric (Union[str, Callable]): Metric to show, either function or sklearn scorer name.
+        feature_1 (str): feature to segment by on y-axis.
+        feature_2 (str): feature to segment by on x-axis.
+        max_segments (int): maximal number of segments to split the a values into.
+    """
+
+    def run(self, dataset, model=None) -> CheckResult:
+        """Run 'segment_performance' check.
+
+        Args:
+            dataset (Dataset): a Dataset object.
+            model (BaseEstimator): A scikit-learn-compatible fitted estimator instance.
+        """
+        return segment_performance(dataset, model, **self.params)
