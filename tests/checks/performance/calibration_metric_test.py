6b4ef082f3e5188dcc51d9ddf4ec1ab48db22785
Itay Gabbay <itay@deepchecks.com>
2021-11-02 12:15:29 +0200

    fixing tests

--- tests/checks/performance/calibration_metric_test.py
+++ tests/checks/performance/calibration_metric_test.py
@@ -10,13 +10,13 @@ def test_dataset_wrong_input():
     # Act & Assert
     assert_that(calling(calibration_metric).with_args(bad_dataset, None),
                 raises(MLChecksValueError,
-                       'function calibration_metric_check requires dataset to be of type Dataset. instead got: str'))
+                       'function calibration_metric requires dataset to be of type Dataset. instead got: str'))
 
 
 def test_dataset_no_label(iris_dataset, iris_adaboost):
     # Assert
     assert_that(calling(calibration_metric).with_args(iris_dataset, iris_adaboost),
-                raises(MLChecksValueError, 'function calibration_metric_check requires dataset to have a label column'))
+                raises(MLChecksValueError, 'function calibration_metric requires dataset to have a label column'))
 
 
 def test_model_info_object(iris_labeled_dataset, iris_adaboost):
