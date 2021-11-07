1782c8bcbd81cd7f289416ed8d2ffdd7fdc2be59
DBI <danb@deepchecks.com>
2021-11-07 17:46:09 +0200

    add tests

--- mlchecks/feature_importance_utils.py
+++ mlchecks/feature_importance_utils.py
@@ -1,11 +1,11 @@
 """Utils module containing feature importance calculations."""
 import numpy as np
 import pandas as pd
-from sklearn.inspection import permutation_importance
 from typing import Any
+from sklearn.inspection import permutation_importance
+from sklearn.utils.validation import check_is_fitted
 
 from mlchecks import Dataset
-from mlchecks.utils import MLChecksValueError
 
 __all__ = ['calculate_feature_importance']
 
@@ -20,12 +20,10 @@ def calculate_feature_importance(model: Any, dataset: Dataset) -> [pd.Series]:
         pd.Series of feature importance normalized to 0-1 indexed by feature names
 
     Raise:
-        MLChecksValueError: if model is not fitted or model is not valid for Dataset
+        NotFittedError: Call 'fit' with appropriate arguments before using this estimator.
     """
+    check_is_fitted(model)
     dataset.validate_model(model)
-
-    if 'is_fitted' in dir(model) and model.is_fitted() is False:
-        raise MLChecksValueError('Model must be fitted to calculate feature importance')
 
     if 'feature_importances_' in dir(model):  # Ensambles
         normalized_feature_importance_values = model.feature_importances_/model.feature_importances_.sum()
@@ -42,11 +40,15 @@ def calculate_feature_importance(model: Any, dataset: Dataset) -> [pd.Series]:
 
 def _calc_importance(model: Any, dataset: Dataset, n_repeats=30, random_state=42):
     """Calculate permutation feature importance. Return nonzero value only when std doesn't mask signal."""
-    dataset.validate_label()
+    dataset.validate_label('_calc_importance')
     r = permutation_importance(model, dataset.features_columns(),
                                dataset.label_col(),
                                n_repeats=n_repeats,
                                random_state=random_state)
     significance_mask = r.importances_mean - r.importances_std > 0
     feature_importances = r.importances_mean * significance_mask
-    return pd.Series(feature_importances, index=dataset.features)
+    total = feature_importances.sum()
+    if total != 0:
+        feature_importances = feature_importances / total
+
+    return pd.Series(feature_importances, index=dataset.features())
