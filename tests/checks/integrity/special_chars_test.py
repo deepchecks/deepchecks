9690d004d8765ab5443c809e7beaabc185cb1bb9
Itay Gabbay <itay@deepchecks.com>
2021-11-07 11:37:38 +0200

    making special_characters without function

--- tests/checks/integrity/special_chars_test.py
+++ tests/checks/integrity/special_chars_test.py
@@ -2,7 +2,7 @@
 import pandas as pd
 
 from hamcrest import assert_that, has_length, calling, raises, has_items
-from mlchecks.checks.integrity.special_chars import special_characters
+from mlchecks.checks.integrity.special_chars import SpecialCharacters
 from mlchecks.utils import MLChecksValueError
 
 
@@ -11,7 +11,7 @@ def test_single_column_no_invalid():
     data = {'col1': ['foo', 'bar', 'cat']}
     dataframe = pd.DataFrame(data=data)
     # Act
-    result = special_characters(dataframe)
+    result = SpecialCharacters().run(dataframe)
     # Assert
     assert_that(result.value, has_length(0))
 
@@ -21,7 +21,7 @@ def test_single_column_invalid():
     data = {'col1': [1, 'bar!', 'cat', '#@$%']}
     dataframe = pd.DataFrame(data=data)
     # Act
-    result = special_characters(dataframe)
+    result = SpecialCharacters().run(dataframe)
     # Assert
     assert_that(result.value, has_length(1))
     assert_that(result.value.iloc[0]['Most Common Special-Only Samples'], has_items('#@$%'))
@@ -32,7 +32,7 @@ def test_single_column_multi_invalid():
     data = {'col1': ['1', 'bar!', 'ca\nt', '\n ']}
     dataframe = pd.DataFrame(data=data)
     # Act
-    result = special_characters(dataframe)
+    result = SpecialCharacters().run(dataframe)
     # Assert
     assert_that(result.value, has_length(1))
 
@@ -42,7 +42,7 @@ def test_double_column_one_invalid():
     data = {'col1': ['^', '?!', '!!!', '?!', '!!!', '?!'], 'col2': ['', 6, 66, 666.66, 7, 5]}
     dataframe = pd.DataFrame(data=data)
     # Act
-    result = special_characters(dataframe)
+    result = SpecialCharacters().run(dataframe)
     # Assert
     assert_that(result.value, has_length(1))
     assert_that(result.value.iloc[0]['Most Common Special-Only Samples'], has_items('!!!', '?!'))
@@ -53,7 +53,7 @@ def test_double_column_ignored_invalid():
     data = {'col1': ['1', 'bar!', 'cat'], 'col2': [6, 66, 666.66]}
     dataframe = pd.DataFrame(data=data)
     # Act
-    result = special_characters(dataframe, ignore_columns=['col1'])
+    result = SpecialCharacters(ignore_columns=['col1']).run(dataframe)
     # Assert
     assert_that(result.value, has_length(0))
 
@@ -63,7 +63,7 @@ def test_double_column_specific_invalid():
     data = {'col1': ['1', 'bar^', '^?!'], 'col2': [6, 66, 666.66]}
     dataframe = pd.DataFrame(data=data)
     # Act
-    result = special_characters(dataframe, columns=['col1'])
+    result = SpecialCharacters(columns=['col1']).run(dataframe)
     # Assert
     assert_that(result.value, has_length(1))
     assert_that(result.value.iloc[0]['Most Common Special-Only Samples'], has_items('^?!'))
@@ -74,7 +74,8 @@ def test_double_column_specific_and_ignored_invalid():
     data = {'col1': ['1', 'bar()', 'cat'], 'col2': [6, 66, 666.66]}
     dataframe = pd.DataFrame(data=data)
     # Act & Assert
-    assert_that(calling(special_characters).with_args(dataframe, ignore_columns=['col1'], columns=['col1']),
+    check = SpecialCharacters(ignore_columns=['col1'], columns=['col1'])
+    assert_that(calling(check.run).with_args(dataframe),
                 raises(MLChecksValueError))
 
 
@@ -83,7 +84,7 @@ def test_double_column_double_invalid():
     data = {'col1': ['1_', 'bar', 'cat}', '{}'], 'col2': ['&!', 6, '66&.66.6', 666.66]}
     dataframe = pd.DataFrame(data=data)
     # Act
-    result = special_characters(dataframe)
+    result = SpecialCharacters().run(dataframe)
     # Assert
     assert_that(result.value, has_length(2))
     assert_that(result.value.loc['col1']['Most Common Special-Only Samples'], has_items('{}'))
