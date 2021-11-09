d50438d4ef53bcd02ed28bc2e764179687b5b21b
Jonthan Liberman <jonthanliberman@ip-192-168-1-143.eu-west-1.compute.internal>
2021-11-09 12:47:20 +0200

    v1

--- mlchecks/checks/leakage/__init__.py
+++ mlchecks/checks/leakage/__init__.py
@@ -3,5 +3,6 @@ from .single_feature_contribution import *
 from .single_feature_contribution_train_validation import *
 from .index_leakage import *
 from .data_sample_leakage import *
-from .date_leakage import *
+from .date_train_validation_leakage_duplicates import *
+from .date_train_validation_leakage_overlap import *
 from .identifier_leakage import *
