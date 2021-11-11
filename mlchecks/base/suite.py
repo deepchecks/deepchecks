04be76e0d60662b7ce5e85817b997b2b8e756b9c
Yuriy Romanyshyn <yuriy.romanyshyn.lv.ua@gmail.com>
2021-11-11 13:47:54 +0200

    bug fix; removed redundant import;

--- mlchecks/base/suite.py
+++ mlchecks/base/suite.py
@@ -9,7 +9,7 @@ from mlchecks.base.check import BaseCheck, CheckResult, TrainValidationBaseCheck
 
 __all__ = ['CheckSuite']
 
-from mlchecks.utils import MLChecksValueError, is_notebook
+from mlchecks.utils import is_notebook
 
 
 class CheckSuite(BaseCheck):
@@ -73,12 +73,23 @@ class CheckSuite(BaseCheck):
         # Run all checks
         results = []
         for check in self.checks:
+
             label.value = f'Running {str(check)}'
-            if isinstance(check, TrainValidationBaseCheck):
-                results.append(check.run(train_dataset=train_dataset, validation_dataset=validation_dataset,
-                                         model=model))
-            elif isinstance(check, CompareDatasetsBaseCheck):
-                results.append(check.run(dataset=validation_dataset, baseline_dataset=train_dataset, model=model))
+
+            if train_dataset is not None and validation_dataset is not None:
+                if isinstance(check, TrainValidationBaseCheck):
+                    results.append(check.run(
+                        train_dataset=train_dataset,
+                        validation_dataset=validation_dataset,
+                        model=model
+                    ))
+                elif isinstance(check, CompareDatasetsBaseCheck):
+                    results.append(check.run(
+                        dataset=validation_dataset,
+                        baseline_dataset=train_dataset,
+                        model=model
+                    ))
+
             elif isinstance(check, SingleDatasetBaseCheck):
                 if check_datasets_policy in ['both', 'train'] and train_dataset is not None:
                     res = check.run(dataset=train_dataset, model=model)
@@ -88,17 +99,15 @@ class CheckSuite(BaseCheck):
                     res = check.run(dataset=validation_dataset, model=model)
                     res.header = f'{res.header} - Validation Dataset'
                     results.append(res)
-            elif isinstance(check, ModelOnlyBaseCheck):
+
+            elif isinstance(check, ModelOnlyBaseCheck) and model is not None:
                 results.append(check.run(model=model))
-            elif isinstance(check, CheckSuite):
-                suite_res = check.run(model, train_dataset, validation_dataset, check_datasets_policy)
-                if check.name in results:
-                    raise MLChecksValueError('Each suite must have a unique name')
-                results.append(suite_res)
+
             else:
-                raise TypeError(f'Expected check of type SingleDatasetBaseCheck, CompareDatasetsBaseCheck, '
+                raise TypeError('Expected check of type SingleDatasetBaseCheck, CompareDatasetsBaseCheck, '
                                 f'TrainValidationBaseCheck or ModelOnlyBaseCheck. Got  {check.__class__.__name__} '
-                                f'instead')
+                                'instead')
+
             progress_bar.value = progress_bar.value + 1
 
         progress_bar.close()
