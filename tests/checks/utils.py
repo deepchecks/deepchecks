67bb83e41839fc56b6537a78a2b88375dc027bab
Matan Perlmutter <matan@deepchecks.com>
2021-11-14 20:05:11 +0200

    Remove re.Pattern import

--- tests/checks/utils.py
+++ tests/checks/utils.py
@@ -1,15 +1,15 @@
 """Utils functions for testing."""
-from re import Pattern
-from typing import Union, Pattern as PatternType
+from typing import Union, Pattern
 
 from hamcrest import all_of, has_property, matches_regexp
 
 from mlchecks import ConditionCategory
 
 
-def equal_condition_result(is_pass: bool, name: str, details: Union[str, PatternType] = '',
+def equal_condition_result(is_pass: bool, name: str, details: Union[str, Pattern] = '',
                            category: ConditionCategory = ConditionCategory.FAIL):
-    if isinstance(details, Pattern):
+    # Check if details is a regex class
+    if 'match' in dir(details):
         details_matcher = matches_regexp(details)
     else:
         details_matcher = details
