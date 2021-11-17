542d02b6b2cb9e04a208a950cd50dbbc2ec8e316
JKL98ISR <jonatan.lib@gmail.com>
2021-11-17 14:05:18 +0200

    added condition to naive model comparison  (#134)
    * added_naive_validation
    
    * added_naive_validation
    
    * added_naive_validation
    
    * added_naive_validation
    
    * added_naive_validation
    
    * added_naive_validation
    
    * added_naive_validation
    
    * added_naive_validation
    
    * added_naive_validation
    
    * added_naive_validation
    
    * added_naive_validation
    
    * added_naive_validation
    
    * added_naive_validation
    
    * added_naive_validation
    
    * added_naive_validation
    
    * added_naive_validation
    
    * name
    
    * name
    
    * name
    
    Co-authored-by: Jonthan Liberman <jonthanliberman@ip-192-168-1-17.eu-west-1.compute.internal>
    Co-authored-by: Jonthan Liberman <jonthanliberman@Jonthans-MacBook-Pro.local>
    Co-authored-by: Jonthan Liberman <jonthanliberman@ip-192-168-1-15.eu-west-1.compute.internal>

--- tests/checks/performance/naive_model_comparison_test.py
+++ tests/checks/performance/naive_model_comparison_test.py
@@ -1,7 +1,10 @@
 """Contains unit tests for the confusion_matrix_report check."""
 from deepchecks.checks.performance import NaiveModelComparison
+from deepchecks.string_utils import format_number
 from deepchecks.utils import DeepchecksValueError
-from hamcrest import assert_that, calling, raises, close_to
+from tests.checks.utils import equal_condition_result
+
+from hamcrest import assert_that, calling, raises, close_to, has_items
 
 
 def test_dataset_wrong_input():
@@ -65,3 +68,38 @@ def test_regression_statistical(diabetes_split_dataset_and_model):
     # Assert
     assert_that(result['given_model_score'], close_to(-57, 0.5))
     assert_that(result['naive_model_score'], close_to(-76, 0.5))
+
+
+def test_condition_ratio_not_less_than_not_passed(diabetes_split_dataset_and_model):
+    # Arrange
+    train_ds, val_ds, clf = diabetes_split_dataset_and_model
+    check = NaiveModelComparison().add_condition_ratio_not_less_than(min_allowed_ratio=1.4)
+
+    # Act
+    check_result = check.run(train_ds, val_ds, clf)
+    condition_result = check.conditions_decision(check_result)
+    ratio = check_result.value['ratio']
+
+    assert_that(ratio, close_to(1.32, 0.03))
+    assert_that(condition_result, has_items(
+        equal_condition_result(is_pass=False,
+                               name='Ratio not less than 1.4 '
+                                    'between the given model\'s result and the naive model\'s result',
+                               details=f'The given model performs {format_number(ratio)} times compared' \
+                                       'to the naive model using the given metric')
+    ))
+
+
+def test_condition_ratio_not_less_than_passed(diabetes_split_dataset_and_model):
+    # Arrange
+    train_ds, val_ds, clf = diabetes_split_dataset_and_model
+    check = NaiveModelComparison().add_condition_ratio_not_less_than(min_allowed_ratio=1.3)
+
+    # Act
+    result = check.conditions_decision(check.run(train_ds, val_ds, clf))
+
+    assert_that(result, has_items(
+        equal_condition_result(is_pass=True,
+                               name='Ratio not less than 1.3 '
+                                    'between the given model\'s result and the naive model\'s result')
+    ))
