b8040ae0f85118836deff0346145a6ff60b9b921
nirchecks <nir@deepchecks.com>
2021-11-20 15:57:08 +0200

    added docstrings

--- deepchecks/checks/drift/train_test_drift.py
+++ deepchecks/checks/drift/train_test_drift.py
@@ -1,7 +1,7 @@
 """Module contains Train Test Drift check."""
 
 from collections import Counter
-from typing import Union, Iterable
+from typing import Union, Iterable, Tuple, List
 
 import numpy as np
 import pandas as pd
@@ -15,25 +15,37 @@ __all__ = ['TrainTestDrift']
 PSI_MIN_PERCENTAGE = 0.01
 
 
-def preprocess_for_psi(dist1: np.array, dist2: np.array, max_num_categories):
+def preprocess_for_psi(dist1: np.array, dist2: np.array, max_num_categories) -> Tuple[float, float, List]:
     """
-    Preprocess distributions in order to be able to be sent f
+    Preprocess distributions in order to be able to be calculated by PSI.
 
-    Function preprocesses the data so it encodes rare categories into an "OTHER" category. This is done according to
-    the values of dist1, which is treated as the "expected" distribution.
+    Function returns the value counts for each distribution and the categories list. If there are more than
+    max_num_categories, it encodes rare categories into an "OTHER" category. This is done according to the values of
+    dist1, which is treated as the "expected" distribution.
 
     Function is for categorical data only.
+    Args:
+        dist1: first distribution, treated as the expected distribution
+        dist2: second distribution, treated as the actual distribution
+        max_num_categories: max number of allowed categories. If there are more, they are binned into an "Other"
+        category. If max_num_categories=None, there is no limit.
+
+    Returns:
+        expected_percents: array of percentages of each value in the expected distribution.
+        actual_percents: array of percentages of each value in the actual distribution.
+        categories_list: list of all categories that the percentages represent.
+
     """
     all_categories = list(set(np.unique(dist1)).union(set(dist2)))
 
     if max_num_categories is not None and len(all_categories) > max_num_categories:
         dist1_counter = dict(Counter(dist1).most_common(max_num_categories))
-        dist1_counter['OTHER_RARE_CATEGORIES'] = len(dist1) - sum(dist1_counter.values())
+        dist1_counter['Other rare categories'] = len(dist1) - sum(dist1_counter.values())
         categories_list = list(dist1_counter.keys())
 
         dist2_counter = Counter(dist2)
         dist2_counter = {k: dist2_counter[k] for k in categories_list}
-        dist2_counter['OTHER_RARE_CATEGORIES'] = len(dist2) - sum(dist2_counter.values())
+        dist2_counter['Other rare categories'] = len(dist2) - sum(dist2_counter.values())
 
     else:
         dist1_counter = Counter(dist1)
@@ -47,6 +59,19 @@ def preprocess_for_psi(dist1: np.array, dist2: np.array, max_num_categories):
 
 
 def psi(expected_percents: np.array, actual_percents: np.array):
+    """
+    Calculate the PSI (Population Stability Index).
+
+    See https://www.lexjansen.com/wuss/2017/47_Final_Paper_PDF.pdf
+
+    Args:
+        expected_percents: array of percentages of each value in the expected distribution.
+        actual_percents: array of percentages of each value in the actual distribution.
+
+    Returns:
+        psi: The PSI score
+
+    """
     psi_value = 0
     for i in range(len(expected_percents)):
         # In order for the value not to diverge, we cap our min percentage value
@@ -60,7 +85,19 @@ def psi(expected_percents: np.array, actual_percents: np.array):
 
 def earth_movers_distance(dist1: np.array, dist2: np.array):
     """
-    Function is for numerical data only
+    Calculate the Earth Movers Distance (Wasserstein distance).
+
+    See https://en.wikipedia.org/wiki/Wasserstein_metric
+
+    Function is for numerical data only.
+
+    Args:
+        dist1: array of numberical values.
+        dist2: array of numberical values to compare dist1 to.
+
+    Returns:
+        the Wasserstein distance between the two distributions.
+
     """
     unique1 = np.unique(dist1)
     unique2 = np.unique(dist2)
@@ -80,7 +117,7 @@ def earth_movers_distance(dist1: np.array, dist2: np.array):
 
 
 class TrainTestDrift(TrainTestBaseCheck):
-    """Calculate drift between train dataset and test dataset"""
+    """Calculate drift between train dataset and test dataset."""
 
     def __init__(self, columns: Union[str, Iterable[str]] = None, ignore_columns: Union[str, Iterable[str]] = None,
                  max_num_categories: int = 10):
@@ -92,6 +129,8 @@ class TrainTestDrift(TrainTestBaseCheck):
             ones.
             ignore_columns (Union[str, Iterable[str]]): Columns to ignore, if none given checks based on columns
             variable.
+            max_num_categories (int): Only for categorical columns. max number of allowed categories. If there are more,
+             they are binned into an Other" category. If max_num_categories=None, there is no limit.
         """
         super().__init__()
         self.columns = columns
@@ -99,11 +138,43 @@ class TrainTestDrift(TrainTestBaseCheck):
         self.max_num_categories = max_num_categories
 
     def run(self, train_dataset, test_dataset, model=None) -> CheckResult:
+        """Run check.
 
+        Check calculates a drift score for each column in test dataset, by comparing its distribution to the train
+        dataset.
+        For numerical columns, we use the Earth Movers Distance.
+        See https://www.lexjansen.com/wuss/2017/47_Final_Paper_PDF.pdf
+        For categorical columns, we use the Population Stability Index (PSI).
+        See https://en.wikipedia.org/wiki/Wasserstein_metric.
+
+        Args:
+            train_dataset (Dataset): The training dataset object. Must contain a label column.
+            test_dataset (Dataset): The test dataset object. Must contain a label column.
+            model: A scikit-learn-compatible fitted estimator instance
+
+        Returns:
+            CheckResult:
+                value: dictionary of column name to drift score.
+                display: distribution graph for each column, comparing the train and test distributions.
+
+        Raises:
+            DeepchecksValueError: If the object is not a Dataset or DataFrame instance
+        """
         return self._calc_drift(train_dataset, test_dataset)
 
     def _calc_drift(self, train_dataset: Dataset, test_dataset: Dataset) -> CheckResult:
+        """
+        Calculate drift for all columns.
 
+        Args:
+            train_dataset (Dataset): The training dataset object. Must contain a label column.
+            test_dataset (Dataset): The test dataset object. Must contain a label column.
+
+        Returns:
+            CheckResult:
+                value: dictionary of column name to drift score.
+                display: distribution graph for each column, comparing the train and test distributions.
+        """
         train_dataset = Dataset.validate_dataset_or_dataframe(train_dataset)
         test_dataset = Dataset.validate_dataset_or_dataframe(test_dataset)
 
@@ -130,6 +201,20 @@ class TrainTestDrift(TrainTestBaseCheck):
 
     def _calc_drift_per_column(self, train_column: pd.Series, test_column: pd.Series, column_name: str,
                                column_type: str):
+        """
+        Calculate drift score per column.
+
+        Args:
+            train_column: column from train dataset
+            test_column: same column from test dataset
+            column_name: name of column
+            column_type: type of column (either "numerical" or "categorical")
+
+        Returns:
+            score: drift score of the difference between the two columns' distributions (Earth movers distance for
+            numerical, PSI for categorical)
+            display: graph comparing the two distributions (density for numerical, stack bar for categorical)
+        """
         train_dist = train_column.dropna().values.reshape(-1)
         test_dist = test_column.dropna().values.reshape(-1)
 
