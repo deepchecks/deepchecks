c8f3381a35a83774feff8c8e8447b638268d0467
Matan Perlmutter <matan@deepchecks.com>
2021-11-21 17:43:52 +0200

    Update display of dataframe to use `set_precision` to support pandas 1.15

--- deepchecks/base/display_pandas.py
+++ deepchecks/base/display_pandas.py
@@ -1,5 +1,6 @@
 """Handle displays of pandas objects."""
 from typing import Union
+import warnings
 
 from IPython.core.display import display_html
 import pandas as pd
@@ -8,33 +9,33 @@ __all__ = ['display_dataframe', 'dataframe_to_html']
 from pandas.io.formats.style import Styler
 
 
-def display_dataframe(df: Union[pd.DataFrame, Styler], hide_index=False):
+def display_dataframe(df: Union[pd.DataFrame, Styler]):
     """Display in IPython given dataframe.
 
     Args:
-        df (pd.DataFrame): Dataframe to display
-        hide_index (bool): Whether to hide or not the dataframe index.
+        df (Union[pd.DataFrame, Styler]): Dataframe to display
     """
-    display_html(dataframe_to_html(df, hide_index), raw=True)
+    display_html(dataframe_to_html(df), raw=True)
 
 
-def dataframe_to_html(df: Union[pd.DataFrame, Styler], hide_index=False):
+def dataframe_to_html(df: Union[pd.DataFrame, Styler]):
     """Convert dataframe to html.
 
     Args:
-        df (pd.DataFrame): Dataframe to convert.
-        hide_index (bool): Whether to hide or not the dataframe index.
+        df (Union[pd.DataFrame, Styler]): Dataframe to convert to html
     """
-    # Align everything to the left
     try:
         if isinstance(df, pd.DataFrame):
             df_styler = df.style
         else:
             df_styler = df
+        # Using deprecated pandas method so hiding the warning
+        with warnings.catch_warnings():
+            warnings.simplefilter(action='ignore', category=FutureWarning)
+            df_styler.set_precision(2)
+
+        # Align everything to the left
         df_styler.set_table_styles([dict(selector='table,thead,tbody,th,td', props=[('text-align', 'left')])])
-        df_styler.format(precision=2)
-        if hide_index:
-            df_styler.hide_index()
         return df_styler.render()
     # Because of MLC-154. Dataframe with Multi-index or non unique indices does not have a style
     # attribute, hence we need to display as a regular pd html format.
