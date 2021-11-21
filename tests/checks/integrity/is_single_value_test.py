39f2664bb708abd24a4a6acef675fdb01a1b54d3
benisraeldan <42312361+benisraeldan@users.noreply.github.com>
2021-11-21 15:31:08 +0200

    undo push to master (#167)

--- tests/checks/integrity/is_single_value_test.py
+++ tests/checks/integrity/is_single_value_test.py
@@ -1,11 +1,8 @@
 """Tests for Single Value Check"""
-import numpy as np
 import pandas as pd
 from deepchecks.utils import DeepchecksValueError
 from deepchecks.checks.integrity.is_single_value import IsSingleValue
-from hamcrest import assert_that, calling, raises, equal_to, has_items
-
-from tests.checks.utils import equal_condition_result
+from hamcrest import assert_that, calling, raises, equal_to
 
 
 def helper_test_df_and_result(df, expected_result_value, ignore_columns=None):
@@ -121,23 +118,3 @@ def test_nans(df_with_fully_nan, df_with_single_nan_in_col):
     assert_that(full_result.value)
     assert_that(not single_result.value)
 
-
-def test_condition_fail():
-    df = pd.DataFrame({'a': ['b', 'b', 'b'], 'bbb': ['a', 'a', 'a'], 'f': [1, 2, 3]})
-    check = IsSingleValue().add_condition_not_single_value()
-    result = check.conditions_decision(check.run(df))
-
-    assert_that(result, has_items(
-        equal_condition_result(is_pass=False,
-                               details='Columns containing a single value: [\'a\', \'bbb\']',
-                               name='columns do not contain only a single value')))
-
-
-def test_condition_pass():
-    df = pd.DataFrame({'a': ['b', 'asadf', 'b'], 'bbb': ['a', 'a', np.nan], 'f': [1, 2, 3]})
-    check = IsSingleValue().add_condition_not_single_value()
-    result = check.conditions_decision(check.run(df))
-
-    assert_that(result, has_items(
-        equal_condition_result(is_pass=True,
-                               name='columns do not contain only a single value')))
