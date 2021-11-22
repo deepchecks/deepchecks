fc7481fd347885e635c308f0803d0b2db21b2867
matanper <matanper@gmail.com>
2021-11-22 11:48:39 +0200

    Boosting overfit condition (#169)
    * Add condition add_condition_test_max_score_difference_not_greater_than
    
    * Add tests to add_condition_test_score_decline_not_greater_than
    
    * Update Overfit suite
    
    * Update deepchecks/checks/overfit/boosting_overfit.py
    
    Co-authored-by: Noam Bressler <noamzbr@gmail.com>
    
    * Update notebooks
    
    * Update notebooks
    
    * Update condition name
    
    * Fix tests
    
    Co-authored-by: Matan Perlmutter <matan@deepchecks.com>
    Co-authored-by: Noam Bressler <noamzbr@gmail.com>

--- deepchecks/suites/overfit_suite.py
+++ deepchecks/suites/overfit_suite.py
@@ -9,6 +9,6 @@ __all__ = ['OverfitCheckSuite']
 OverfitCheckSuite = CheckSuite(
     'Overfit Suite',
     TrainTestDifferenceOverfit().add_condition_percentage_degradation_not_greater_than(),
-    BoostingOverfit(),
+    BoostingOverfit().add_condition_test_score_percent_decline_not_greater_than(),
     UnusedFeatures().add_condition_number_of_high_variance_unused_features_not_greater_than()
 )
