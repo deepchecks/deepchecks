093ac9a9b2f2a1c1b9b0b7835a41c2849b6f810c
Noam Bressler <noamzbr@gmail.com>
2021-11-23 17:47:15 +0200

    Update deepchecks/checks/integrity/dominant_frequency_change.py
    Co-authored-by: shir22 <33841818+shir22@users.noreply.github.com>

--- deepchecks/checks/integrity/dominant_frequency_change.py
+++ deepchecks/checks/integrity/dominant_frequency_change.py
@@ -182,7 +182,7 @@ class DominantFrequencyChange(CompareDatasetsBaseCheck):
              allowed to change
         """
         if percent_change_threshold < 0 or percent_change_threshold > 1:
-            raise DeepchecksValueError(f'percent_change_threshold be between 0 and 1, found {percent_change_threshold}')
+            raise DeepchecksValueError(f'percent_change_threshold should be between 0 and 1, found {percent_change_threshold}')
 
         def condition(result: Dict) -> ConditionResult:
             failed_columns = []
