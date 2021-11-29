a6a9b59a265616727eaad62121db44f9aa162d6f
JKL98ISR <jonatan.lib@gmail.com>
2021-11-29 18:49:17 +0200

    fixed_name of condition in data duplicates (#203)
    * fixed_name
    
    * fixed_name
    
    * fixed_name
    
    * fixed_name
    
    Co-authored-by: Jonthan Liberman <jonthanliberman@Jonthans-MacBook-Pro.local>
    Co-authored-by: Jonthan Liberman <jonthanliberman@ip-192-168-1-143.eu-west-1.compute.internal>

--- deepchecks/suites/integrity_suite.py
+++ deepchecks/suites/integrity_suite.py
@@ -35,7 +35,7 @@ def single_dataset_integrity_check_suite() -> CheckSuite:
         MixedNulls().add_condition_different_nulls_not_more_than(),
         MixedTypes().add_condition_rare_type_ratio_not_less_than(),
         StringMismatch().add_condition_no_variants(),
-        DataDuplicates().add_condition_duplicates_not_greater_than(),
+        DataDuplicates().add_condition_ratio_not_greater_than(),
         RareFormatDetection().add_condition_ratio_of_rare_formats_not_greater_than(),
         StringLengthOutOfBounds().add_condition_ratio_of_outliers_not_greater_than(),
         SpecialCharacters().add_condition_ratio_of_special_characters_not_grater_than(),
