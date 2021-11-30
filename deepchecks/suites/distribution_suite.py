681a46d7d7b6184fa848920294d986a2dcf5708f
Itay Gabbay <itay@deepchecks.com>
2021-11-30 13:48:22 +0200

    Renaming checksuite to suite (#206)
    * Renaming checksuite to suite
    
    * fixing tests
    
    * Resolving conflicts

--- deepchecks/suites/distribution_suite.py
+++ deepchecks/suites/distribution_suite.py
@@ -1,15 +1,15 @@
 """The predefined Data Distribution suite module."""
-from deepchecks import CheckSuite
+from deepchecks import Suite
 from deepchecks.checks.distribution import TrainTestDrift
 from deepchecks.checks.distribution import TrustScoreComparison
 
 
-def data_distribution_suite() -> CheckSuite:
+def data_distribution_suite() -> Suite:
     """Create 'Data Distribution Suite'.
 
     The suite runs a check comparing the distributions of the training and test datasets.
     """
-    return CheckSuite(
+    return Suite(
         'Data Distribution',
         TrainTestDrift().add_condition_drift_score_not_greater_than(),
         TrustScoreComparison().add_condition_mean_score_percent_decline_not_greater_than()
