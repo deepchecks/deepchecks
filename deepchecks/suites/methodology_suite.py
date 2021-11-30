28c795381b7498408d4149758267908ccf4cd72a
Yurii Romanyshyn <yurii.romanyshyn@starnavi.io>
2021-11-30 09:40:33 +0200

    fix

--- deepchecks/suites/methodology_suite.py
+++ deepchecks/suites/methodology_suite.py
@@ -107,5 +107,5 @@ def methodological_flaws_check_suite() -> CheckSuite:
         leakage_check_suite(),
         overfit_check_suite(),
         UnusedFeatures().add_condition_number_of_high_variance_unused_features_not_greater_than(),
-        ModelInferenceTimeCheck().add_condition_inference_time_is_not_greater_than()
+        # ModelInferenceTimeCheck().add_condition_inference_time_is_not_greater_than()
     )
