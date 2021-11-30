dddfe7563f3628a4e73b0ee1b7a57fd9e4ea7b61
Jonthan Liberman <jonthanliberman@ip-192-168-1-20.eu-west-1.compute.internal>
2021-11-30 11:57:26 +0200

    fixed for 3.6

--- deepchecks/checks/performance/simple_model_comparison.py
+++ deepchecks/checks/performance/simple_model_comparison.py
@@ -10,7 +10,7 @@ from deepchecks import CheckResult, Dataset
 from deepchecks.base.check import ConditionResult, TrainTestBaseCheck
 from deepchecks.utils.metrics import DEFAULT_METRICS_DICT, DEFAULT_SINGLE_METRIC, task_type_check, \
                                      ModelType, validate_scorer, get_metrics_ratio
-from deepchecks.utils import model_type_validation
+from deepchecks.utils.validation import model_type_validation
 
 __all__ = ['SimpleModelComparison']
 
