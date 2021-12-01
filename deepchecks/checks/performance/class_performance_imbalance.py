e2a081d3672d8984b30ad009eca2d8ad6c6758f2
Yurii Romanyshyn <yurii.romanyshyn@starnavi.io>
2021-12-01 15:19:53 +0200

    renamed alternative_metrics to alternative_scorers

--- deepchecks/checks/performance/class_performance_imbalance.py
+++ deepchecks/checks/performance/class_performance_imbalance.py
@@ -19,52 +19,52 @@ from deepchecks.errors import DeepchecksValueError
 __all__ = ['ClassPerformanceImbalanceCheck']
 
 
-MetricFunc = t.Callable[
+ScorerFunc = t.Callable[
     [object, pd.DataFrame, pd.Series], # model, features, labels
     t.Dict[t.Hashable, t.Union[float, int]] # score for each label
 ]
 
-AlternativeMetric = t.Union[str, MetricFunc]
+AlternativeScorer = t.Union[str, ScorerFunc]
 
 
 CP = t.TypeVar('CP', bound='ClassPerformanceImbalanceCheck')
 
 
 class ClassPerformanceImbalanceCheck(SingleDatasetBaseCheck):
-    """Visualize class imbalance by displaying the difference between class metrics.
+    """Visualize class imbalance by displaying the difference between class score values.
 
     Args:
-        alternative_metrics (Mapping[str, Union[str, Callable]]):
-            An optional dictionary of metric name or scorer functions
+        alternative_scorers (Mapping[str, Union[str, Callable]]):
+            An optional dictionary of scorer name or scorer functions
 
     Raises:
         DeepchecksValueError:
-            if provided dict of metrics is emtpy;
-            if one of the entries of the provided metrics dict contains not callable value;
+            if provided dict of scorers is emtpy;
+            if one of the entries of the provided scorers dict contains not callable value;
     """
 
     def __init__(
         self,
-        alternative_metrics: t.Optional[t.Mapping[str, AlternativeMetric]] = None
+        alternative_scorers: t.Optional[t.Mapping[str, AlternativeScorer]] = None
     ):
         super().__init__()
-        self.alternative_metrics: t.Optional[t.Mapping[str, MetricFunc]] = None
+        self.alternative_scorers: t.Optional[t.Mapping[str, ScorerFunc]] = None
 
-        if alternative_metrics is not None and len(alternative_metrics) == 0:
-            raise DeepchecksValueError('alternative_metrics - expected to receive not empty dict of scorers!')
+        if alternative_scorers is not None and len(alternative_scorers) == 0:
+            raise DeepchecksValueError('alternative_scorers - expected to receive not empty dict of scorers!')
 
-        elif alternative_metrics is not None:
-            self.alternative_metrics = {}
+        elif alternative_scorers is not None:
+            self.alternative_scorers = {}
 
-            for name, metric in alternative_metrics.items():
-                if isinstance(metric, t.Callable):
-                    self.alternative_metrics[name] = metric
-                elif isinstance(metric, str):
-                    self.alternative_metrics[name] = get_scorer(metric)
+            for name, scorer in alternative_scorers.items():
+                if isinstance(scorer, t.Callable):
+                    self.alternative_scorers[name] = scorer
+                elif isinstance(scorer, str):
+                    self.alternative_scorers[name] = get_scorer(scorer)
                 else:
                     raise DeepchecksValueError(
-                        f"alternative_metrics - expected to receive 'Mapping[str, Callable]' but got "
-                        f"'Mapping[str, {type(metric).__name__}]'!"
+                        f"alternative_scorers - expected to receive 'Mapping[str, Callable]' but got "
+                        f"'Mapping[str, {type(scorer).__name__}]'!"
                     )
 
     def run(
@@ -80,7 +80,7 @@ class ClassPerformanceImbalanceCheck(SingleDatasetBaseCheck):
 
         Returns:
             CheckResult: value is a dictionary of labels, value of each key is another dictionary
-                with calculated metrics
+                with calculated scores
 
         Raises:
             DeepchecksValueError:
@@ -107,8 +107,8 @@ class ClassPerformanceImbalanceCheck(SingleDatasetBaseCheck):
         labels = t.cast(pd.Series, dataset.label_col())
         features = t.cast(pd.DataFrame, dataset.features_columns())
 
-        if self.alternative_metrics is not None:
-            df = pd.DataFrame.from_dict(self._execute_alternative_metrics(
+        if self.alternative_scorers is not None:
+            df = pd.DataFrame.from_dict(self._execute_alternative_scorers(
                 model, features, labels
             ))
         else:
@@ -116,8 +116,11 @@ class ClassPerformanceImbalanceCheck(SingleDatasetBaseCheck):
             y_pred = model.predict(features)
             unique_labels = get_unique_labels(labels, y_pred)
             df = pd.DataFrame.from_dict({
-                name: dict(zip(unique_labels, metric_func(y_true, y_pred, labels=unique_labels))) # type: ignore
-                for name, metric_func in self._get_default_metrics().items()
+                name: dict(zip(
+                    unique_labels,
+                    t.cast(t.Iterable, scorer_func(y_true, y_pred, labels=unique_labels))
+                ))
+                for name, scorer_func in self._get_default_scorers().items()
             })
 
         def display():
@@ -130,7 +133,7 @@ class ClassPerformanceImbalanceCheck(SingleDatasetBaseCheck):
             df.transpose().plot.bar(
                 title=title,
                 backend='matplotlib',
-                xlabel='Metrics',
+                xlabel='Score',
                 ylabel='Values'
             )
 
@@ -141,49 +144,50 @@ class ClassPerformanceImbalanceCheck(SingleDatasetBaseCheck):
             display=display
         )
 
-    def _get_default_metrics(self) -> t.Dict[str, t.Callable[..., np.ndarray]]:
+    def _get_default_scorers(self) -> t.Dict[str, t.Callable[..., np.ndarray]]:
         return {
             'Accuracy': partial(recall_score, zero_division=0, average=None),
             'Precision': partial(precision_score, zero_division=0, average=None),
             'Recall': partial(recall_score, zero_division=0, average=None)
         }
 
-    def _execute_alternative_metrics(
+    def _execute_alternative_scorers(
         self,
         model: t.Any,
         features: pd.DataFrame,
         labels: pd.Series,
     ) -> t.Dict[str, t.Dict[t.Hashable, t.Union[int, float]]]:
         check_name = type(self).__name__
-        metrics = t.cast(t.Mapping[str, MetricFunc], self.alternative_metrics)
+        scorers = t.cast(t.Mapping[str, ScorerFunc], self.alternative_scorers)
         result: t.Dict[str, t.Dict[t.Hashable, t.Union[int, float]]] = {}
 
-        for metric_name, metric_func in metrics.items():
-            metric_result = metric_func(model, features, labels)
+        for scorer_name, scorer_func in scorers.items():
+            scorer_result = scorer_func(model, features, labels)
 
-            if not isinstance(metric_result, dict):
+            if not isinstance(scorer_result, dict):
+                result_type = type(scorer_result).__name__
                 raise DeepchecksValueError(
-                    f'Check {check_name} expecting that alternative metrics will return '
-                    f"not empty instance of 'Mapping[Hashable, float|int]', but got {type(metric_result).__name__}" #pylint: disable=inconsistent-quotes
+                    f"Check {check_name} expecting that alternative scorer '{scorer_name}' will return "
+                    f"not empty instance of 'Mapping[Hashable, float|int]', but got {result_type}"
                 )
 
-            if len(metric_result) == 0:
+            if len(scorer_result) == 0:
                 raise DeepchecksValueError(
-                    f'Check {check_name} expecting that alternative metrics will return '
-                    "not empty instance of 'Mapping[Hashable, float|int]'" #pylint: disable=inconsistent-quotes
+                    f"Check {check_name} expecting that alternative scorer '{scorer_name}' will return "
+                    "not empty instance of 'Mapping[Hashable, float|int]'"
                 )
 
-            incorrect_values = [v for v in metric_result.values() if not isinstance(v, (int, float))]
+            incorrect_values = [v for v in scorer_result.values() if not isinstance(v, (int, float))]
 
             if len(incorrect_values) != 0:
                 value_type = type(incorrect_values[0]).__name__
                 raise DeepchecksValueError(
-                    f'Check {check_name} expecting that alternative metrics will return '
+                    f"Check {check_name} expecting that alternative scorer '{scorer_name}' will return "
                     "not empty instance of 'Mapping[Hashable, float|int]', "
-                    f"but got 'Mapping[Hashable, {value_type}]'" #pylint: disable=inconsistent-quotes
+                    f"but got 'Mapping[Hashable, {value_type}]'"
                 )
 
-            result[metric_name] = metric_result
+            result[scorer_name] = scorer_result
 
         return result
 
@@ -208,8 +212,8 @@ class ClassPerformanceImbalanceCheck(SingleDatasetBaseCheck):
 
             result = defaultdict(dict)
 
-            # For each metric calculate: (highest-class - lowest-class)/highest-class
-            for metric_name, classes_values in data.items():
+            # For each score calculate: (highest-class - lowest-class)/highest-class
+            for score_name, classes_values in data.items():
                 getval = lambda it: it[1]
                 lowest_class_name, min_value = min(classes_values.items(), key=getval)
                 highest_class_name, max_value = max(classes_values.items(), key=getval)
@@ -217,14 +221,14 @@ class ClassPerformanceImbalanceCheck(SingleDatasetBaseCheck):
                 relative_difference = abs((min_value - max_value) / max_value)
 
                 if relative_difference >= threshold:
-                    result[metric_name][(lowest_class_name, highest_class_name)] = relative_difference
+                    result[score_name][(lowest_class_name, highest_class_name)] = relative_difference
 
             if len(result) == 0:
                 return ConditionResult(True)
 
             details = '\n'.join([
-                f'Metric: {metric_name}, lowest class: {lowest_class_name}, highest class: {highest_class_name};'
-                for metric_name, difference in result.items()
+                f'Score: {score_name}, lowest class: {lowest_class_name}, highest class: {highest_class_name};'
+                for score_name, difference in result.items()
                 for ((lowest_class_name, highest_class_name), _) in difference.items()
             ])
 
