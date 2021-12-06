f27731e36e1b4daaac0a6e58f8619bd8f01d1df6
Yurii Romanyshyn <yurii.romanyshyn@starnavi.io>
2021-12-06 22:47:51 +0200

    fixes;

--- tests/checks/methodology/datasets_size_comparison.py
+++ tests/checks/methodology/datasets_size_comparison.py
@@ -1,7 +1,8 @@
-from hamcrest import assert_that, calling, raises, has_entries, instance_of
+import pandas as pd
+from hamcrest import assert_that, calling, raises, has_entries, instance_of, all_of
 
 from deepchecks import CheckResult, ConditionCategory
-from deepchecks.checks import TestDatasetSize
+from deepchecks.checks import DatasetsSizeComparison
 from deepchecks.errors import DeepchecksValueError
 
 from tests.checks.utils import equal_condition_result
@@ -10,71 +11,41 @@ from tests.checks.utils import equal_condition_result
 def test_test_dataset_size_check(iris_split_dataset_and_model):
     train, test, model = iris_split_dataset_and_model
 
-    check_result = TestDatasetSize().run(train, test, model)
+    check_result = DatasetsSizeComparison().run(train, test, model)
 
     assert_that(check_result, instance_of(CheckResult))
-    assert_that(check_result.value, instance_of(dict))
+    assert_that(check_result.value, instance_of(pd.DataFrame))
     assert_that(check_result.value, has_entries({
-        "train": instance_of(int),
-        "test": instance_of(int),
+        "train": all_of(
+            instance_of(pd.Series),
+            has_entries({"size": instance_of(int)})
+        ),
+        "test": all_of(
+            instance_of(pd.Series),
+            has_entries({"size": instance_of(int)})
+        )
     }))
 
 
 def test_run_test_dataset_size_check_with_empty_datasets(iris_split_dataset_and_model):
     train, test, model = iris_split_dataset_and_model
-    
+
     train._data = train._data[0:0]
     test._data = test._data[0:0]
 
     assert_that(
-        calling(TestDatasetSize().run).with_args(train, test, model),
-        raises(DeepchecksValueError, 'Check TestDatasetSize required a non-empty dataset')
-    )
-
-
-def test_run_test_dataset_size_check_without_shared_label(iris_split_dataset_and_model):
-    train, test, model = iris_split_dataset_and_model
-
-    test._data = test._data.rename(columns={'target': 'label-column'})
-    test._label_name = 'label-column'
-
-    assert_that(
-        calling(TestDatasetSize().run).with_args(train, test, model),
-        raises(DeepchecksValueError, 'Check TestDatasetSize requires datasets to share the same label')
-    )
-
-
-def test_run_test_dataset_size_check_without_shared_features(iris_split_dataset_and_model):
-    train, test, model = iris_split_dataset_and_model
-
-    test._data['new-feature'] = 0.5
-    test._features = test._features + ['new-feature']
-    
-    assert_that(
-        calling(TestDatasetSize().run).with_args(train, test, model),
-        raises(DeepchecksValueError, 'Check TestDatasetSize requires datasets to share the same features')
-    )
-
-
-def test_run_test_dataset_size_check_without_features_columns(iris_split_dataset_and_model):
-    train, test, model = iris_split_dataset_and_model
-
-    test._data = test._data.drop(columns=test._features)
-    test._features = []
-    
-    assert_that(
-        calling(TestDatasetSize().run).with_args(train, test, model),
-        raises(DeepchecksValueError, 'Check TestDatasetSize requires dataset to have a features columns!')
+        calling(DatasetsSizeComparison().run).with_args(train, test, model),
+        raises(DeepchecksValueError, 'Check DatasetsSizeComparison required a non-empty dataset')
     )
 
 
 def test_test_dataset_size_check_with_condition_that_should_pass(iris_split_dataset_and_model):
     train, test, model = iris_split_dataset_and_model
-    check = TestDatasetSize().add_condition_test_size_not_smaller_than(10)
-    
+    check = DatasetsSizeComparison().add_condition_test_size_not_smaller_than(10)
+
     check_result = check.run(train, test, model)
     condition_result, *_ = check.conditions_decision(check_result)
-    
+
     assert_that(actual=condition_result, matcher=equal_condition_result( # type: ignore
         is_pass=True,
         name='Test dataset size is not smaller than 10.',
@@ -85,8 +56,8 @@ def test_test_dataset_size_check_with_condition_that_should_pass(iris_split_data
 
 def test_test_dataset_size_check_with_condition_that_should_not_pass(iris_split_dataset_and_model):
     train, test, model = iris_split_dataset_and_model
-    check = TestDatasetSize().add_condition_test_size_not_smaller_than(10_000)
-    
+    check = DatasetsSizeComparison().add_condition_test_size_not_smaller_than(10_000)
+
     check_result = check.run(train, test, model)
     condition_result, *_ = check.conditions_decision(check_result)
 
@@ -100,11 +71,11 @@ def test_test_dataset_size_check_with_condition_that_should_not_pass(iris_split_
 
 def test_test_dataset_size_check_with_size_ratio_condition_that_should_pass(iris_split_dataset_and_model):
     train, test, model = iris_split_dataset_and_model
-    check = TestDatasetSize().add_condition_test_train_size_ratio_not_smaller_than(0.2)
-    
+    check = DatasetsSizeComparison().add_condition_test_train_size_ratio_not_smaller_than(0.2)
+
     check_result = check.run(train, test, model)
     condition_result, *_ = check.conditions_decision(check_result)
-    
+
     assert_that(actual=condition_result, matcher=equal_condition_result( # type: ignore
         is_pass=True,
         name='Test-Train size ratio is not smaller than 0.2.',
@@ -115,11 +86,11 @@ def test_test_dataset_size_check_with_size_ratio_condition_that_should_pass(iris
 
 def test_test_dataset_size_check_with_size_ratio_condition_that_should_not_pass(iris_split_dataset_and_model):
     train, test, model = iris_split_dataset_and_model
-    check = TestDatasetSize().add_condition_test_train_size_ratio_not_smaller_than(0.8)
-    
+    check = DatasetsSizeComparison().add_condition_test_train_size_ratio_not_smaller_than(0.8)
+
     check_result = check.run(train, test, model)
     condition_result, *_ = check.conditions_decision(check_result)
-    
+
     assert_that(actual=condition_result, matcher=equal_condition_result( # type: ignore
         is_pass=False,
         name='Test-Train size ratio is not smaller than 0.8.',
