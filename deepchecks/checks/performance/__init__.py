66e9c9f629aa56260fa04cc827eef97de03642f1
Itay Gabbay <itay@deepchecks.com>
2021-12-07 10:11:41 +0200

    Adding license to files

--- deepchecks/checks/performance/__init__.py
+++ deepchecks/checks/performance/__init__.py
@@ -1,3 +1,13 @@
+# ----------------------------------------------------------------------------
+# Copyright (C) 2021 Deepchecks (https://www.deepchecks.com)
+#
+# This file is part of Deepchecks.
+# Deepchecks is distributed under the terms of the GNU Affero General
+# Public License (version 3 or later).
+# You should have received a copy of the GNU Affero General Public License
+# along with Deepchecks.  If not, see <http://www.gnu.org/licenses/>.
+# ----------------------------------------------------------------------------
+#
 """Module contains checks of model performance metrics."""
 from .performance_report import *
 from .confusion_matrix_report import *
