188a6de23c789708e39eb984a03423bf6a93b52c
Itay Gabbay <itay@deepchecks.com>
2021-12-07 10:56:21 +0200

    License add (#224)
    * Create .licenserc_fix.yaml
    
    * Update lint.yml
    
    * Update lint.yml
    
    * Update lint.yml
    
    * Update lint.yml
    
    * Update lint.yml
    
    * Update lint.yml
    
    * Update lint.yml
    
    * Update .licenserc_fix.yaml
    
    * Adding license to all files
    
    * Adding license to all files
    
    * Adding license-check to makefile
    
    * Adding license-check to makefile
    
    * Revert "Adding license to all files"
    
    This reverts commit c3a3fb99a6398eb2a46baead192ea26c6c5f833d.
    
    * Revert "Adding license to all files"
    
    This reverts commit 0a25b9a273e1fb9fcd993bcbf20bf2f29a8e47f7.
    
    * Adding skip ignore to license check
    
    * Adding license to files
    
    * Adding license check to github-actions
    
    * Adding licenses

--- tests/checks/performance/segment_performance_test.py
+++ tests/checks/performance/segment_performance_test.py
@@ -1,3 +1,13 @@
+# ----------------------------------------------------------------------------
+# Copyright (C) 2021 Deepchecks (https://www.deepchecks.com)
+#
+# This file is part of Deepchecks.
+# Deepchecks is distributed under the terms of the GNU Affero General
+# Public License (version 3 or later).
+# You should have received a copy of the GNU Affero General Public License
+# along with Deepchecks.  If not, see <http://www.gnu.org/licenses/>.
+# ----------------------------------------------------------------------------
+#
 """Tests for segment performance check."""
 from hamcrest import assert_that, has_entries, close_to, has_property, equal_to, calling, raises
 
