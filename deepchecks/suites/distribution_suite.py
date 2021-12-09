5acc83409e0b3227ca286b59f8a98e1c9c4651af
Noam Bressler <noamzbr@gmail.com>
2021-12-09 18:40:49 +0200

    remove unused import

--- deepchecks/suites/distribution_suite.py
+++ deepchecks/suites/distribution_suite.py
@@ -11,7 +11,6 @@
 """The predefined Data Distribution suite module."""
 from deepchecks import Suite
 from deepchecks.checks.distribution import TrainTestDrift
-from deepchecks.checks.distribution import TrustScoreComparison
 
 
 def data_distribution_suite() -> Suite:
