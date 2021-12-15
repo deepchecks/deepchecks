d3648da4b0c2cde220ae85356ebc86654d18da87
Jonthan Liberman <jonthanliberman@ip-192-168-1-17.eu-west-1.compute.internal>
2021-12-15 17:36:38 +0200

    v1

--- deepchecks/utils/model.py
+++ deepchecks/utils/model.py
@@ -10,7 +10,7 @@ __all__ = ['get_model_of_pipeline']
 
 
 def get_model_of_pipeline(model: Union[Pipeline, BaseEstimator]):
-    """return the model of a given Pipeline or itself if a BaseEstimator is given.
+    """Return the model of a given Pipeline or itself if a BaseEstimator is given.
 
     Args:
         model (Union[Pipeline, BaseEstimator]): a Pipeline or a BaseEstimator model
