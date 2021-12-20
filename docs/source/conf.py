35ed67790a825d062d11f5be6303656ee45251d9
Yurii Romanyshyn <yurii.romanyshyn@starnavi.io>
2021-12-20 12:09:45 +0200

    removed redundant code

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,11 +13,6 @@ import sys
 import pathlib
 import functools
 from subprocess import check_output
-# from sphinx.ext.autosummary import Autosummary
-# from sphinx.ext.autosummary import get_documenter
-# from docutils.parsers.rst import directives
-# from sphinx.util.inspect import safe_getattr
-# import re
 
 
 CURRENT_DIR = pathlib.Path(__file__).parent
@@ -457,55 +452,6 @@ def get_check_example_api_reference(filepath: str) -> t.Optional[str]:
     apipath = f"/api/checks/generated/{check_clazz.__module__}.{notebook_name}"
     result = f"* :doc:`API Reference - {notebook_name} <{apipath}>`"
     return result
-
-
-
-# TODO: Not sure if we need this
-
-# class AutoAutoSummary(Autosummary):
-
-#     option_spec = {
-#         'methods': directives.unchanged,
-#         'attributes': directives.unchanged,
-#         'classes': directives.unchanged
-#     }
-
-#     required_arguments = 1
-
-#     @staticmethod
-#     def get_members(obj, typ, include_public=None):
-#         if not include_public:
-#             include_public = []
-#         items = []
-#         for name in dir(obj):
-#             try:
-#                 documenter = get_documenter(safe_getattr(obj, name), obj)
-#             except AttributeError:
-#                 continue
-#             if documenter.objtype == typ:
-#                 items.append(name)
-#         public = [x for x in items if x in include_public or not x.startswith('_')]
-#         return public, items
-
-#     def run(self):
-#         clazz = str(self.arguments[0])
-#         try:
-#             (module_name, class_name) = clazz.rsplit('.', 1)
-#             m = __import__(module_name, globals(), locals(), [class_name])
-#             c = getattr(m, class_name)
-#             if 'methods' in self.options:
-#                 _, methods = self.get_members(c, 'method', ['__init__'])
-
-#                 self.content = ["~%s.%s" % (clazz, method) for method in methods if not method.startswith('_')]
-#             if 'attributes' in self.options:
-#                 _, attribs = self.get_members(c, 'attribute')
-#                 self.content = ["~%s.%s" % (clazz, attrib) for attrib in attribs if not attrib.startswith('_')]
-#             if 'classes' in self.options:
-#                 _, classes = self.get_members(c, 'class')
-#                 self.content = ["~%s.%s" % (clazz, cls) for cls in classes if not cls.startswith('_')]
-
-#         finally:
-#             return super(AutoAutoSummary, self).run()
 
 
 # -- Registration of hooks ---------
