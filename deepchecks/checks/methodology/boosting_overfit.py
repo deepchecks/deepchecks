ceba2ede21ffee18180dd683b9ac4dbe441c380e
matanper <matanper@gmail.com>
2021-12-21 15:42:21 +0200

    Update utils.metric to delete duplicated code (#290)
    * Update utils.metric to delete duplicated code
    
    * Fix class performance display bug
    
    Co-authored-by: Matan Perlmutter <matan@deepchecks.com>

--- deepchecks/checks/methodology/boosting_overfit.py
+++ deepchecks/checks/methodology/boosting_overfit.py
@@ -18,8 +18,8 @@ from matplotlib.ticker import MaxNLocator
 import numpy as np
 
 from deepchecks import Dataset, CheckResult, TrainTestBaseCheck, ConditionResult
-from deepchecks.utils.metrics import task_type_check, DEFAULT_SCORERS_DICT, validate_scorer, DEFAULT_SINGLE_SCORER, \
-    calculate_scorer_with_nulls
+from deepchecks.utils.metrics import initialize_single_scorer, get_scorer_single
+
 from deepchecks.utils.strings import format_percent
 from deepchecks.utils.validation import validate_model
 from deepchecks.utils.model import get_model_of_pipeline
@@ -97,7 +97,7 @@ class PartialBoostingModel:
 
 def partial_score(scorer, dataset, model, step):
     partial_model = PartialBoostingModel(model, step)
-    return calculate_scorer_with_nulls(partial_model, dataset, scorer)
+    return scorer(partial_model, dataset)
 
 
 def calculate_steps(num_steps, num_estimators):
@@ -133,7 +133,7 @@ class BoostingOverfit(TrainTestBaseCheck):
 
     def __init__(self, scorer: Union[Callable, str] = None, scorer_name: str = None, num_steps: int = 20):
         super().__init__()
-        self.scorer = scorer
+        self.scorer = initialize_single_scorer(scorer)
         self.scorer_name = scorer_name
         self.num_steps = num_steps
 
@@ -165,16 +165,7 @@ class BoostingOverfit(TrainTestBaseCheck):
         validate_model(train_dataset, model)
 
         # Get default scorer
-        model_type = task_type_check(model, train_dataset)
-        if self.scorer is not None:
-            scorer = validate_scorer(self.scorer, model, train_dataset)
-            # TODO:
-            # if I understood it right 'scorer_name' var is here to give user ability
-            # to name his scorer function, but it is never used if 'scorer' is instance of callable
-            scorer_name = self.scorer_name or self.scorer if isinstance(self.scorer, str) else 'User score'
-        else:
-            scorer_name = DEFAULT_SINGLE_SCORER[model_type]
-            scorer = DEFAULT_SCORERS_DICT[model_type][scorer_name]
+        scorer_name, scorer = get_scorer_single(model, train_dataset, self.scorer)
 
         # Get number of estimators on model
         num_estimators = PartialBoostingModel.n_estimators(model)
