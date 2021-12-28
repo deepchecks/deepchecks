2cef59f511a2bb1cc0e3488ab6de0a81f6ad07e8
DBI <danb@deepchecks.com>
2021-12-28 11:04:45 +0200

    fix docstring

--- deepchecks/base/dataset.py
+++ deepchecks/base/dataset.py
@@ -383,32 +383,28 @@ class Dataset:
         return self.n_samples
 
     def train_test_split(self,
-                         train_size: t.Union[int, float] = None,
+                         train_size: t.Union[int, float] = 0.75,
                          test_size: t.Union[int, float] = None,
                          random_state: int = 42,
                          shuffle: bool = True) -> t.Tuple[TDataset, TDataset]:
-        """Split dataset into random train and test.
+        """Split dataset into random train and test datasets.
 
         Args:
             train_size (float or int):
-                If float, should be between 0.0 and 1.0 and represent the proportion
-                of the dataset to include in the test split. If int, represents the
-                absolute number of test samples. If None, the value is set to the
-                complement of the train size. If ``train_size`` is also None, it will
-                be set to 0.25.
-            train_size (float or int):
-                If float, should be between 0.0 and 1.0 and represent the
-                proportion of the dataset to include in the train split. If
-                int, represents the absolute number of train samples. If None,
-                the value is automatically set to the complement of the test size.
+                If float, should be between 0.0 and 1.0 and represent the proportion of the dataset to include in
+                the train split. If int, represents the absolute number of train samples. (default = 0.75)
+            test_size (float or int):
+                If float, should be between 0.0 and 1.0 and represent the proportion of the dataset to include in the
+                test split. If int, represents the absolute number of test samples. If None, the value is
+                automatically set to the complement of the train size. (default = None)
             random_state (int):
                 The random state to use for shuffling. (default=42)
             shuffle (bool):
                 Whether or not to shuffle the data before splitting. (default=True)
 
         Returns:
-            Train Dataset
-            Test Dataset
+            (Dataset) Dataset containing train split data.
+            (Dataset) Dataset containing test split data.
         """
         train_df, test_df = train_test_split(self._data,
                                              test_size=test_size,
