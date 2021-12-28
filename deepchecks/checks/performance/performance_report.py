1c752c87fd51f0a9cba2117babc684622f4804f5
JKL98ISR <jonatan.lib@gmail.com>
2021-12-28 12:26:13 +0200

    v0

--- deepchecks/checks/performance/performance_report.py
+++ deepchecks/checks/performance/performance_report.py
@@ -14,15 +14,15 @@ import pandas as pd
 import plotly.express as px
 
 from deepchecks.base.check import ModelComparisonContext
-from deepchecks import CheckResult, Dataset, SingleDatasetBaseCheck, ConditionResult, ModelComparisonBaseCheck
-from deepchecks.utils.metrics import get_scorers_list, initialize_multi_scorers, ModelType
+from deepchecks import CheckResult, Dataset, TrainTestBaseCheck, ConditionResult, ModelComparisonBaseCheck
+from deepchecks.utils.metrics import get_scorers_list, initialize_multi_scorers, ModelType, task_type_check
 from deepchecks.utils.validation import validate_model
 
 
 __all__ = ['PerformanceReport', 'MultiModelPerformanceReport']
 
 
-class PerformanceReport(SingleDatasetBaseCheck):
+class PerformanceReport(TrainTestBaseCheck):
     """Summarize given scores on a dataset and model.
 
     Args:
@@ -35,7 +35,7 @@ class PerformanceReport(SingleDatasetBaseCheck):
         super().__init__()
         self.alternative_scorers = initialize_multi_scorers(alternative_scorers)
 
-    def run(self, dataset, model=None) -> CheckResult:
+    def run(self, train_dataset: Dataset, test_dataset: Dataset, model=None) -> CheckResult:
         """Run check.
 
         Args:
@@ -45,24 +45,53 @@ class PerformanceReport(SingleDatasetBaseCheck):
         Returns:
             CheckResult: value is dictionary in format 'score-name': score-value
         """
-        return self._performance_report(dataset, model)
+        return self._performance_report(train_dataset, test_dataset, model)
 
-    def _performance_report(self, dataset: Dataset, model):
-        Dataset.validate_dataset(dataset)
-        dataset.validate_label()
-        validate_model(dataset, model)
+    def _performance_report(self, train_dataset: Dataset, test_dataset: Dataset, model):
+        Dataset.validate_dataset(train_dataset)
+        Dataset.validate_dataset(test_dataset)
+        train_dataset.validate_label()
+        test_dataset.validate_label()
+        train_dataset.validate_shared_label(test_dataset)
+        train_dataset.validate_shared_features(test_dataset)
+        validate_model(test_dataset, model)
+
+        task_type = task_type_check(model, train_dataset)
 
         # Get default scorers if no alternative, or validate alternatives
-        scorers = get_scorers_list(model, dataset, self.alternative_scorers)
-        scores = {
-            scorer.name: scorer(model, dataset)
-            for scorer in scorers
-        }
+        scorers = get_scorers_list(model, test_dataset, self.alternative_scorers, multiclass_avg=False)
+        datasets = {'Train': train_dataset, 'Test': test_dataset}
+        if task_type == ModelType.MULTICLASS:
+            results = []
+            for dataset in datasets.keys():
+                for scorer in scorers:
+                    score_result = scorer(model, datasets[dataset])
+                    # Multiclass scorers return numpy array of result per class
+                    for class_i, value in enumerate(score_result):
+                        if scorer.is_negative_scorer():
+                            value = -value
+                        results.append([dataset, value, scorer.name, class_i])
+            results_df = pd.DataFrame(results, columns=['Dataset', 'Value', 'Metric', 'Class'])
 
-        display_df = pd.DataFrame(scores.values(), columns=['Value'], index=scores.keys())
-        display_df.index.name = 'Score'
+        else:
+            results = []
+            for scorer in scorers:
+                score_result = scorer(model, test_dataset)
+                if scorer.is_negative_scorer():
+                    score_result = -score_result
+                results.append([score_result, scorer.name])
 
-        return CheckResult(scores, header='Performance Report', display=display_df)
+            results_df = pd.DataFrame(results, columns=['Value', 'Metric'])
+
+        fig = px.bar(results_df, x=['Class', 'Dataset'], y='Value', color='Dataset', barmode='group',
+                        facet_col='Metric', facet_col_spacing=0.05)
+        fig.update_xaxes(title=None, tickprefix='Class ', tickangle=60)
+        fig.update_yaxes(title=None, matches=None)
+        fig.for_each_annotation(lambda a: a.update(text=a.text.split('=')[-1]))
+        fig.for_each_yaxis(lambda yaxis: yaxis.update(showticklabels=True))
+
+
+        return CheckResult(results_df, header='Performance Report', display=fig)
 
     def add_condition_score_not_less_than(self, min_score: float):
         """Add condition - metric scores are not less than given score.
