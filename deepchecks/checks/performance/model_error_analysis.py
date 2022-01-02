cd8a1c8f41a078d65bcc981be2878117c23c6f22
Noam Bressler <noamzbr@gmail.com>
2022-01-02 16:29:46 +0200

    lints

--- deepchecks/checks/performance/model_error_analysis.py
+++ deepchecks/checks/performance/model_error_analysis.py
@@ -62,15 +62,21 @@ class ModelErrorAnalysis(TrainTestBaseCheck):
     `See scorers documentation <https://scikit-learn.org/stable/modules/model_evaluation.html#scoring>`_
     A scorer is a function which accepts (model, X, y_true) and returns a float result which is the score.
     For every scorer higher scores are better than lower scores.
+
     You can create a scorer out of existing sklearn metrics:
     .. code-block:: python
         from sklearn.metrics import roc_auc_score, make_scorer
         auc_scorer = make_scorer(roc_auc_score)
+
     Or you can implement your own:
     .. code-block:: python
         from sklearn.metrics import make_scorer
+
+
         def my_mse(y_true, y_pred):
             return (y_true - y_pred) ** 2
+
+
         # Mark greater_is_better=False, since scorers always suppose to return
         # value to maximize.
         my_mse_scorer = make_scorer(my_mse, greater_is_better=False)
