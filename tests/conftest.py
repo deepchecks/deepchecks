ab89dea5542a5289edea48ddcf2fc9ab3aaeff34
JKL98ISR <jonatan.lib@gmail.com>
2022-01-09 12:46:20 +0200

    refactor_check_py

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,7 +23,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.tree import DecisionTreeClassifier
 
-from deepchecks.base.dataset import Dataset
+from deepchecks.base import Dataset
 
 
 @pytest.fixture(scope='session')
