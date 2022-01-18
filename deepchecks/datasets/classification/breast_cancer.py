8c227f269820b75a099fc202f7b31dabedf120ff
Noam Bressler <noamzbr@gmail.com>
2022-01-18 11:16:13 +0200

    fix more pylint

--- deepchecks/datasets/classification/breast_cancer.py
+++ deepchecks/datasets/classification/breast_cancer.py
@@ -36,32 +36,32 @@ def load_data(data_format: str = 'Dataset', as_train_test: bool = True) -> \
 
     This is a copy of UCI ML Breast Cancer Wisconsin (Diagnostic) datasets. https://goo.gl/U2Uwz2
 
-    Features are computed from a digitized image of a fine needle aspirate (FNA) of a breast mass. They describe 
-    characteristics of the cell nuclei present in the image. 
+    Features are computed from a digitized image of a fine needle aspirate (FNA) of a breast mass. They describe
+    characteristics of the cell nuclei present in the image.
 
-    Separating plane described above was obtained using Multisurface Method-Tree (MSM-T) [K. P. Bennett, 
-    “Decision Tree Construction Via Linear Programming.” Proceedings of the 4th Midwest Artificial Intelligence and 
-    Cognitive Science Society, pp. 97-101, 1992], a classification method which uses linear programming to construct 
-    a decision tree. Relevant features were selected using an exhaustive search in the space of 1-4 features and 1-3 
-    separating planes. 
+    Separating plane described above was obtained using Multisurface Method-Tree (MSM-T) [K. P. Bennett,
+    “Decision Tree Construction Via Linear Programming.” Proceedings of the 4th Midwest Artificial Intelligence and
+    Cognitive Science Society, pp. 97-101, 1992], a classification method which uses linear programming to construct
+    a decision tree. Relevant features were selected using an exhaustive search in the space of 1-4 features and 1-3
+    separating planes.
 
     The actual linear program used to obtain the separating plane in the 3-dimensional space is that described in: [
-    K. P. Bennett and O. L. Mangasarian: “Robust Linear Programming Discrimination of Two Linearly Inseparable Sets”, 
-    Optimization Methods and Software 1, 1992, 23-34]. 
+    K. P. Bennett and O. L. Mangasarian: “Robust Linear Programming Discrimination of Two Linearly Inseparable Sets”,
+    Optimization Methods and Software 1, 1992, 23-34].
 
     This database is also available through the UW CS ftp server:
 
     ftp ftp.cs.wisc.edu cd math-prog/cpo-dataset/machine-learn/WDBC/
 
-    References: 
-        * W.N. Street, W.H. Wolberg and O.L. Mangasarian. Nuclear feature extraction for breast tumor 
-          diagnosis. IS&T/SPIE 1993 International Symposium on Electronic Imaging: Science and Technology, volume 1905, 
-          pages 861-870, San Jose, CA, 1993. 
-        * O.L. Mangasarian, W.N. Street and W.H. Wolberg. Breast cancer diagnosis and 
-          prognosis via linear programming. Operations Research, 43(4), pages 570-577, July-August 1995. 
-        * W.H. Wolberg, 
-          W.N. Street, and O.L. Mangasarian. Machine learning techniques to diagnose breast cancer from fine-needle 
-          aspirates. Cancer Letters 77 (1994) 163-171. 
+    References:
+        * W.N. Street, W.H. Wolberg and O.L. Mangasarian. Nuclear feature extraction for breast tumor
+          diagnosis. IS&T/SPIE 1993 International Symposium on Electronic Imaging: Science and Technology, volume 1905,
+          pages 861-870, San Jose, CA, 1993.
+        * O.L. Mangasarian, W.N. Street and W.H. Wolberg. Breast cancer diagnosis and
+          prognosis via linear programming. Operations Research, 43(4), pages 570-577, July-August 1995.
+        * W.H. Wolberg,
+          W.N. Street, and O.L. Mangasarian. Machine learning techniques to diagnose breast cancer from fine-needle
+          aspirates. Cancer Letters 77 (1994) 163-171.
 
     The typical ML task in this dataset is to build a model that classifies between benign and malignant samples.
 
