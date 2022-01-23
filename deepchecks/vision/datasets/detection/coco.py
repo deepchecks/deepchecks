8df094f55a75b0bd1c2b65d34ee9f1c18381e62e
Noam Bressler <noamzbr@gmail.com>
2022-01-23 15:37:55 +0200

    replace coco dataloader

--- deepchecks/vision/datasets/detection/coco.py
+++ deepchecks/vision/datasets/detection/coco.py
@@ -1,15 +1,16 @@
 """Module for loading a sample of the COOC dataset and the yolov5s model."""
-import numpy as np
-import torch
-from torch.utils.data import DataLoader
-from torchvision.datasets.utils import download_and_extract_archive
-from torchvision.datasets import VisionDataset
-from torchvision.transforms import transforms
 import glob
 import os
-from numpy import genfromtxt
+from pathlib import Path
+from typing import List, Any, Tuple, Optional, Callable
+
+import numpy as np
+import torch
 from PIL import Image
-from typing import List, Any, Tuple
+from numpy import genfromtxt
+from torch.utils.data import DataLoader
+from torchvision.datasets import VisionDataset
+from torchvision.datasets.utils import download_and_extract_archive
 
 
 def get_trained_yolov5_object_detection():
@@ -22,39 +23,60 @@ def get_trained_yolov5_object_detection():
 
 class CocoDataset(VisionDataset):
 
-    def __init__(self, path):
-        super().__init__(path)
-        self.file_path = os.path.join(path, 'coco128/images/train2017')
-        self.label_path = os.path.join(path, 'coco128/labels/train2017')
-        self.files = sorted(glob.glob(os.path.join(self.file_path, '*.jpg')))
-        self.labels = sorted(glob.glob(os.path.join(self.label_path, '*.txt')))
-        self.ids = list(range(len(self.files)))
+    def __init__(
+        self,
+        root: str,
+        name: str,
+        transform: Optional[Callable] = None,
+        target_transform: Optional[Callable] = None,
+        transforms: Optional[Callable] = None,
+    ) -> None:
+        super(CocoDataset, self).__init__(root, transforms, transform, target_transform)
+        images_dir = Path(root) / 'images' / name
+        labels_dir = Path(root) / 'labels' / name
+        self.images = [n for n in images_dir.iterdir()]
+        self.labels = []
+        for image in self.images:
+            base, _ = os.path.splitext(os.path.basename(image))
+            label = labels_dir / f'{base}.txt'
+            self.labels.append(label if label.exists() else None)
 
-        self._transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Resize((480, 640)),
-            transforms.ToPILImage()
-        ])
+    def __getitem__(self, idx: int) -> Tuple[Any, Any]:
+        img = Image.open(self.images[idx]).convert('RGB')
 
-    def _load_image(self, idx: int) -> Image.Image:
-        return Image.open(self.files[idx]).convert("RGB")
+        label_file = self.labels[idx]
+        if label_file is not None:  # found
+            with open(label_file, 'r') as f:
+                labels = [x.split() for x in f.read().strip().splitlines()]
+                labels = np.array(labels, dtype=np.float32)
+        else:  # missing
+            labels = np.zeros((0, 5), dtype=np.float32)
 
-    def _load_target(self, idx: int) -> List[Any]:
-        return genfromtxt(self.labels[idx])
+        boxes = []
+        classes = []
+        for i in range(len(labels)):
+            x, y, w, h = labels[i, 1:]
+            labels[i, 1:] = np.array([
+                (x - w / 2) * img.width,
+                (y - h / 2) * img.height,
+                w * img.width,
+                h * img.height])
 
-    def __getitem__(self, index: int) -> Tuple[Any, Any]:
-        idx = self.ids[index]
-        image = self._load_image(idx)
-        target = self._load_target(idx)
+        if self.transforms is not None:
+            img, labels = self.transforms(img, labels)
 
-        return torch.tensor(np.array(self._transform(image))).to('cpu'), target.reshape(-1, 5)
+        return img, labels
 
-    def __len__(self):
-        return len(self.ids)
+    def __len__(self) -> int:
+        return len(self.images)
 
 
 def get_coco_dataloader():
     data_url = 'https://ultralytics.com/assets/coco128.zip'
     download_and_extract_archive(data_url, './', './coco128')
-    return DataLoader(CocoDataset('./coco128/'), batch_size=1)
+    dataset = CocoDataset(os.path.join('coco128', 'coco128'), 'train2017')
 
+    dataloader = DataLoader(dataset, batch_size=64, shuffle=True,
+                            collate_fn=lambda batch: tuple(zip(*batch)))
+
+    return dataloader
