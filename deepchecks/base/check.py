82b447e96778f8e0ddbe27c14270afea41e7b94d
DBI <42312361+benisraeldan@users.noreply.github.com>
2022-01-25 23:56:25 +0200

    add check display tests (#735)
    * add check display tests
    
    * dataset functionality removed
    
    * fix IPython requirements
    
    Co-authored-by: Itay Gabbay <itay@deepchecks.com>

--- deepchecks/base/check.py
+++ deepchecks/base/check.py
@@ -27,7 +27,7 @@ import pandas as pd
 import numpy as np
 import ipywidgets as widgets
 import plotly.graph_objects as go
-from IPython.core.display import display_html
+from IPython.display import display_html
 from pandas.io.formats.style import Styler
 from plotly.basedatatypes import BaseFigure
 import plotly
@@ -110,7 +110,7 @@ class CheckResult:
                 raise DeepchecksValueError(f'Can\'t display item of type: {type(item)}')
 
     def display_check(self, unique_id: str = None, as_widget: bool = False,
-                      show_additional_outputs=True):  # pragma: no cover
+                      show_additional_outputs=True):
         """Display the check result or return the display as widget.
 
         Parameters
