82f2dddd0e52e171566de5bafdfc68b417eac39f
Matan Perlmutter <matan@deepchecks.com>
2022-01-25 16:19:10 +0200

    Warn user when trying to show result in a non-interactive shell

--- deepchecks/base/check.py
+++ deepchecks/base/check.py
@@ -15,6 +15,7 @@ import base64
 import inspect
 import io
 import traceback
+import warnings
 from collections import OrderedDict
 from functools import wraps
 from typing import Any, Callable, List, Union, Dict, Mapping
@@ -36,7 +37,7 @@ from deepchecks.base.condition import Condition, ConditionCategory, ConditionRes
 from deepchecks.base.dataset import Dataset
 from deepchecks.base.display_pandas import dataframe_to_html, get_conditions_table
 from deepchecks.utils.strings import get_docs_summary, split_camel_case
-from deepchecks.utils.ipython import is_ipython_display
+from deepchecks.utils.ipython import is_ipython_display, is_notebook
 from deepchecks.errors import DeepchecksValueError, DeepchecksNotSupportedError
 
 
@@ -331,11 +332,12 @@ class CheckResult:
 
     def show(self, unique_id=None, show_additional_outputs=True):
         """Display check result."""
-        if is_ipython_display():
+        if is_notebook():
             self._ipython_display_(unique_id=unique_id,
                                    show_additional_outputs=show_additional_outputs)
         else:
-            print(self)
+            warnings.warn('You are running in a non-interactive python shell. in order to show result you have to use'
+                          'an IPython shell (etc Jupyter)')
 
 
 def wrap_run(func, class_instance):
