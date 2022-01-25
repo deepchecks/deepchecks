7cbdc4de977044dad215164d3aa92d7d955a514c
Itay Gabbay <itay@deepchecks.com>
2022-01-25 11:50:15 +0200

    Added validations to numpydoc docstrings ✅ (#738)

--- deepchecks/checks/performance/simple_model_comparison.py
+++ deepchecks/checks/performance/simple_model_comparison.py
@@ -99,6 +99,7 @@ class SimpleModelComparison(TrainTestBaseCheck):
             value is a Dict of: given_model_score, simple_model_score, ratio <br>
             ratio is given model / simple model (if the scorer returns negative values we divide 1 by it) <br>
             if ratio is infinite max_ratio is returned
+
         Raises
         ------
         DeepchecksValueError
