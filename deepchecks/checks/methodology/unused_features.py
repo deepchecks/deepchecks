5209c2e723dd5a75e72509e0315334d8add5efc3
matanper <matan@deepchecks.com>
2022-01-26 11:41:06 +0200

    Update deepchecks/checks/methodology/unused_features.py
    Co-authored-by: Itay Gabbay <itay@deepchecks.com>

--- deepchecks/checks/methodology/unused_features.py
+++ deepchecks/checks/methodology/unused_features.py
@@ -83,7 +83,7 @@ class UnusedFeatures(TrainTestBaseCheck):
             A scikit-learn-compatible fitted estimator instance
         feature_importance_force_permutation : bool , default None
             force calculation of permutation features importance
-        feature_importance_timeout : int , default None
+        feature_importance_timeout : int , default: None
             timeout in second for the permutation features importance calculation
 
         Returns
