60fb2de31597177df7451916e59e86c3d8bbb397
Itay Gabbay <itay@deepchecks.com>
2022-01-30 12:03:49 +0200

    Fixing license

--- tests/vision/utils/metrics_test.py
+++ tests/vision/utils/metrics_test.py
@@ -1,3 +1,13 @@
+# ----------------------------------------------------------------------------
+# Copyright (C) 2021 Deepchecks (https://www.deepchecks.com)
+#
+# This file is part of Deepchecks.
+# Deepchecks is distributed under the terms of the GNU Affero General
+# Public License (version 3 or later).
+# You should have received a copy of the GNU Affero General Public License
+# along with Deepchecks.  If not, see <http://www.gnu.org/licenses/>.
+# ----------------------------------------------------------------------------
+#
 from hamcrest import equal_to, assert_that, calling, raises
 
 from deepchecks.vision.datasets.detection.coco import yolo_wrapper
