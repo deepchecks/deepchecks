e0be42374b9ff25404abeac17574e6e2cba8aedc
Noam Bressler <noamzbr@gmail.com>
2022-01-30 16:44:47 +0200

    add init to metrics_utils

---
+++ deepchecks/vision/metrics_utils/__init__.py
@@ -0,0 +1,14 @@
+"""Module containing metrics utils"""
+
+from .metrics import (
+    task_type_check,
+    get_scorers_list,
+    calculate_metrics
+)
+
+__all__ = [
+    'task_type_check',
+    'get_scorers_list',
+    'calculate_metrics',
+]
+
