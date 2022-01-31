6dc44dd7e8c3e22499ab970601b8cac002c59a46
Matan Perlmutter <matan@deepchecks.com>
2022-01-31 12:42:50 +0200

    Fix compile errors

--- deepchecks/vision/metrics_utils/__init__.py
+++ deepchecks/vision/metrics_utils/__init__.py
@@ -11,13 +11,11 @@
 """Module containing metrics utils."""
 
 from .metrics import (
-    task_type_check,
     get_scorers_list,
     calculate_metrics
 )
 
 __all__ = [
-    'task_type_check',
     'get_scorers_list',
     'calculate_metrics',
 ]
