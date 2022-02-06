4f1d04291cb336a5773cbb8fee418fa90b40fb5a
Itay Gabbay <itay@deepchecks.com>
2022-02-06 09:39:16 +0200

    Adding mean average recall

--- demos/performance_report.py
+++ demos/performance_report.py
@@ -8,7 +8,7 @@ from deepchecks.vision.base import SingleDatasetCheck
 class MeanAveragePrecisionReport(SingleDatasetCheck):
 
     def run_logic(self, context, dataset_type: str = 'train') -> CheckResult:
-        results = pd.DataFrame(columns=['Area size', 'mAP(COCO challenge)', 'AP_50', 'AP_75'])
+        results = pd.DataFrame(columns=['Area size', 'mAP(COCO challenge)', 'AP@.50', 'AP@.75'])
         results.loc[0] = ['All', 34.9, 55.7, 37.4]
         results.loc[1] = ['Small (area<32^2)', 15.6, 30.4, 17.9]
         results.loc[2] = ['Medium (32^2<area<96^2)', 38.7, 60.1, 42.7]
@@ -20,7 +20,7 @@ class MeanAveragePrecisionReport(SingleDatasetCheck):
         }
         df = pd.DataFrame.from_dict(data)
 
-        fig = px.line(df, x="IoU", y="AP", title='AP over increasing IoU thresholds')
+        fig = px.line(df, x="IoU", y="AP", title='Mean average precision over increasing IoU thresholds')
 
         return CheckResult(value=results, display=[results, fig])
 
@@ -30,4 +30,24 @@ class MeanAveragePrecisionReport(SingleDatasetCheck):
 
     def run(self, dataset, model=None) -> CheckResult:
         return self.run_logic(None)
+
+
+class MeanAverageRecallReport(SingleDatasetCheck):
+
+    def run_logic(self, context, dataset_type: str = 'train') -> CheckResult:
+        results = pd.DataFrame(columns=['Area size', 'AR@100', 'AR@1', 'AR@10'])
+        results.loc[0] = ['All', 45.9, 11.2, 26.4]
+        results.loc[1] = ['Small (area<32^2)', 25.0, 5.4, 12.6]
+        results.loc[2] = ['Medium (32^2<area<96^2)', 49.3, 12.4, 23.2]
+        results.loc[3] = ['Large (area<96^2)', 62.0, 20.4, 31.2]
+
+        return CheckResult(value=results, display=[results])
+
+    def __init__(self, prediction_formatter):
+        super().__init__()
+        self.prediction_formatter = prediction_formatter
+
+    def run(self, dataset, model=None) -> CheckResult:
+        return self.run_logic(None)
+
 
