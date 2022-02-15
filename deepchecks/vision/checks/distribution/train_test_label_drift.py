d4cfb9b70804ef1868f1e411b1891499c82be1de
Noam Bressler <noamzbr@gmail.com>
2022-02-15 14:57:05 +0200

    change also in vision

--- deepchecks/vision/checks/distribution/train_test_label_drift.py
+++ deepchecks/vision/checks/distribution/train_test_label_drift.py
@@ -458,9 +458,9 @@ def calc_drift_and_plot(train_distribution: dict, test_distribution: dict, plot_
         raise DeepchecksValueError(
             f'column_type must be one of ["numerical", "categorical"], instead got {column_type}')
 
-    fig = make_subplots(rows=2, cols=1, vertical_spacing=0.4, shared_yaxes=False, shared_xaxes=False,
+    fig = make_subplots(rows=2, cols=1, vertical_spacing=0.2, shared_yaxes=False, shared_xaxes=False,
                         row_heights=[0.1, 0.9],
-                        subplot_titles=['Drift Score - ' + scorer_name, plot_title])
+                        subplot_titles=['Drift Score - ' + scorer_name, 'Distribution Plot'])
 
     fig.add_traces(bar_traces, rows=[1] * len(bar_traces), cols=[1] * len(bar_traces))
     fig.add_traces(dist_traces, rows=[2] * len(dist_traces), cols=[1] * len(dist_traces))
@@ -475,7 +475,8 @@ def calc_drift_and_plot(train_distribution: dict, test_distribution: dict, plot_
             yanchor='top',
             y=0.6),
         width=700,
-        height=400
+        height=400,
+        title=plot_title
     )
 
     fig.update_layout(shared_layout)
