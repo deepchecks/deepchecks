8dcb67624af00dadd4d80e34134a148be0f43ad5
Nir Hutnik <92314933+nirhutnik@users.noreply.github.com>
2022-02-16 23:46:54 +0200

    Vision domain classifier issue 841 (#879)
    * Took out whole dataset logic to utils file
    
    * Added WholeDatasetDriftCheck for vision
    
    * Small fixes
    
    * Created tests
    
    * Updated notebook
    
    * Updated notebook
    
    * Fixed comments
    
    * Fixed comments
    
    * Fixed comments
    
    * Fixed comments
    
    * Fixed pylint
    
    * Fixed comments
    
    * Renamed check to ImageDatasetDrift
    Fixed comment in notebook

--- deepchecks/vision/checks/distribution/__init__.py
+++ deepchecks/vision/checks/distribution/__init__.py
@@ -10,7 +10,9 @@
 #
 """Module containing the distribution checks in the vision package."""
 from .train_test_label_drift import TrainTestLabelDrift
+from .image_dataset_drift import ImageDatasetDrift
 
 __all__ = [
     'TrainTestLabelDrift',
+    'ImageDatasetDrift',
 ]
