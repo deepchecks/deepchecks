195f30b8d848e82c751bb12e74ad7cf0cea8a830
Jonthan Liberman <jonthanliberman@Jonthans-MacBook-Pro.local>
2022-03-01 12:20:40 +0200

    v0

--- tests/vision/utils_tests/metrics_test.py
+++ tests/vision/utils_tests/metrics_test.py
@@ -11,37 +11,36 @@
 import torch
 from hamcrest import has_items, assert_that, has_length, close_to
 
-from deepchecks.vision.datasets.detection.coco import yolo_prediction_formatter
 from deepchecks.vision.metrics_utils.metrics import calculate_metrics
 from deepchecks.vision.metrics_utils.detection_precision_recall import AveragePrecision
 from deepchecks.vision.utils.detection_formatters import DetectionPredictionFormatter
 from deepchecks.vision import VisionData
 
 
-def test_default_ap_ignite_complient(coco_test_visiondata: VisionData, trained_yolov5_object_detection):
+def test_default_ap_ignite_complient(fake_coco_test_visiondata: VisionData, fake_trained_yolov5_object_detection, simple_formatter):
     res = calculate_metrics({'AveragePrecision': AveragePrecision()},
-                            coco_test_visiondata, trained_yolov5_object_detection,
-                            prediction_formatter=DetectionPredictionFormatter(yolo_prediction_formatter),
+                            fake_coco_test_visiondata, fake_trained_yolov5_object_detection,
+                            prediction_formatter=DetectionPredictionFormatter(simple_formatter),
                             device=torch.device('cpu'))
     assert_that(res.keys(), has_length(1))
     assert_that(res['AveragePrecision'], has_length(59))
 
 
-def test_ar_ignite_complient(coco_test_visiondata: VisionData, trained_yolov5_object_detection):
+def test_ar_ignite_complient(fake_coco_test_visiondata: VisionData, fake_trained_yolov5_object_detection, simple_formatter):
     res = calculate_metrics({'AveragePrecision': AveragePrecision(return_option=1)},
-                            coco_test_visiondata, trained_yolov5_object_detection,
-                            prediction_formatter=DetectionPredictionFormatter(yolo_prediction_formatter),
+                            fake_coco_test_visiondata, fake_trained_yolov5_object_detection,
+                            prediction_formatter=DetectionPredictionFormatter(simple_formatter),
                             device=torch.device('cpu'))
 
     assert_that(res.keys(), has_length(1))
     assert_that(res['AveragePrecision'], has_length(59))
 
 
-def test_equal_pycocotools(coco_test_visiondata: VisionData, trained_yolov5_object_detection):
+def test_equal_pycocotools(fake_coco_test_visiondata: VisionData, fake_trained_yolov5_object_detection, simple_formatter):
     metric = AveragePrecision(return_option=None)
-    for batch in coco_test_visiondata.get_data_loader():
-        label = coco_test_visiondata.label_formatter(batch)
-        prediction = DetectionPredictionFormatter(yolo_prediction_formatter)(batch, trained_yolov5_object_detection,
+    for batch in fake_coco_test_visiondata.get_data_loader():
+        label = fake_coco_test_visiondata.label_formatter(batch)
+        prediction = DetectionPredictionFormatter(simple_formatter)(batch, fake_trained_yolov5_object_detection,
                                                                              torch.device('cpu'))
         metric.update((prediction, label))
     res = metric.compute()[0]
--- tests/vision/vision_conftest.py
+++ tests/vision/vision_conftest.py
@@ -8,12 +8,15 @@
 # along with Deepchecks.  If not, see <http://www.gnu.org/licenses/>.
 # ----------------------------------------------------------------------------
 #
+import os
 import pathlib
+import pickle
 
 import pytest
 import torch
 from torch.utils.data import DataLoader, Dataset
-
+from deepchecks.vision.dataset import VisionData
+from deepchecks.vision.datasets.detection import coco
 from deepchecks.vision.datasets.detection.coco import (
     load_model as load_yolov5_model,
     load_dataset as load_coco_dataset
@@ -22,6 +25,7 @@ from deepchecks.vision.datasets.classification.mnist import (
     load_model as load_mnist_net_model,
     load_dataset as load_mnist_dataset
 )
+from deepchecks.vision.utils.detection_formatters import DetectionLabelFormatter
 from tests.vision.utils_tests.mnist_imgaug import mnist_dataset_imgaug
 
 # Fix bug with torch.hub path on windows
@@ -36,14 +40,28 @@ __all__ = ['mnist_data_loader_train',
            'mnist_dataset_test',
            'trained_mnist',
            'trained_yolov5_object_detection',
+           'fake_trained_yolov5_object_detection',
            'obj_detection_images',
            'coco_train_dataloader',
            'coco_train_visiondata',
+           'fake_coco_train_visiondata',
            'coco_test_dataloader',
            'coco_test_visiondata',
+           'fake_coco_test_visiondata',
            'two_tuples_dataloader',
+           'simple_formatter',
         ]
 
+
+def _batch_collate(batch):
+    imgs, labels = zip(*batch)
+    return list(imgs), list(labels)
+
+@pytest.fixture(scope='session')
+def simple_formatter():
+    def format(batch, model, _):
+        return model(batch[0])
+    return format
 
 @pytest.fixture(scope='session')
 def mnist_data_loader_train():
@@ -84,6 +102,18 @@ def mnist_dataset_train_imgaug():
 def trained_yolov5_object_detection():
     return load_yolov5_model()
 
+@pytest.fixture(scope='session')
+def fake_trained_yolov5_object_detection():
+    curr_path = os.path.dirname(os.path.abspath(__file__))
+    with open(os.path.join(curr_path, 'assets', 'detections_dict.pickle'), 'rb') as f:
+        det_dict = pickle.loads(f.read())
+    class FakeYolo():
+        def __call__(self, batch):
+            detections = []
+            for im_id in batch:
+                detections.append(det_dict[im_id])
+            return detections
+    return FakeYolo()
 
 @pytest.fixture(scope='session')
 def obj_detection_images():
@@ -95,7 +125,6 @@ def obj_detection_images():
 
     return uris
 
-
 @pytest.fixture(scope='session')
 def coco_train_dataloader():
     return load_coco_dataset(train=True, object_type='DataLoader')
@@ -105,16 +134,41 @@ def coco_train_dataloader():
 def coco_train_visiondata():
     return load_coco_dataset(train=True, object_type='VisionData')
 
+@pytest.fixture(scope='session')
+def fake_coco_train_visiondata():
+    train_dataset = load_coco_dataset(train=True, object_type='DataLoader').dataset
+    class fake_train_dataset(Dataset):
+        def __len__(self):
+            return 64
+
+        def __getitem__(self, idx):
+            return (f'train_{idx}', train_dataset[idx][1])
+    fake_train_dataloader = DataLoader(fake_train_dataset(), shuffle=False, batch_size=32, collate_fn=_batch_collate)
+    label_formatter = DetectionLabelFormatter(coco.yolo_label_formatter)
+    fake_train_visiondata = VisionData(fake_train_dataloader, label_formatter=label_formatter, num_classes=80, label_map=coco.LABEL_MAP)
+    return fake_train_visiondata
 
 @pytest.fixture(scope='session')
 def coco_test_dataloader():
     return load_coco_dataset(train=False, object_type='DataLoader')
 
-
 @pytest.fixture(scope='session')
 def coco_test_visiondata():
     return load_coco_dataset(train=False, object_type='VisionData')
 
+@pytest.fixture(scope='session')
+def fake_coco_test_visiondata():
+    test_dataset = load_coco_dataset(train=False, object_type='DataLoader').dataset
+    class fake_test_dataset(Dataset):
+        def __len__(self):
+            return 64
+
+        def __getitem__(self, idx):
+            return (f'test_{idx}', test_dataset[idx][1])
+    fake_test_dataloader = DataLoader(fake_test_dataset(), shuffle=False, batch_size=32, collate_fn=_batch_collate)
+    label_formatter = DetectionLabelFormatter(coco.yolo_label_formatter)
+    fake_test_visiondata = VisionData(fake_test_dataloader, label_formatter=label_formatter, num_classes=80, label_map=coco.LABEL_MAP)
+    return fake_test_visiondata
 
 @pytest.fixture(scope='session')
 def two_tuples_dataloader():
