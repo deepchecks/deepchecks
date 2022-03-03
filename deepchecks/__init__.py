37a0e7a7c356e52b830de6f04bd62d41ee1e0cca
Matan Perlmutter <matan@deepchecks.com>
2022-03-03 13:27:29 +0200

    Update to check semantic version

--- deepchecks/__init__.py
+++ deepchecks/__init__.py
@@ -15,6 +15,7 @@ import http.client
 import warnings
 import matplotlib
 import plotly.io as pio
+import semantic_version
 
 from deepchecks.utils.ipython import is_notebook
 from deepchecks.tabular import (
@@ -99,15 +100,24 @@ except:  # pylint: disable=bare-except
     # If version file can't be found, leave version empty
     __version__ = ''
 
+warnings.warn('Looks like you are using outdated version of deepchecks. consider upgrading using'
+              ' pip install -U deepchecks')
+
 # Check for latest version
 try:
-    if not os.environ.get('DEEPCHECKS_DISABLE_LATEST', False):
-        conn = http.client.HTTPConnection('api.deepchecks.com', timeout=3)
+    disable = os.environ.get('DEEPCHECKS_DISABLE_LATEST', 'false').lower() == 'true'
+    if not disable:
+        conn = http.client.HTTPSConnection('api.deepchecks.com', timeout=3)
         conn.request('GET', '/latest')
         response = conn.getresponse()
         latest_version = response.read().decode('utf-8')
-        if __version__ and __version__ != latest_version:
-            warnings.warn('Looks like you are using outdated version of deepchecks. consider upgrading using'
-                          ' pip install -U deepchecks')
+        if __version__:
+            def to_semantic_version(ver: str):
+                # Remove extra version arguments
+                return semantic_version.Version('.'.join(ver.split('.')[:3]))
+
+            if to_semantic_version(__version__) < to_semantic_version(latest_version):
+                warnings.warn('Looks like you are using outdated version of deepchecks. consider upgrading using'
+                              ' pip install -U deepchecks')
 except:  # pylint: disable=bare-except
     pass
