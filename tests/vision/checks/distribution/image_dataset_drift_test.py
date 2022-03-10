0ada41971872526ac228874953fcdf3d385bddd0
nirhutnik <nirhutnik@gmail.com>
2022-03-10 15:54:41 +0200

    Fixing tests

--- tests/vision/checks/distribution/image_dataset_drift_test.py
+++ tests/vision/checks/distribution/image_dataset_drift_test.py
@@ -19,8 +19,9 @@ from tests.vision.vision_conftest import *
 
 def add_brightness(img):
     reverse = 255 - img
-    addition_of_brightness = (reverse * 0.31).astype(int)
+    addition_of_brightness = (reverse * 0.08).astype(int)
     return img + addition_of_brightness
+
 
 def pil_drift_formatter(batch):
     return [add_brightness(np.array(img)) for img in batch[0]]
@@ -35,10 +36,11 @@ def test_no_drift_grayscale(mnist_dataset_train, device):
     result = check.run(train, test, random_state=42, device=device)
     # Assert
     assert_that(result.value, has_entries({
-        'domain_classifier_auc': close_to(0.494, 0.001),
+        'domain_classifier_auc': close_to(0.497, 0.001),
         'domain_classifier_drift_score': equal_to(0),
         'domain_classifier_feature_importance': has_entries({
-            'brightness': equal_to(1),
+            'brightness': equal_to(0),
+            'rms_contrast': equal_to(1),
             'aspect_ratio': equal_to(0),
             'area': equal_to(0),
             'normalized_red_mean': equal_to(0),
@@ -57,10 +59,11 @@ def test_drift_grayscale(mnist_dataset_train, mnist_dataset_test, device):
     result = check.run(train, test, random_state=42, device=device)
     # Assert
     assert_that(result.value, has_entries({
-        'domain_classifier_auc': close_to(0.509, 0.001),
-        'domain_classifier_drift_score': close_to(0.019, 0.001),
+        'domain_classifier_auc': close_to(0.505, 0.001),
+        'domain_classifier_drift_score': close_to(0.011, 0.001),
         'domain_classifier_feature_importance': has_entries({
-            'brightness': equal_to(1),
+            'brightness': equal_to(0),
+            'rms_contrast': equal_to(1),
             'aspect_ratio': equal_to(0),
             'area': equal_to(0),
             'normalized_red_mean': equal_to(0),
@@ -80,12 +83,13 @@ def test_no_drift_rgb(coco_train_dataloader, coco_test_dataloader, device):
     result = check.run(train, test, random_state=42, device=device)
     # Assert
     assert_that(result.value, has_entries({
-        'domain_classifier_auc': close_to(0.465, 0.001),
+        'domain_classifier_auc': close_to(0.486, 0.001),
         'domain_classifier_drift_score': equal_to(0),
         'domain_classifier_feature_importance': has_entries({
-            'brightness': close_to(0.761, 0.01),
-            'aspect_ratio': close_to(0.238, 0.01),
-            'area': close_to(0, 0.001),
+            'brightness': equal_to(1),
+            'rms_contrast': equal_to(0),
+            'aspect_ratio': equal_to(0),
+            'area': equal_to(0),
             'normalized_red_mean': equal_to(0),
             'normalized_green_mean': equal_to(0),
             'normalized_blue_mean': equal_to(0),
@@ -108,14 +112,15 @@ def test_with_drift_rgb(coco_train_dataloader, coco_test_dataloader, device):
     result = check.run(train, test, random_state=42, device=device)
     # Assert
     assert_that(result.value, has_entries({
-        'domain_classifier_auc': close_to(0.753, 0.001),
+        'domain_classifier_auc': close_to(1, 0.001),
         'domain_classifier_drift_score': close_to(0.507, 0.001),
         'domain_classifier_feature_importance': has_entries({
-            'brightness': close_to(0.95, 0.01),
+            'brightness': equal_to(1),
+            'rms_contrast': equal_to(0),
             'aspect_ratio': equal_to(0),
             'area': equal_to(0),
-            'normalized_red_mean': close_to(0, 0.01),
-            'normalized_green_mean': close_to(0.04, 0.01),
-            'normalized_blue_mean': close_to(0, 0.01),
+            'normalized_red_mean': equal_to(0),
+            'normalized_green_mean': equal_to(0),
+            'normalized_blue_mean': equal_to(0)
         })
     }))
