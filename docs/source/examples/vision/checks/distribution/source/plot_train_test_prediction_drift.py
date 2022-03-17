ba9371c465a3e6e034da2fd8b2586c5b5662d904
Itay Gabbay <itay@deepchecks.com>
2022-03-17 13:38:02 +0200

    fixing plot_train_test_prediction_drift.py

--- docs/source/examples/vision/checks/distribution/source/plot_train_test_prediction_drift.py
+++ docs/source/examples/vision/checks/distribution/source/plot_train_test_prediction_drift.py
@@ -134,13 +134,16 @@ import numpy as np
 
 np.random.seed(42)
 
+
 def collate_test(batch):
     modified_batch = []
     for item in batch:
         image, label = item
         if label == 0:
-            if np.random.randint(10) == 0:
+            if np.random.randint(5) == 0:
                 modified_batch.append(item)
+            else:
+                modified_batch.append((image, 1))
         else:
             modified_batch.append(item)
             
