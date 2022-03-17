1e82b54370fb42d057aa9d2f4d534666575972a3
JKL98ISR <jonatan.lib@gmail.com>
2022-03-17 15:53:33 +0200

    test_fix

--- tests/vision/checks/performance/class_performance_test.py
+++ tests/vision/checks/performance/class_performance_test.py
@@ -112,14 +112,14 @@ def test_coco_best(coco_train_visiondata, coco_test_visiondata, trained_yolov5_o
     assert_that(len(result.value), equal_to(8))
 
     first_row = result.value.loc[result.value['Metric'] == 'AP'].sort_values(by='Value', ascending=False).iloc[0]
-    assert_that(first_row['Value'], close_to(0.990854, 0.05))
+    assert_that(first_row['Value'], close_to(0.999, 0.001))
     assert_that(first_row['Number of samples'], equal_to(1))
-    assert_that(first_row['Class'], is_in([29]))
+    assert_that(first_row['Class'], is_in([28]))
 
     first_row = result.value.loc[result.value['Metric'] == 'AR'].sort_values(by='Value', ascending=False).iloc[0]
-    assert_that(first_row['Value'], close_to(0.990854, 0.05))
+    assert_that(first_row['Value'], close_to(1, 0.001))
     assert_that(first_row['Number of samples'], equal_to(1))
-    assert_that(first_row['Class'], is_in([29]))
+    assert_that(first_row['Class'], is_in([28]))
 
 
 def test_class_list(mnist_dataset_train, mnist_dataset_test, trained_mnist, device):
