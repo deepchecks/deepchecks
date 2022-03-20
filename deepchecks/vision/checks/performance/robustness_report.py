50982930367026ee89740ced3033fa765056b5ca
Yurii Romanyshyn <71635444+yromanyshyn@users.noreply.github.com>
2022-03-20 22:58:36 +0200

    [Issue-990] "numpy_to_html_image" improvement (#1043)
    * update
    
    * update
    
    * update
    
    Co-authored-by: matanper <matan@deepchecks.com>

--- deepchecks/vision/checks/performance/robustness_report.py
+++ deepchecks/vision/checks/performance/robustness_report.py
@@ -29,7 +29,7 @@ from deepchecks.vision.metrics_utils import calculate_metrics, metric_results_to
 from deepchecks.vision.utils.validation import set_seeds
 from deepchecks.vision.metrics_utils import get_scorers_list
 from deepchecks.utils.strings import format_percent, split_camel_case
-from deepchecks.vision.utils.image_functions import ImageInfo, numpy_to_html_image
+from deepchecks.vision.utils.image_functions import ImageInfo, prepare_thumbnail, draw_bboxes
 
 
 __all__ = ['RobustnessReport']
@@ -50,6 +50,8 @@ class RobustnessReport(SingleDatasetCheck):
         A list of augmentations to test on the data. If none are given default augmentations are used.
         Supported augmentations are of albumentations and imgaug.
     """
+
+    _THUMBNAIL_SIZE = (200, 200)
 
     def __init__(self,
                  alternative_metrics: Optional[Dict[str, Metric]] = None,
@@ -247,23 +249,44 @@ class RobustnessReport(SingleDatasetCheck):
                 figures.append('<hr style="background-color:#2a3f5f; height:5px">')
         return figures
 
-    def _create_example_figure(self, dataset: VisionData, images, aug_name):
-        html_classes = ''
-        html_base_images = ''
-        html_aug_images = ''
+    def _create_example_figure(self, dataset: VisionData, images, aug_name: str):
+        classes = []
+        base_images = []
+        aug_images = []
 
         for sample in images:
             base_image = sample[0]
             aug_image = sample[1]
             class_name = dataset.label_id_to_name(sample[2])
-            bboxes = sample[3] if len(sample) == 4 else (None, None)
+            classes.append(f'<h4>{class_name}</h4>')
 
-            html_base_images += f'<div class="item image-div">{numpy_to_html_image(base_image, labels=bboxes[0])}</div>'
-            html_aug_images += f'<div class="item image-div">{numpy_to_html_image(aug_image, labels=bboxes[1])}</div>'
-            html_classes += f'<h4 class="item class-div">{class_name}</h4>'
+            if len(sample) == 4:
+                base_image_bboxes, aug_image_bboxes = sample[3]
+                base_image = draw_bboxes(base_image, base_image_bboxes, copy_image=True, border_width=2)
+                aug_image = draw_bboxes(aug_image, aug_image_bboxes, copy_image=True, border_width=2)
 
-        return HTML_TEMPLATE.format(class_names=html_classes, base_images=html_base_images, aug_images=html_aug_images,
-                                    aug_name=aug_name)
+            base_images.append(prepare_thumbnail(
+                image=base_image,
+                size=self._THUMBNAIL_SIZE,
+                copy_image=False
+            ))
+            aug_images.append(prepare_thumbnail(
+                image=aug_image,
+                size=self._THUMBNAIL_SIZE,
+                copy_image=False
+            ))
+
+        classes = ''.join(classes)
+        base_images_thumbnails = ''.join(base_images)
+        aug_images_thumbnails = ''.join(aug_images)
+
+        return HTML_TEMPLATE.format(
+            aug_name=aug_name,
+            classes=classes,
+            n_of_images=len(base_images),
+            base_images=base_images_thumbnails,
+            aug_images=aug_images_thumbnails,
+        )
 
     def _create_performance_graph(self, base_scores: dict, augmented_scores: dict):
         metrics = sorted(list(base_scores.keys()))
@@ -369,53 +392,23 @@ def get_random_image_pairs_from_dataset(original_dataset: VisionData,
 
 
 HTML_TEMPLATE = """
-<style>
-    .container {{
+<h3><b>Augmentation "{aug_name}"</b></h3>
+<div
+    style="
         overflow-x: auto;
-        display: flex;
-        flex-direction: column;
-    }}
-
-    .row {{
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-    }}
-
-    .item {{
-      flex: 1;
-      min-width: 200px;
-      position: relative;
-      word-wrap: break-word;
-    }}
-
-    .image-div {{
-      min-height: 200px;
-    }}
-
-    .class-div {{
-      text-align: center;
-    }}
-
-    h4 {{
-        font-family: "Open Sans", verdana, arial, sans-serif;
-        color: #2a3f5f
-    }}
-
-</style>
-<h4><b>Augmentation "{aug_name}"</b></h4>
-<div class="container">
-    <div class="row">
-        <h4 class="item">Class</h4>
-        {class_names}
-    </div>
-    <div class="row">
-        <h4 class="item">Base Image</h4>
-        {base_images}
-    </div>
-    <div class="row">
-        <h4 class="item">Augmented Image</h4>
-        {aug_images}
-    </div>
+        display: grid;
+        grid-template-rows: auto 1fr 1fr;
+        grid-template-columns: auto repeat({n_of_images}, 1fr);
+        grid-gap: 1.5rem;
+        justify-items: center;
+        align-items: center;
+        padding: 2rem;
+        width: max-content;">
+    <h4>Class</h4>
+    {classes}
+    <h4>Base Image</h4>
+    {base_images}
+    <h4>Augmented Image</h4>
+    {aug_images}
 </div>
 """
