e4c266122ad68d416dbe523956dfbe2ea19997db
JKL98ISR <jonatan.lib@gmail.com>
2022-03-21 17:19:40 +0200

    shh

--- tests/vision/utils_tests/validation_test.py
+++ tests/vision/utils_tests/validation_test.py
@@ -18,17 +18,21 @@ from deepchecks.utils.ipython import is_headless
 FILE_NAME = 'deepchecks_formatted_image.jpg'
 
 def test_mnist_validation(mnist_dataset_train, mock_trained_mnist):
-    if is_headless():
-        validate_extractors(mnist_dataset_train, mock_trained_mnist)
-        assert_that(os.path.exists(FILE_NAME), True)
-        assert_that(os.path.isfile(FILE_NAME), True)
+    # if is_headless():
+    validate_extractors(mnist_dataset_train, mock_trained_mnist)
+    assert_that(os.path.exists(FILE_NAME), True)
+    assert_that(os.path.isfile(FILE_NAME), True)
 
 
 def test_mnist_validation_no_save(mnist_dataset_train, mock_trained_mnist):
-    if is_headless():
-        validate_extractors(mnist_dataset_train, mock_trained_mnist, save_images=False)
-        assert_that(os.path.exists(FILE_NAME), False)
+    # if is_headless():
+    validate_extractors(mnist_dataset_train, mock_trained_mnist, save_images=False)
+    assert_that(os.path.exists(FILE_NAME), False)
 
+def test_mnist_validation_new_loc_save(mnist_dataset_train, mock_trained_mnist):
+    # if is_headless():
+    validate_extractors(mnist_dataset_train, mock_trained_mnist, 'tmp')
+    assert_that(os.path.exists('tmp/' + FILE_NAME), False)
 
 def test_coco_validation(coco_test_visiondata, mock_trained_yolov5_object_detection):
     if is_headless():
