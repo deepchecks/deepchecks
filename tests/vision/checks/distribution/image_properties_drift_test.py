cee44c9c111fb0db835c7c4d5fa5169f8cd80b0a
Noam Bressler <noamzbr@gmail.com>
2022-03-21 17:07:19 +0200

    improve tests

--- tests/vision/checks/distribution/image_properties_drift_test.py
+++ tests/vision/checks/distribution/image_properties_drift_test.py
@@ -20,7 +20,7 @@ from hamcrest import (
     has_length,
     contains_exactly,
     greater_than,
-    equal_to, has_key
+    equal_to, has_key, has_entries, close_to
 )
 
 from deepchecks.core import CheckResult
@@ -33,16 +33,32 @@ from deepchecks.vision.datasets.detection import coco
 def test_image_property_drift_check(device):
     train_dataset = coco.load_dataset(train=True, object_type='VisionData')
     test_dataset = coco.load_dataset(train=False, object_type='VisionData')
+
+    # Run
     result = ImagePropertyDrift().run(train_dataset, test_dataset, device=device)
+
+    # Assert
     assert_that(result, is_correct_image_property_drift_result())
+
+    assert_that(result.value, has_entries(
+        {'Brightness': close_to(0.05, 0.01)}
+    ))
 
 
 def test_image_property_drift_check_limit_classes(device):
     train_dataset = coco.load_dataset(train=True, object_type='VisionData')
     test_dataset = coco.load_dataset(train=False, object_type='VisionData')
+
+    # Run
     result = ImagePropertyDrift(classes_to_display=['person', 'cat', 'cell phone', 'car'], min_samples=5
                                 ).run(train_dataset, test_dataset, device=device)
+
+    # Assert
     assert_that(result, is_correct_image_property_drift_result())
+
+    assert_that(result.value, has_entries(
+        {'Brightness': close_to(0.15, 0.01)}
+    ))
 
 
 def test_image_property_drift_check_limit_classes_illegal(device):
