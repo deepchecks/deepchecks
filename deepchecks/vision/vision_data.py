65b81ad35459832eb75d9a85f2fba29174c450a4
JKL98ISR <jonatan.lib@gmail.com>
2022-03-31 15:13:28 +0300

    Fix problems in old torch version (#1152)
    * v0
    
    * v0
    
    * v0
    
    * v0

--- deepchecks/vision/vision_data.py
+++ deepchecks/vision/vision_data.py
@@ -496,16 +496,19 @@ class VisionData:
     @staticmethod
     def _get_data_loader_props(data_loader: DataLoader):
         """Get properties relevant for the copy of a DataLoader."""
-        return {
-            'num_workers': data_loader.num_workers,
-            'collate_fn': data_loader.collate_fn,
-            'pin_memory': data_loader.pin_memory,
-            'timeout': data_loader.timeout,
-            'worker_init_fn': data_loader.worker_init_fn,
-            'prefetch_factor': data_loader.prefetch_factor,
-            'persistent_workers': data_loader.persistent_workers,
-            'dataset': copy(data_loader.dataset)
-        }
+        attr_list = ['num_workers',
+                     'collate_fn',
+                     'pin_memory',
+                     'timeout',
+                     'worker_init_fn',
+                     'prefetch_factor',
+                     'persistent_workers']
+        aval_attr = {}
+        for attr in attr_list:
+            if hasattr(data_loader, attr):
+                aval_attr[attr] = getattr(data_loader, attr)
+        aval_attr['dataset'] = copy(data_loader.dataset)
+        return aval_attr
 
     @staticmethod
     def _get_data_loader_sequential(data_loader: DataLoader):
@@ -529,7 +532,7 @@ class VisionData:
         return data_loader.__class__(**props), sampler
 
 
-class IndicesSequentialSampler(Sampler[int]):
+class IndicesSequentialSampler(Sampler):
     """Samples elements sequentially from a given list of indices, without replacement.
 
     Args:
