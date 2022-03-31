5e273497466b02224bd203b7a3062b37fd4aeb02
Matan Perlmutter <matan@deepchecks.com>
2022-03-31 16:06:02 +0300

    Add new integrity suite and kwargs to vision suites

--- deepchecks/vision/suites/default_suites.py
+++ deepchecks/vision/suites/default_suites.py
@@ -15,46 +15,56 @@ It is possible to customize these suites by editing the checks and conditions in
 """
 from deepchecks.vision.checks import ClassPerformance, TrainTestLabelDrift, MeanAveragePrecisionReport, \
     MeanAverageRecallReport, ImagePropertyDrift, ImageDatasetDrift, SimpleModelComparison, ConfusionMatrixReport, \
-    TrainTestPredictionDrift, ImageSegmentPerformance, SimpleFeatureContribution
+    TrainTestPredictionDrift, ImageSegmentPerformance, SimpleFeatureContribution, HeatmapComparison, \
+    ImagePropertyOutliers, LabelPropertyOutliers, ModelErrorAnalysis
 from deepchecks.vision import Suite
 
 
-__all__ = ['train_test_validation', 'model_evaluation', 'full_suite']
-
-from deepchecks.vision.checks.distribution import HeatmapComparison
+__all__ = ['train_test_validation', 'model_evaluation', 'full_suite', 'integrity_validation']
 
 
-def train_test_validation() -> Suite:
+def train_test_validation(**kwargs) -> Suite:
     """Create a suite that is meant to validate correctness of train-test split, including integrity, \
     distribution and leakage checks."""
     return Suite(
         'Train Test Validation Suite',
-        HeatmapComparison(),
-        TrainTestLabelDrift().add_condition_drift_score_not_greater_than(),
-        TrainTestPredictionDrift().add_condition_drift_score_not_greater_than(),
-        ImagePropertyDrift().add_condition_drift_score_not_greater_than(),
-        ImageDatasetDrift(),
-        SimpleFeatureContribution().add_condition_feature_pps_difference_not_greater_than()
+        HeatmapComparison(**kwargs),
+        TrainTestLabelDrift(**kwargs).add_condition_drift_score_not_greater_than(),
+        TrainTestPredictionDrift(**kwargs).add_condition_drift_score_not_greater_than(),
+        ImagePropertyDrift(**kwargs).add_condition_drift_score_not_greater_than(),
+        ImageDatasetDrift(**kwargs),
+        SimpleFeatureContribution(**kwargs).add_condition_feature_pps_difference_not_greater_than()
     )
 
 
-def model_evaluation() -> Suite:
+def model_evaluation(**kwargs) -> Suite:
     """Create a suite that is meant to test model performance and overfit."""
     return Suite(
         'Model Evaluation Suite',
-        ClassPerformance().add_condition_train_test_relative_degradation_not_greater_than(),
-        MeanAveragePrecisionReport().add_condition_test_mean_average_precision_not_less_than(),
-        MeanAverageRecallReport(),
-        SimpleModelComparison(),
-        ConfusionMatrixReport(),
-        ImageSegmentPerformance().add_condition_score_from_mean_ratio_not_less_than()
+        ClassPerformance(**kwargs).add_condition_train_test_relative_degradation_not_greater_than(),
+        MeanAveragePrecisionReport(**kwargs).add_condition_test_mean_average_precision_not_less_than(),
+        MeanAverageRecallReport(**kwargs),
+        SimpleModelComparison(**kwargs),
+        ConfusionMatrixReport(**kwargs),
+        ImageSegmentPerformance(**kwargs).add_condition_score_from_mean_ratio_not_less_than(),
+        ModelErrorAnalysis(**kwargs)
     )
 
 
-def full_suite() -> Suite:
+def integrity_validation(**kwargs) -> Suite:
+    """Create a suite that is meant to validate integrity of the data."""
+    return Suite(
+        'Integrity Validation Suite',
+        ImagePropertyOutliers(**kwargs),
+        LabelPropertyOutliers(**kwargs)
+    )
+
+
+def full_suite(**kwargs) -> Suite:
     """Create a suite that includes many of the implemented checks, for a quick overview of your model and data."""
     return Suite(
         'Full Suite',
-        model_evaluation(),
-        train_test_validation()
+        model_evaluation(**kwargs),
+        train_test_validation(**kwargs),
+        integrity_validation(**kwargs)
     )
