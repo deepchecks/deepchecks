d7af0f0a9950fe8c0ac3a61f218c17dbc7ace150
Shiv Shankar Dayal <shivshankar.dayal@gmail.com>
2022-04-03 17:44:44 +0530

    pylint fix and missing load template

--- deepchecks/core/suite.py
+++ deepchecks/core/suite.py
@@ -97,7 +97,7 @@ class SuiteResult:
         if file is None:
             file = 'output.html'
         display_suite_result(self.name, self.results, html_out=file, requirejs=requirejs,\
-                             minimize_json=True, load_template="")
+                             minimize_json=True, load_template='empty-template')
 
     def to_json(self, with_display: bool = True):
         """Return check result as json.
