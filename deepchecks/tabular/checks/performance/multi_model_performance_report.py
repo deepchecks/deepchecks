83e8c22678d0f471dc2a0c07ae63e6d7e67cc35e
Matan Perlmutter <matan@deepchecks.com>
2022-04-07 16:54:49 +0300

    Fix lint

--- deepchecks/tabular/checks/performance/multi_model_performance_report.py
+++ deepchecks/tabular/checks/performance/multi_model_performance_report.py
@@ -1,3 +1,14 @@
+# ----------------------------------------------------------------------------
+# Copyright (C) 2021-2022 Deepchecks (https://www.deepchecks.com)
+#
+# This file is part of Deepchecks.
+# Deepchecks is distributed under the terms of the GNU Affero General
+# Public License (version 3 or later).
+# You should have received a copy of the GNU Affero General Public License
+# along with Deepchecks.  If not, see <http://www.gnu.org/licenses/>.
+# ----------------------------------------------------------------------------
+#
+"""Module containing multi model performance report check."""
 from typing import Dict, Callable, cast
 
 import pandas as pd
