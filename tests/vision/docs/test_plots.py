b508882fc7b710cf6342172dc5abd30b9ca89bfb
Matan Perlmutter <matan@deepchecks.com>
2022-04-10 13:27:04 +0300

    Add license to file

--- tests/vision/docs/test_plots.py
+++ tests/vision/docs/test_plots.py
@@ -1,3 +1,13 @@
+# ----------------------------------------------------------------------------
+# Copyright (C) 2021 Deepchecks (https://www.deepchecks.com)
+#
+# This file is part of Deepchecks.
+# Deepchecks is distributed under the terms of the GNU Affero General
+# Public License (version 3 or later).
+# You should have received a copy of the GNU Affero General Public License
+# along with Deepchecks.  If not, see <http://www.gnu.org/licenses/>.
+# ----------------------------------------------------------------------------
+#
 from pathlib import Path
 from runpy import run_path
 import glob
