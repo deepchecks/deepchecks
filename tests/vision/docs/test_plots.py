61ba1c605e9e41144f8d0109313e38504f061d34
Matan Perlmutter <matan@deepchecks.com>
2022-04-10 12:36:12 +0300

    Update to use path

--- tests/vision/docs/test_plots.py
+++ tests/vision/docs/test_plots.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from runpy import run_path
 import glob
 
@@ -7,8 +8,9 @@ import torch
 def test_plots_on_gpu():
     """If there is GPU available running all the docs plot files. Only makes sure the plots don't crash, and not \
     testing any other display or functionality."""
-    if torch.cuda.is_available() or True:
-        plots_files = glob.glob('../docs/source/**/plot_*.py', recursive=True)
+    if torch.cuda.is_available():
+        path = Path(__file__).parent.parent.parent.parent / "docs" / "source" / "**" / "plot_*.py"
+        plots_files = glob.glob(str(path), recursive=True)
         if not plots_files:
             raise ValueError("No plots found in docs/source")
         for file in plots_files:
