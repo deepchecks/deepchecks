9de5fca6c4e709a043c4dd45058628c526655d48
Matan Perlmutter <matan@deepchecks.com>
2022-04-10 14:49:52 +0300

    Fix tests plots on gpu to run only on source files

--- tests/vision/docs/test_plots.py
+++ tests/vision/docs/test_plots.py
@@ -10,18 +10,25 @@
 #
 from pathlib import Path
 from runpy import run_path
-import glob
 
 import torch
+import wandb
+
+# Since we have a plot that's calling `wandb.login` we need to setup first
+wandb.setup(wandb.Settings(mode="disabled", program=__name__, program_relpath=__name__, disable_code=True))
+
+DOCS_COMPILED_DIR = 'examples'
 
 
 def test_plots_on_gpu():
     """If there is GPU available running all the docs plot files. Only makes sure the plots don't crash, and not \
     testing any other display or functionality."""
-    if torch.cuda.is_available():
-        path = Path(__file__).parent.parent.parent.parent / "docs" / "source" / "**" / "plot_*.py"
-        plots_files = glob.glob(str(path), recursive=True)
-        if not plots_files:
+    if torch.cuda.is_available() or True:
+        path = Path(__file__).parent.parent.parent.parent / "docs" / "source"
+
+        # Take only source file and excluding compiled files
+        source_files = set(path.glob("**/plot_*.py")) - set(path.glob(f"**/{DOCS_COMPILED_DIR}/plot_*.py"))
+        if not source_files:
             raise ValueError("No plots found in docs/source")
-        for file in plots_files:
-            run_path(file)
+        for file in source_files:
+            run_path(str(file))
