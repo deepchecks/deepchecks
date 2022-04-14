e1f466ca61ae5d0f5727aa48ac48e2b856ec25b2
DBI <danb@deepchecks.com>
2022-04-14 11:14:36 +0300

    add Using alternative scorers section

--- docs/source/examples/tabular/checks/performance/source/plot_performance_report.py
+++ docs/source/examples/tabular/checks/performance/source/plot_performance_report.py
@@ -10,13 +10,14 @@ This notebooks provides an overview for using and understanding performance repo
 * `Generate data & model <#generate-data-model>`__
 * `Run the check <#run-the-check>`__
 * `Define a condition <#define-a-condition>`__
+* `Using alternative scorers <#using-alternative-scorers>`__
 
 What is the purpose of the check?
 =================================
 This check helps you compare your model's performance between two datasets.
 The default metric that are used are F1, Percision, and Recall for Classification
 and Negative Root Mean Square Error, Negative Mean Absolute Error, and R2 for Regression. RMSE and MAE Scorers are
-negative because we subscribe to the convention of sklearn to evaluate a model.
+negative because we subscribe to the sklearn convention of defining scoring functions.
 `See scorers documentation <https://scikit-learn.org/stable/modules/model_evaluation.html#scoring>`_
 
 """
@@ -55,3 +56,14 @@ result.show(show_additional_outputs=False)
 #%%
 # We detected that for class "0" our the Precision result is degraded by more than 5%
 
+#%%
+# Using alternative scorers
+# =========================
+# We can define alternative scorers that are not run by default:
+
+from sklearn.metrics import make_scorer, fbeta_score
+
+fbeta_scorer = make_scorer(fbeta_score, labels=[0, 1], average=None, beta=0.2)
+
+check = PerformanceReport(alternative_scorers={'my scorer': fbeta_scorer})
+check.run(train_dataset, test_dataset, model)
