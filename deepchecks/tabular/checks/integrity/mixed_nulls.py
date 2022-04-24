640e634de152bd08d972ac23d9f794b8d448d469
DBI <danb@deepchecks.com>
2022-04-24 16:30:48 +0300

    add nothing found message to tabular column checks

--- deepchecks/tabular/checks/integrity/mixed_nulls.py
+++ deepchecks/tabular/checks/integrity/mixed_nulls.py
@@ -17,6 +17,7 @@ import pandas as pd
 from deepchecks.tabular import Context, SingleDatasetCheck
 from deepchecks.core import CheckResult, ConditionResult, ConditionCategory
 from deepchecks.core.errors import DeepchecksValueError
+from deepchecks.tabular.utils.display_utils import nothing_found_on_columns
 from deepchecks.utils.dataframes import select_from_dataframe
 from deepchecks.utils.features import N_TOP_MESSAGE, column_importance_sorter_df
 from deepchecks.utils.strings import string_baseform, format_percent
@@ -114,7 +115,7 @@ class MixedNulls(SingleDatasetCheck):
                                                    self.n_top_columns, col='Column Name')
             display = [N_TOP_MESSAGE % self.n_top_columns, df_graph]
         else:
-            display = None
+            display = nothing_found_on_columns(df.columns)
 
         return CheckResult(result_dict, display=display)
 
