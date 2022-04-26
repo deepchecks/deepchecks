cd9517dd46e20836ff6a544b71d481a7f9753de9
Matan Perlmutter <matan@deepchecks.com>
2022-04-26 11:18:02 +0300

    Update plot

--- docs/source/examples/tabular/checks/methodology/source/plot_train_test_samples_mix.py
+++ docs/source/examples/tabular/checks/methodology/source/plot_train_test_samples_mix.py
@@ -2,41 +2,42 @@
 """
 Train Test Samples Mix
 **********************
+This notebook provides an overview for using and understanding the Train Test Samples Mix check.
+
+**Structure:**
+
+* `Why samples mix are unwanted? <#why-samples-mix-are-unwanted>`__
+* `Run the check <#run-the-check>`__
+* `Define a condition <#define-a-condition>`__
+
+Why samples mix are unwanted?
+=============================
+Samples mix is a situation when train and test have joint samples. While using the test dataset for evaluating the
+model, if we have samples from the train dataset which was used for training the model, then the resulting metrics will
+be biased and won't reflect the performance we will get in a real life scenario. Therefore, it's important to always
+avoid samples mix.
+
+Run the check
+=============
+We will run the check on the iris dataset.
 """
-#%%
-# Imports
-# =======
 
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
+from deepchecks.tabular.datasets.classification import iris
 from deepchecks.tabular.checks.methodology import TrainTestSamplesMix
-from deepchecks.tabular import Dataset
 
-#%%
-# Generating data
-# ===============
-
-iris = load_iris(return_X_y=False, as_frame=True)
-X = iris.data
-y = iris.target
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=55)
-train_dataset = Dataset(pd.concat([X_train, y_train], axis=1), 
-            features=iris.feature_names,
-            label='target')
-
-test_df = pd.concat([X_test, y_test], axis=1)
-bad_test = test_df.append(train_dataset.data.iloc[[0, 1, 1, 2, 3, 4]], ignore_index=True)
-                    
-test_dataset = Dataset(bad_test, 
-            features=iris.feature_names,
-            label='target')
-
-#%%
-# Running ``data_sample_leakage_report check``
-# ============================================
+# Create data with leakage from train to test
+train_df, test_df = iris.load_data(data_format='Dataframe')
+bad_test = test_df.append(train_df.data.iloc[[0, 1, 1, 2, 3, 4, 2, 2, 10]], ignore_index=True)
 
 check = TrainTestSamplesMix()
-check.run(test_dataset=test_dataset, train_dataset=train_dataset)
+result = check.run(test_dataset=bad_test, train_dataset=train_df)
+result
+
+# %%
+# Define a condition
+# ==================
+# We can define a condition that enforces that the ratio of samples in test which appears in train is below a given
+# amount, the default is `0.1`.
+check = TrainTestSamplesMix().add_condition_duplicates_ratio_not_greater_than()
+result = check.run(test_dataset=bad_test, train_dataset=train_df)
+result.show(show_additional_outputs=False)
