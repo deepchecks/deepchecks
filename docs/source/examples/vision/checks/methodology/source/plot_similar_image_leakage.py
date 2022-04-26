9e806a147c8a61f57830154192cd9f04f8495ce7
Nir Hutnik <92314933+nirhutnik@users.noreply.github.com>
2022-04-26 18:16:58 +0300

    Update docs/source/examples/vision/checks/methodology/source/plot_similar_image_leakage.py
    Co-authored-by: Noam Bressler <noamzbr@gmail.com>

--- docs/source/examples/vision/checks/methodology/source/plot_similar_image_leakage.py
+++ docs/source/examples/vision/checks/methodology/source/plot_similar_image_leakage.py
@@ -35,7 +35,7 @@ image, when distance is defined by the Hamming distance between the binary vecto
 
 Note about default parameters
 --------------------------------------
-Similarity between images is dependent on the purpose of the dataset. This is because sometimes we're training a model
+Similarity between images depends on the purpose of the dataset. This is because sometimes we're training a model
 to find large differences between images (e.g. people vs dogs) and sometimes we're training to find small differences
 (e.g. different types of trees). Moreover, sometimes our images are taken from real-world datasets, where they were
 taken by different people, in different locations, and sometimes the images are "cleaner", such as ones taken under
