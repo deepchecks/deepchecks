ff8ad715f6f1a9a5a1aa16d27ee7376c72b5a279
Nadav-Barak <nadavbarak148@gmail.com>
2022-04-28 15:11:22 +0300

    test update

--- tests/vision/checks/distribution/image_dataset_drift_test.py
+++ tests/vision/checks/distribution/image_dataset_drift_test.py
@@ -110,8 +110,8 @@ def test_with_drift_rgb(coco_train_dataloader, coco_test_dataloader, device):
     result = check.run(train, test, random_state=42, device=device)
     # Assert
     assert_that(result.value, has_entries({
-        'domain_classifier_auc': close_to(1, 0.001),
-        'domain_classifier_drift_score': close_to(1, 0.001),
+        'domain_classifier_auc': close_to(0.908, 0.001),
+        'domain_classifier_drift_score': close_to(0.815, 0.001),
         'domain_classifier_feature_importance': has_entries({
             'Brightness': close_to(1, 0.001),
             'Aspect Ratio': equal_to(0),
