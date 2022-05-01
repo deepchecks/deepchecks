3eb1406ebeea9e26848249116de7d634ddb830ec
Noam Bressler <noamzbr@gmail.com>
2022-05-01 17:06:12 +0300

    Finalize the example and fix Segment Performance

--- docs/source/checks/tabular/performance/plot_model_error_analysis.py
+++ docs/source/checks/tabular/performance/plot_model_error_analysis.py
@@ -13,10 +13,32 @@ This notebooks provides an overview for using and understanding the model error 
 
 What is Model Error Analysis?
 =============================
-The check trains a regression model to predict the error of the user's model. Then, the features scoring the highest
-feature importance for the error regression model are selected and the distribution of the error vs the feature
-values is plotted. The check results are shown only if the error regression model manages to predict the error
-well enough.
+Looking at performance metrics are often enough when comparing various modeling alternatives or when tracking model
+progress during training. However, when it's time to fully evaluate if a model is fit for production, it's recommended
+to look deeper at how the model performs on various segments of the data. The model error analysis check searches for
+data segments in which the model error is significantly lower from the model error of the dataset as a whole.
+
+Algorithm:
+----------
+
+1. Computes the per-sample loss (for log-loss for classification, mse for regression).
+2. Trains a regression model to predict the error of the user's model, based on the input features.
+3. Do stage 3 several times with various tree parameters and random states to ensure that the most relevant partitions for model error are selected.
+4. The features scoring the highest feature importance for the error regression model are selected and the distribution of the error vs the feature values is plotted.
+
+The check results are shown only if the error regression model manages
+to predict the error well enough. The resulting plots show the distribution of the error for the features that are
+most effective at segmenting the error to high and low values, without need for manual selection of segmentation
+features.
+
+Related Checks:
+---------------
+When the important segments of the data are known in advance (when we know that some population segments have
+different behaviours and business importance, for example income levels or state of residence) it is possible to just
+have a look at the performance at various pre-defined segments. In deepchecks, this can be done using the
+:doc:`Segment Performance </checks_gallery/tabular/performance/plot_segment_performance>`_ check, which shows the
+performance for segments defined by combination of values from two pre-defined columns.
+
 
 Run the check
 =============
@@ -34,6 +56,19 @@ model = adult.load_fitted_model()
 check = ModelErrorAnalysis(min_error_model_score=0.3)
 result = check.run(train_ds, test_ds, model)
 result
+
+#%%
+# The check has found that the features 'hours-per-week', 'age' and 'relationship' are the most predictive of
+# differences in the model error. We can further investigate the model performance by passing two of these columns
+# to the :doc:`Segment Performance </checks_gallery/tabular/performance/plot_segment_performance>`_ check:
+
+from deepchecks.tabular.checks import SegmentPerformance
+
+SegmentPerformance(feature_1='age', feature_2='relationship').run(test_ds, model)
+
+#%%
+# From which we learn that the model error is exceptionally higher for people in the "Husband" or "Other" status,
+# except for the lower age groups for which the error is lower.
 
 #%%
 # Define a condition
