994982095f906715e94b361bdf01b08ab65686a8
Nadav-Barak <nadavbarak148@gmail.com>
2022-05-04 10:54:21 +0300

    Clearer doc

--- docs/source/checks/vision/performance/plot_mean_average_precision_report.py
+++ docs/source/checks/vision/performance/plot_mean_average_precision_report.py
@@ -22,14 +22,13 @@ This check only works on the Object Detection task.
 
 """
 
-#%%
+# %%
 # Generate Data and Model
 # =================================
 # We generate a sample dataset of 128 images from the `COCO dataset <https://cocodataset.org/#home>`__,
 # and using the `YOLOv5 model <https://github.com/ultralytics/yolov5>`__.
 #
-# For the label formatter - our dataset returns exactly the accepted format, so our
-# formatting function is the simple `lambda x: x` function.
+
 
 from deepchecks.vision.checks.performance import MeanAveragePrecisionReport
 from deepchecks.vision.datasets.detection import coco
@@ -37,7 +36,7 @@ from deepchecks.vision.datasets.detection import coco
 yolo = coco.load_model(pretrained=True)
 test_ds = coco.load_dataset(train=False, object_type='VisionData')
 
-#%%
+# %%
 # Run the check
 # =================================
 
@@ -45,7 +44,7 @@ check = MeanAveragePrecisionReport()
 result = check.run(test_ds, yolo)
 result
 
-#%%
+# %%
 # Observe the check’s output
 # --------------------------
 # The result value is a dataframe that has the Average Precision score for different bounding box sizes. We report the
@@ -54,11 +53,11 @@ result
 
 result.value
 
-#%%
+# %%
 # Define a condition 
 # ==================
 # We can define a condition that checks whether our model's mean average precision score is not less than
-# a given threshold for any bounding box size.
+# a given threshold for all bounding box sizes.
 
 check = MeanAveragePrecisionReport().add_condition_test_mean_average_precision_not_less_than(0.4)
 result = check.run(test_ds, yolo)
