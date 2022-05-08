e343aeb13db2c26a39ddfe4f6c4009472ce87342
JKL98ISR <jonatan.lib@gmail.com>
2022-05-08 15:18:54 +0300

    typings

--- deepchecks/core/serialization/suite_result/html.py
+++ deepchecks/core/serialization/suite_result/html.py
@@ -145,7 +145,7 @@ class SuiteResultSerializer(HtmlSerializer['suite.SuiteResult']):
         long_prologue_version = 'The suite is composed of various checks such as: {names}, etc...'
         short_prologue_version = 'The suite is composed of the following checks: {names}.'
         check_names = list(set(
-            it.check.name()
+            it.check_name
             for it in self.value.results
         ))
         return (
