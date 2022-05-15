d86a46c13869633a895ffc5eed63fcead13889df
TheSolY <solyarkoni@gmail.com>
2022-05-15 17:25:39 +0300

    Example - WIP

---
+++ docs/source/checks/vision/performance/plot_single_data_scalar_performance.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Single Data Scalar Performance
+******************************
+
+This notebooks provides an overview for using and understanding single data scalar performance check.
+
+**Structure:**
+
+* `What Is the Purpose of the Check? <#what-is-the-purpose-of-the-check>`__
+* `Generate data an model <#generate-data-and-model>`__
+* `Run the check <#run-the-check>`__
+
+What Is the Purpose of the Check?
+=================================
+This check returns a metric result as a single scalar, which is especially useful for monitoring.
+
+"""
