25a559b2de33bc36ed310bf42bc0374b71e8c257
Matan Perlmutter <matan@deepchecks.com>
2022-05-16 12:22:23 +0300

    Change skelarn model equality test

--- tests/tabular/test_datasets.py
+++ tests/tabular/test_datasets.py
@@ -12,6 +12,7 @@ import sys
 
 import numpy as np
 import sklearn
+from deepdiff import DeepDiff
 from hamcrest import assert_that, instance_of
 
 from deepchecks.tabular.datasets.classification import breast_cancer, iris, phishing, adult
@@ -19,26 +20,21 @@ from deepchecks.tabular.datasets.regression import avocado
 from deepchecks.utils.model import get_model_of_pipeline
 
 
-def assert_sklearn_model_equals(model1, model2):
-    model1 = get_model_of_pipeline(model1)
-    model2 = get_model_of_pipeline(model2)
+def assert_sklearn_model_params_equals(model1, model2):
     assert type(model1) == type(model2)
-    if hasattr(model1, 'tree_'):
-        assert (model1.tree_.value == model2.tree_.value).all()
-    elif hasattr(model1, 'estimators_'):
-        assert len(model1.estimators_) == len(model2.estimators_)
-        for sub1, sub2 in zip(model1.estimators_, model2.estimators_):
-            assert_sklearn_model_equals(sub1, sub2)
-    else:
-        raise Exception('Don\'t know how to compare models')
+    diff = DeepDiff(model1.get_params(), model2.get_params(), ignore_numeric_type_changes=True)
+    assert diff == {}
+
+
+def assert_model_predict_on_data(mode, train, test):
+    assert_that(mode.predict(train.features_columns.iloc[:1]), instance_of(np.ndarray))
+    assert_that(mode.predict(test.features_columns.iloc[:1]), instance_of(np.ndarray))
 
 
 def assert_dataset_module(dataset_module):
     train, test = dataset_module.load_data()
     trained_model = dataset_module.load_fitted_model(pretrained=False)
-
-    assert_that(trained_model.predict(train.features_columns.iloc[:1]), instance_of(np.ndarray))
-    assert_that(trained_model.predict(test.features_columns.iloc[:1]), instance_of(np.ndarray))
+    assert_model_predict_on_data(trained_model, train, test)
 
     # The models were trained on python 3.8, therefore tests for equality of pretrained only on this version
     python_minor_version = sys.version_info[1]
@@ -46,7 +42,8 @@ def assert_dataset_module(dataset_module):
         if sklearn.__version__ != dataset_module._MODEL_VERSION:
             raise Exception(f'Can\'t test pretrained model for non matching sklearn version {sklearn.__version__}')
         pretrained_model = dataset_module.load_fitted_model(pretrained=True)
-        assert_sklearn_model_equals(pretrained_model, trained_model)
+        assert_sklearn_model_params_equals(pretrained_model, trained_model)
+        assert_model_predict_on_data(pretrained_model, train, test)
 
 
 def test_model_predict_on_breast_cancer():
