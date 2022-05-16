ae67d5ef486f4049b2f0c20a7ebdfdc431c91933
JKL98ISR <jonatan.lib@gmail.com>
2022-05-16 11:10:38 +0300

    isort_shananigans

--- deepchecks/core/suite.py
+++ deepchecks/core/suite.py
@@ -279,6 +279,7 @@ class SuiteResult:
         # Previous implementation used ProgressBar to show serialization progress
         try:
             import wandb
+
             from deepchecks.core.serialization.suite_result.wandb import \
                 SuiteResultSerializer as WandbSerializer
         except ImportError as error:
