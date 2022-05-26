0b480ecd887d22e4db8626c10ff7c552d06093d0
Yurii Romanyshyn <yurii.romanyshyn@starnavi.io>
2022-05-26 10:31:22 +0300

    small fix

--- deepchecks/core/check_result.py
+++ deepchecks/core/check_result.py
@@ -608,7 +608,7 @@ class CheckFailure(BaseCheckResult):
         """Return CheckFailure as a ipywidgets.Widget instance."""
         return CheckFailureWidgetSerializer(self).serialize()
 
-    def to_json(self):
+    def to_json(self, with_display: Optional[bool] = None):
         """Return check failure as json.
 
         Returned JSON string will have next structure:
@@ -624,10 +624,23 @@ class CheckFailure(BaseCheckResult):
         >>        params: Dict[Any, Any]
         >>        summary: str
 
+        Parameters
+        ----------
+        with_display : bool
+            controls if to serialize display or not
+            (the parameter is deprecated and does not have any effect since version 0.6.4,
+            it will be removed in future versions)
+
         Returns
         -------
         str
         """
+        if with_display is not None:
+            warnings.warn(
+                '"with_display" parameter is deprecated and does not have any effect '
+                'since version 0.6.4, it will be removed in future versions',
+                DeprecationWarning
+            )
         return jsonpickle.dumps(
             CheckFailureJsonSerializer(self).serialize(),
             unpicklable=False
