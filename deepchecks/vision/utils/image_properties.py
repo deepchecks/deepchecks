d8ea077647dfaea983f916a67c8a46161290f066
Yurii Romanyshyn <yurii.romanyshyn@starnavi.io>
2022-05-31 17:51:40 +0300

    added warning filter

--- deepchecks/vision/utils/image_properties.py
+++ deepchecks/vision/utils/image_properties.py
@@ -195,7 +195,7 @@ def validate_properties(properties: List[Dict[str, Any]]):
     if len(list_of_warnings) > 0:
         concatenated_warnings = '\n+ '.join(list_of_warnings)
         warnings.warn(
-            f'Property Warnings:\n+ {concatenated_warnings}', 
+            f'Property Warnings:\n+ {concatenated_warnings}',
             category=DeprecationWarning
         )
 
