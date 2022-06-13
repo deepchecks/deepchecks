fece2e2337d5b24b53c893121ec30ae4d724e917
Sol <99395146+TheSolY@users.noreply.github.com>
2022-06-13 14:38:20 +0300

    Update docs/source/checks/tabular/data_integrity/plot_feature_feature_correlation.py
    Co-authored-by: Noam Bressler <noamzbr@gmail.com>

--- docs/source/checks/tabular/data_integrity/plot_feature_feature_correlation.py
+++ docs/source/checks/tabular/data_integrity/plot_feature_feature_correlation.py
@@ -47,7 +47,7 @@ ds = adult.load_data(as_train_test=False)
 check = FeatureFeatureCorrelation()
 check.run(ds)
 
-# For displaying the results in an IDE like PyCharm, you can use the following code:
+# To display the results in an IDE like PyCharm, you can use the following code:
 # check.run(ds).show()
 # The result will be displayed in a new window.
 
