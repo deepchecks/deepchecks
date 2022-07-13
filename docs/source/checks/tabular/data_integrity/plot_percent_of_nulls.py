50f6c83eb5dbffe29463c8011dea6d2946c78d93
Yurii Romanyshyn <yurii.romanyshyn@starnavi.io>
2022-07-13 16:19:19 +0300

    added check example to docs

---
+++ docs/source/checks/tabular/data_integrity/plot_percent_of_nulls.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+.. _plot_percent_of_nulls:
+
+Percent Of Nulls
+****************
+"""
+#%% 
+# Imports
+# =============
+import numpy as np
+import pandas as pd
+from deepchecks.tabular.checks.data_integrity import PercentOfNulls
+
+#%%
+# Run check
+# =============
+df = pd.DataFrame({'foo': [1, 2, None, np.nan], 'bar': [None, 1, 2, 3]})
+result = PercentOfNulls().run(df)
+result.show()
+
+#%%
+# Run check with condition
+# =============
+df = pd.DataFrame({'foo': [1, 2, None, np.nan], 'bar': [None, 1, 2, 3]})
+check = PercentOfNulls().add_condition_percent_of_nulls_not_greater_than()
+result = check.run(df)
+result.show()
