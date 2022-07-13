ab15b80795c7f324323df23ae4b82f49c124dfbb
Nadav-Barak <nadavbarak148@gmail.com>
2022-07-13 12:59:17 +0300

    pipeline test 2

--- tests/examples_validation.py
+++ tests/examples_validation.py
@@ -11,14 +11,16 @@
 import os
 import sys
 from urllib.parse import urlparse
+import inspect
 
 import deepchecks.tabular.checks as tabular_checks
 import deepchecks.vision.checks as vision_checks
 from deepchecks import BaseCheck
 from deepchecks.utils.strings import generate_check_docs_link
+from deepchecks.vision.checks.data_integrity.abstract_property_outliers import AbstractPropertyOutliers
 
 # This script is used to validate the examples in the repo. It will validate that the names of the examples are
-# correct and match the name of the check. In addition it will validate the examples include a single H1 tag.
+# correct and match the name of the check. In addition, it will validate the examples include a single H1 tag.
 
 checks_dirs = ["deepchecks/tabular/checks", "deepchecks/vision/checks"]
 
@@ -27,14 +29,14 @@ ignored_files = [
     "deepchecks/tabular/checks/model_evaluation/performance_report.py"
     ]
 
+ignored_classes = [tabular_checks.PerformanceReport, AbstractPropertyOutliers]
 
 def test_read_more_link(check_class, compiled_dir: str):
     link = urlparse(generate_check_docs_link(check_class()))
     # For path "/stable/examples/..." remove the version part
     relevant_path_parts = link.path.split("/")[2:]
     file_path = os.path.join(*compiled_dir.split("/"), *relevant_path_parts)
-    if not os.path.exists(file_path) and file_path not in ignored_files:
-        print(f"Check {file_path}")
+    if not os.path.exists(file_path):
         print(f"Check {check_class.__name__} 'read more' link didn't correspond to an html file")
         return False
     return True
@@ -44,7 +46,7 @@ def get_check_classes_in_module(module):
     all_classes = dir(module)
     for class_name in all_classes:
         class_ = getattr(module, class_name)
-        if hasattr(class_, "mro") and BaseCheck in class_.mro():
+        if hasattr(class_, "mro") and BaseCheck in class_.mro() and class_ not in ignored_classes:
             yield class_
 
 
@@ -54,7 +56,7 @@ def validate_dir(checks_path, examples_path):
         for file_name in files:
             if file_name != "__init__.py" and file_name.endswith(".py"):
                 check_path = os.path.join(root, file_name)
-                if check_path in ignored_files:
+                if any(inspect.getmodule(cls).__file__.endswith(check_path) for cls in ignored_classes):
                     continue
                 example_file_name = "plot_" + file_name
                 splitted_path = check_path.split("/")
