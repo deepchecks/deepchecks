3b0e7f8dbff31c52a8e10716e1393005c4f4d6b9
Noam Bressler <noamzbr@gmail.com>
2022-07-13 15:01:18 +0300

    remove warning in pandas 1.4.2 about deprecation of hide_index()

--- deepchecks/core/serialization/suite_result/widget.py
+++ deepchecks/core/serialization/suite_result/widget.py
@@ -151,7 +151,7 @@ class SuiteResultSerializer(WidgetSerializer['suite.SuiteResult']):
             children = (HTML(value='<p>No outputs to show.</p>'),)
         else:
             df = create_failures_dataframe(failures)
-            table = DataFrameSerializer(df.style.hide_index()).serialize()
+            table = DataFrameSerializer(df.style.hide(axis='index')).serialize()
             children = (table,)
         accordion = normalize_widget_style(Accordion(
             children=children,
