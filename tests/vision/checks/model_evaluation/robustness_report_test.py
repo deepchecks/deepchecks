ddbbad168c29f5337d0bbcbdca78152e3379caa5
JKL98ISR <jonatan.lib@gmail.com>
2022-07-14 12:46:14 +0300

    eh

--- tests/vision/checks/model_evaluation/robustness_report_test.py
+++ tests/vision/checks/model_evaluation/robustness_report_test.py
@@ -79,12 +79,12 @@ def test_mnist_torch_default(mnist_dataset_train_torch, mock_trained_mnist, devi
     # Assert
     assert_that(result.value, has_entries({
         'Random Brightness Contrast': has_entries({
-            'Precision': has_entries(score=close_to(0.964, 0.001), diff=close_to(-0.014, 0.001)),
-            'Recall': has_entries(score=close_to(0.964, 0.001), diff=close_to(-0.014, 0.001))
+            'Precision': has_entries(score=close_to(0.964, 0.005), diff=close_to(-0.014, 0.005)),
+            'Recall': has_entries(score=close_to(0.964, 0.005), diff=close_to(-0.014, 0.005))
         }),
         'Shift Scale Rotate': has_entries({
-            'Precision': has_entries(score=close_to(0.784, 0.001), diff=close_to(-0.198, 0.001)),
-            'Recall': has_entries(score=close_to(0.779, 0.001), diff=close_to(-0.203, 0.001))
+            'Precision': has_entries(score=close_to(0.784, 0.005), diff=close_to(-0.198, 0.005)),
+            'Recall': has_entries(score=close_to(0.779, 0.005), diff=close_to(-0.203, 0.005))
         }),
     }))
 
