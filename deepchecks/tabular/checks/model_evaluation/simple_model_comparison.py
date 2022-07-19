c42c5d57825dfcdf541e3b096dd96679e87ba92a
Itay Gabbay <itay@deepchecks.com>
2022-07-19 09:59:29 +0300

    Fixing isort

--- deepchecks/tabular/checks/model_evaluation/simple_model_comparison.py
+++ deepchecks/tabular/checks/model_evaluation/simple_model_comparison.py
@@ -27,7 +27,7 @@ from deepchecks.tabular import Context, Dataset, TrainTestCheck
 from deepchecks.tabular.utils.task_type import TaskType
 from deepchecks.utils.distribution.preprocessing import ScaledNumerics
 from deepchecks.utils.metrics import get_gain
-from deepchecks.utils.simple_models import RandomModel, ClassificationUniformModel, RegressionUniformModel
+from deepchecks.utils.simple_models import ClassificationUniformModel, RandomModel, RegressionUniformModel
 from deepchecks.utils.strings import format_percent
 
 __all__ = ['SimpleModelComparison']
