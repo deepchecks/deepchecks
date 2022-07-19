9f024133b3e64599c9313743ffd4afd393401987
Noam Bressler <noamzbr@gmail.com>
2022-07-19 15:13:58 +0300

    simplify the customization referencing

--- docs/source/user-guide/general/customizations/plot_create_a_custom_check.py
+++ docs/source/user-guide/general/customizations/plot_create_a_custom_check.py
@@ -7,9 +7,7 @@ It is possible to extend deepchecks by implementing custom checks. This
 enables you to have your own logic of metrics or validation, or even just
 to display your own graph using deepchecks' suite.
 
-While some aspects for writing a check are similar when writing checks for the
-``deepchecks.tabular`` ``and deepchecks.vision`` subpackages, there are major differences
-that warrant a separate guide for each:
+For implementing custom checks, please refer to:
 
 - :doc:`Tabular Custom Check Guide. </user-guide/tabular/tutorials/plot_add_a_custom_check>`__
 - :doc:`Vision Custom Check Guide. </user-guide/vision/tutorials/plot_custom_checks>`__
