83baf16f6aae072234a6b95ef84e57ca0191b7fc
Nadav-Barak <nadavbarak148@gmail.com>
2022-08-15 14:56:54 +0300

    Added l2 combination feature drift

--- tests/tabular/checks/train_test_validation/train_test_feature_drift_test.py
+++ tests/tabular/checks/train_test_validation/train_test_feature_drift_test.py
@@ -185,6 +185,18 @@ def test_weighted_aggregation_drift_with_model(drifted_data_and_model):
     assert_that(aggregated_result['Weighted Drift Score'], close_to(0.1195, 0.01))
 
 
+def test_l2_aggregation_drift_with_model(drifted_data_and_model):
+    # Arrange
+    train, test, model = drifted_data_and_model
+    check = TrainTestFeatureDrift(categorical_drift_method='PSI', aggregation_method='l2_combination')
+
+    # Act
+    aggregated_result = check.run(train, test, model).reduce_output()
+    # Assert
+    assert_that(aggregated_result.keys(), has_item('L2 Combined Drift Score'))
+    assert_that(aggregated_result['L2 Combined Drift Score'], close_to(0.232, 0.01))
+
+
 def test_none_aggregation_drift_with_model(drifted_data_and_model):
     # Arrange
     train, test, model = drifted_data_and_model
