0aa3d593796191cae752c45cd5bc564bd9512596
JKL98ISR <jonatan.lib@gmail.com>
2022-08-28 13:51:59 +0300

    Update deepchecks/tabular/feature_importance.py
    Co-authored-by: Nir Hutnik <92314933+nirhutnik@users.noreply.github.com>

--- deepchecks/tabular/feature_importance.py
+++ deepchecks/tabular/feature_importance.py
@@ -47,7 +47,7 @@ def calculate_feature_importance(
     >>> from deepchecks.tabular.feature_importance import calculate_feature_importance
     >>> from deepchecks.tabular.datasets.classification.iris import load_data, load_fitted_model
     >>> from deepchecks.tabular.checks import UnusedFeatures
-    >>> iris_train_dataset, iris_test_dataset = load_data()
+    >>> _, iris_test_dataset = load_data()
     >>> iris_model = load_fitted_model()
     >>> fi = calculate_feature_importance(model=iris_model, dataset=iris_train_dataset)
     >>> result = UnusedFeatures().run(iris_test_dataset, model=iris_model, feature_importance=fi)
