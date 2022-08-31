050f8ec06e4b61c02646553b6a410d859e805142
Noam Bressler <noamzbr@gmail.com>
2022-08-31 12:27:37 +0300

    fix min_category_size_ratio bug

--- deepchecks/utils/distribution/preprocessing.py
+++ deepchecks/utils/distribution/preprocessing.py
@@ -114,7 +114,7 @@ class ScaledNumerics(TransformerMixin, BaseEstimator):
 
 
 def preprocess_2_cat_cols_to_same_bins(dist1: Union[np.ndarray, pd.Series], dist2: Union[np.ndarray, pd.Series],
-                                       min_category_size_ratio: float = 0, max_num_categories: int = None,
+                                       min_category_size_ratio: float = 0.01, max_num_categories: int = None,
                                        sort_by: str = 'dist1'
                                        ) -> Tuple[np.ndarray, np.ndarray, List]:
     """
@@ -160,8 +160,7 @@ def preprocess_2_cat_cols_to_same_bins(dist1: Union[np.ndarray, pd.Series], dist
 
     """
     dist1_counter, dist2_counter = Counter(dist1), Counter(dist2)
-    len1 = len(dist1)
-    len2 = len(dist2)
+    size_dist1, size_dist2 = len(dist1), len(dist2)
     categories_list = list(set(dist1_counter.keys()) | (set(dist2_counter.keys())))
 
     if max_num_categories is not None and len(categories_list) > max_num_categories:
@@ -179,18 +178,18 @@ def preprocess_2_cat_cols_to_same_bins(dist1: Union[np.ndarray, pd.Series], dist
         categories_list = [x[0] for x in sorted(sort_by_counter.items(), key=lambda x: (-x[1], x[0]))][
                           :max_num_categories]
         dist1_counter = Counter({k: dist1_counter[k] for k in categories_list})
-        dist1_counter[OTHER_CATEGORY_NAME] = len1 - sum(dist1_counter.values())
+        dist1_counter[OTHER_CATEGORY_NAME] = size_dist1 - sum(dist1_counter.values())
         dist2_counter = Counter({k: dist2_counter[k] for k in categories_list})
-        dist2_counter[OTHER_CATEGORY_NAME] = len2 - sum(dist2_counter.values())
+        dist2_counter[OTHER_CATEGORY_NAME] = size_dist2 - sum(dist2_counter.values())
 
     for cat in copy.deepcopy(categories_list):
-        if dist1_counter[cat] < len1 * min_category_size_ratio:
+        if dist1_counter[cat] < size_dist1 * min_category_size_ratio:
             dist1_counter[OTHER_CATEGORY_NAME] += dist1_counter[cat]
             dist2_counter[OTHER_CATEGORY_NAME] += dist2_counter[cat]
             categories_list.remove(cat)
 
-    if dist1_counter[OTHER_CATEGORY_NAME] > min_category_size_ratio or \
-            dist2_counter[OTHER_CATEGORY_NAME] > min_category_size_ratio:
+    if dist1_counter[OTHER_CATEGORY_NAME] > (min_category_size_ratio * size_dist1) or \
+            dist2_counter[OTHER_CATEGORY_NAME] > (min_category_size_ratio * size_dist2):
         categories_list.append(OTHER_CATEGORY_NAME)
 
     # aligns both counts to the same index
