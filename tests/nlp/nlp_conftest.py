21ab2f04c11a1b35e56f314840d7f3051592de6e
Noam Bressler <noamzbr@gmail.com>
2022-09-01 08:54:43 +0300

    fix tests

--- tests/nlp/nlp_conftest.py
+++ tests/nlp/nlp_conftest.py
@@ -15,6 +15,7 @@ import pytest
 
 from deepchecks.nlp.text_data import TextData
 from nltk.corpus import movie_reviews
+from nltk import download as nltk_download
 
 
 @pytest.fixture(scope='session')
@@ -41,9 +42,16 @@ def text_multilabel_classification_dataset_mock():
                     task_type='text_classification')
 
 
+def download_nltk_resources():
+    """Download nltk resources"""
+    nltk_download('movie_reviews')
+    nltk_download('punkt')
+
+
 @pytest.fixture(scope='session')
 def movie_reviews_data():
     """Dataset of single sentence samples."""
+    download_nltk_resources()
     sentences = [' '.join(x) for x in movie_reviews.sents()]
     random.seed(42)
     train_data = TextData(random.choices(sentences, k=10000))
@@ -54,6 +62,7 @@ def movie_reviews_data():
 @pytest.fixture(scope='session')
 def movie_reviews_data_positive():
     """Dataset of single sentence samples labeled positive."""
+    download_nltk_resources()
     random.seed(42)
     pos_sentences = [' '.join(x) for x in movie_reviews.sents(categories='pos')]
     pos_data = TextData(random.choices(pos_sentences, k=1000), dataset_name='Positive')
@@ -63,6 +72,7 @@ def movie_reviews_data_positive():
 @pytest.fixture(scope='session')
 def movie_reviews_data_negative():
     """Dataset of single sentence samples labeled negative."""
+    download_nltk_resources()
     random.seed(42)
     neg_sentences = [' '.join(x) for x in movie_reviews.sents(categories='neg')]
     neg_data = TextData(random.choices(neg_sentences, k=1000), dataset_name='Negative')
