e6ab3ea5088306ed069aa64db97182542a5050eb
nirhutnik <nirhutnik@gmail.com>
2022-09-10 16:48:08 +0300

    Added tests for SegmentationData

---
+++ deepchecks/vision/datasets/segmentation/__init__.py
@@ -0,0 +1,14 @@
+# ----------------------------------------------------------------------------
+# Copyright (C) 2021-2022 Deepchecks (https://www.deepchecks.com)
+#
+# This file is part of Deepchecks.
+# Deepchecks is distributed under the terms of the GNU Affero General
+# Public License (version 3 or later).
+# You should have received a copy of the GNU Affero General Public License
+# along with Deepchecks.  If not, see <http://www.gnu.org/licenses/>.
+# ----------------------------------------------------------------------------
+#
+"""Module for segmentation datasets and models."""
+from . import segmentation_coco
+
+__all__ = ["segmentation_coco"]
