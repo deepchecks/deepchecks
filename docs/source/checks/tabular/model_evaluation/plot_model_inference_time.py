e1954e70450c066b54ad50eaf4ce3c86cf6d1ffe
DanBasson <danbas87@gmail.com>
2022-09-19 16:45:04 +0300

    [DOCS] Added documentation for tabular Model Evaluation checks (#1999)
    * added documentation
    
    * Update docs/source/checks/tabular/model_evaluation/plot_model_info.py
    
    Co-authored-by: Nir Hutnik <92314933+nirhutnik@users.noreply.github.com>
    
    * Update docs/source/checks/tabular/model_evaluation/plot_multi_model_performance_report.py
    
    Co-authored-by: Nir Hutnik <92314933+nirhutnik@users.noreply.github.com>
    
    * Update docs/source/checks/tabular/model_evaluation/plot_multi_model_performance_report.py
    
    Co-authored-by: Nir Hutnik <92314933+nirhutnik@users.noreply.github.com>
    
    * Update docs/source/checks/tabular/model_evaluation/plot_regression_error_distribution.py
    
    Co-authored-by: Nir Hutnik <92314933+nirhutnik@users.noreply.github.com>
    
    * fix doc according to authors' comments
    
    * added changes according to author's requests
    
    * minor fixes
    
    Co-authored-by: Nir Hutnik <92314933+nirhutnik@users.noreply.github.com>
    Co-authored-by: shir22 <33841818+shir22@users.noreply.github.com>

--- docs/source/checks/tabular/model_evaluation/plot_model_inference_time.py
+++ docs/source/checks/tabular/model_evaluation/plot_model_inference_time.py
@@ -3,11 +3,29 @@
 .. _plot_tabular_model_inference_time:
 
 Model Inference Time
-********************
+*********************
+This notebook provides an overview for using and understanding the Model Inference Time check.
+
+**Structure:**
+
+* `What is the Model Inference Time check? <#what-is-the-model-inference-time-check>`__
+* `Generate data & model <#generate-data-model>`__
+* `Run the check <#run-the-check>`__
+* `Define a condition <#define-a-condition>`__
+
+
+What is the Model Inference Time check?
+========================================
+The ``ModelInferenceTime`` check measures the model's average inference time (in seconds) per sample.
+Inference time is an important metric for prediction models, especially in real-time applications and
+data streaming processes, where a fast runtime can affect the user's experience or the overall system
+load.
+
 """
 
 #%%
-
+# Imports
+# =============
 from sklearn.datasets import load_iris
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.model_selection import train_test_split
@@ -16,7 +34,8 @@ from deepchecks.tabular import Dataset
 from deepchecks.tabular.checks import ModelInferenceTime
 
 #%%
-
+# Generate data & model
+# =======================
 iris = load_iris(as_frame=True)
 train, test = train_test_split(iris.frame, test_size=0.33, random_state=42)
 
@@ -26,13 +45,15 @@ test_ds = Dataset(test, features=iris.feature_names, label='target')
 clf = AdaBoostClassifier().fit(train_ds.data[train_ds.features], train_ds.data[train_ds.label_name])
 
 #%%
-
+# Run the check
+# =============
 check = ModelInferenceTime()
 check.run(test_ds, clf)
 
 #%%
-# Instantiating check instance with condition
-# ===========================================
-
-check = ModelInferenceTime().add_condition_inference_time_less_than(0.00001)
+# Define a condition
+# ===================
+# A condition for the average inference time per sample can be defined.
+# Here, we define the threshold to be 0.00001 seconds.
+check = ModelInferenceTime().add_condition_inference_time_less_than(value=0.00001)
 check.run(test_ds, clf)
--- docs/source/checks/tabular/model_evaluation/plot_regression_error_distribution.py
+++ docs/source/checks/tabular/model_evaluation/plot_regression_error_distribution.py
@@ -4,11 +4,27 @@
 
 Regression Error Distribution
 *****************************
+This notebook provides an overview for using and understanding the Regression Error Distribution check.
+
+**Structure:**
+
+* `What is the Regression Error Distribution check? <#what-is-the-regression-error-distribution-check>`__
+* `Run the check - normal distribution <#run-the-check-normal-distribution>`__
+* `Run the check - abnormal distribution <#run-the-check-abnormal-distribution>`__
+
+
+What is the Regression Error Distribution check?
+==================================================
+The ``RegressionErrorDistribution`` check shows the distribution of the regression error,
+and enables to set conditions on the distribution's kurtosis. Kurtosis is a measure of the shape
+of the distribution, helping us understand if the distribution is significantly "wider" from
+the normal distribution, which may imply a certain cause of error deforming the normal shape.
+
 """
 
 #%%
 # Imports
-# =======
+# ========
 
 from sklearn.datasets import load_diabetes
 from sklearn.ensemble import GradientBoostingRegressor
@@ -18,8 +34,12 @@ from deepchecks.tabular import Dataset
 from deepchecks.tabular.checks import RegressionErrorDistribution
 
 #%%
-# Generating data
-# ===============
+# Run the check - normal distribution
+# ====================================
+
+#%%
+# Generate data & model
+# ----------------------
 
 diabetes_df = load_diabetes(return_X_y=False, as_frame=True).frame
 train_df, test_df = train_test_split(diabetes_df, test_size=0.33, random_state=42)
@@ -31,27 +51,33 @@ clf = GradientBoostingRegressor(random_state=0)
 _ = clf.fit(train.data[train.features], train.data[train.label_name])
 
 #%%
-# Running RegressionErrorDistribution check (normal distribution)
-# ===============================================================
-
+# Run the check (normal distribution)
+# ---------------------------------------
+# Since the following distribution resembles the normal distribution,
+# the kurtosis will be ~0.
 check = RegressionErrorDistribution()
-
-#%%
-
 check.run(test, clf)
+
 
 #%%
 # Skewing the data
-# ----------------
+# ------------------
 
 test.data[test.label_name] = 150
 
+
 #%%
-# Running RegressionErrorDistribution check (abnormal distribution)
-# =================================================================
+# Run the check - abnormal distribution
+# =======================================
 
 check = RegressionErrorDistribution()
+check.run(test, clf)
 
 #%%
+# Define a condition
+# --------------------
+# Since we artificially skewed the target variable, the kurtosis would be bigger than
+# the previous check, which implies a certain cause of error.
 
-check.run(test, clf)
+check = RegressionErrorDistribution()
+check.add_condition_kurtosis_greater_than(0.1).run(test, clf)
