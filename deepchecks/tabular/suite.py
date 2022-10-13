4a55424961ed11d26f6a0a612eee65701e95806e
Sol <99395146+TheSolY@users.noreply.github.com>
2022-10-13 14:14:22 +0300

    2027 single dataset check select dataset (#2091)
    It's now possible to run the single datasets checks in a suite on only Train, only Test, or both.

--- deepchecks/tabular/suite.py
+++ deepchecks/tabular/suite.py
@@ -51,6 +51,7 @@ class Suite(BaseSuite):
         y_pred_test: Optional[np.ndarray] = None,
         y_proba_train: Optional[np.ndarray] = None,
         y_proba_test: Optional[np.ndarray] = None,
+        run_single_dataset: Optional[str] = None
     ) -> SuiteResult:
         """Run all checks.
 
@@ -62,6 +63,8 @@ class Suite(BaseSuite):
             object, representing data an estimator predicts on
         model : Optional[BasicModel] , default None
             A scikit-learn-compatible fitted estimator instance
+        run_single_dataset: Optional[str], default None
+            'Train', 'Test' , or None to run on both train and test.
         {additional_context_params:2*indent}
 
         Returns
@@ -103,7 +106,7 @@ class Suite(BaseSuite):
                         msg = 'Check is irrelevant if not supplied with both train and test datasets'
                         results.append(Suite._get_unsupported_failure(check, msg))
                 elif isinstance(check, SingleDatasetCheck):
-                    if train_dataset is not None:
+                    if train_dataset is not None and (run_single_dataset in [DatasetKind.TRAIN.value, None]):
                         # In case of train & test, doesn't want to skip test if train fails. so have to explicitly
                         # wrap it in try/except
                         try:
@@ -115,7 +118,7 @@ class Suite(BaseSuite):
                         except Exception as exp:
                             check_result = CheckFailure(check, exp, ' - Train Dataset')
                         results.append(check_result)
-                    if test_dataset is not None:
+                    if test_dataset is not None and (run_single_dataset in [DatasetKind.TEST.value, None]):
                         try:
                             check_result = check.run_logic(context, dataset_kind=DatasetKind.TEST)
                             context.finalize_check_result(check_result, check, DatasetKind.TEST)
