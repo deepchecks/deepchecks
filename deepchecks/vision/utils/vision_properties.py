bdadbc23ce9eb7d54213fc904133540bdcef44f2
TheSolY <solyarkoni@gmail.com>
2022-10-20 18:18:34 +0300

    link to doc

--- deepchecks/vision/utils/vision_properties.py
+++ deepchecks/vision/utils/vision_properties.py
@@ -140,7 +140,8 @@ def static_properties_from_df(df,
     """
     Transform the pre-calculated properties from a DataFrame to the expected dict format.
     
-    Read more about the excepted dict format for pre-calculated properties at X
+    Read more about the excepted dict format for pre-calculated properties at
+    https://docs.deepchecks.com/stable/user-guide/vision/vision_properties.html
 
     Parameters
     ----------
