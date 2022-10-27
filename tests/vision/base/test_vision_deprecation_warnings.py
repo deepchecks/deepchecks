d2b87c292ecc524db221a2c7e4b660123b8bdf5f
TheSolY <solyarkoni@gmail.com>
2022-10-27 13:34:25 +0300

    vision unittest

--- tests/vision/base/test_vision_deprecation_warnings.py
+++ tests/vision/base/test_vision_deprecation_warnings.py
@@ -10,3 +10,15 @@
 #
 """Contains unit tests for the vision package deprecation warnings."""
 
+import pytest
+import torch
+from deepchecks.vision.checks import SingleDatasetPerformance
+
+
+def test_deprecation_train_predictions(mnist_dataset_train):
+    pred_train = torch.rand((mnist_dataset_train.num_samples, 10))
+    pred_train = pred_train / torch.sum(pred_train, dim=1, keepdim=True)
+    pred_train_dict = dict(zip(range(mnist_dataset_train.num_samples), pred_train))
+    with pytest.warns(DeprecationWarning,
+                      match='train_predictions is deprecated, please use predictions instead.'):
+        _ = SingleDatasetPerformance().run(mnist_dataset_train, train_predictions=pred_train_dict)
