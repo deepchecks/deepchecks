61b9ec147718f0f02f16a2460badb8694be0445e
Nir Hutnik <92314933+nirhutnik@users.noreply.github.com>
2022-12-08 03:25:49 +0200

    typo fixes (#2191)
    * Some fixes
    
    * Some fixes
    
    * fixed dependencies + unfixed something i destroyed

--- deepchecks/vision/checks/train_test_validation/new_labels.py
+++ deepchecks/vision/checks/train_test_validation/new_labels.py
@@ -60,7 +60,7 @@ def draw_image(data: VisionData, sample_index: int, class_id: int) -> str:
 
 
 class NewLabels(TrainTestCheck, ReduceMixin):
-    """Detects labels that apper only in the test set.
+    """Detects labels that appear only in the test set.
 
     Parameters
     ----------
@@ -160,7 +160,7 @@ class NewLabels(TrainTestCheck, ReduceMixin):
         Parameters
         ----------
         max_allowed_new_labels_ratio: float , default: 0.005
-            the max threshold for percentage of labels that only apper in the test set.
+            the max threshold for percentage of labels that only appear in the test set.
         """
 
         def condition(result: Dict) -> ConditionResult:
