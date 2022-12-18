1399185b75ebc6aca960d3c99b977453f81b16bb
Noam Bressler <noamzbr@gmail.com>
2022-12-18 17:18:12 +0200

    rename classification_label to multiclass

--- docs/source/checks/tabular/train_test_validation/plot_new_label_train_test.py
+++ docs/source/checks/tabular/train_test_validation/plot_new_label_train_test.py
@@ -17,15 +17,15 @@ from deepchecks.tabular.checks import NewLabelTrainTest
 
 test_data = {"col1": [0, 1, 2, 3] * 10}
 val_data = {"col1": [4, 5, 6, 7, 8, 9] * 10}
-test = Dataset(pd.DataFrame(data=test_data), label="col1", label_type="classification_label")
-val = Dataset(pd.DataFrame(data=val_data), label="col1", label_type="classification_label")
+test = Dataset(pd.DataFrame(data=test_data), label="col1", label_type="multiclass")
+val = Dataset(pd.DataFrame(data=val_data), label="col1", label_type="multiclass")
 
 #%%
 
 test_data = {"col1": ["a", "b", "a", "c"] * 10, "col2": [1,2,2,3]*10}
 val_data = {"col1": ["a","b","d"] * 10, "col2": [1, 4, 5]*10}
-test = Dataset(pd.DataFrame(data=test_data), label="col2", label_type="classification_label")
-val = Dataset(pd.DataFrame(data=val_data), label="col2", label_type="classification_label")
+test = Dataset(pd.DataFrame(data=test_data), label="col2", label_type="multiclass")
+val = Dataset(pd.DataFrame(data=val_data), label="col2", label_type="multiclass")
 
 #%%
 NewLabelTrainTest().run(test, val)
