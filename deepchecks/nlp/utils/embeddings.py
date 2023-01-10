c30aec0365b19a3bd79b37450b38cd5d6d93619f
nirhutnik <nirhutnik@gmail.com>
2023-01-10 13:54:14 +0200

    fixes

--- deepchecks/nlp/utils/embeddings.py
+++ deepchecks/nlp/utils/embeddings.py
@@ -13,6 +13,7 @@
 
 __all__ = ['get_default_embeddings']
 
+import os
 import warnings
 import asyncio
 
@@ -54,6 +55,8 @@ def get_default_embeddings(dataset: TextData, model: str = 'miniLM', file_path: 
         embeddings = model.encode(dataset.text)
         embeddings = pd.DataFrame(embeddings, index=dataset.index)
     elif model == 'open_ai':
+
+        # Importing here to avoid importing openai if not needed:
         try:
             import openai
         except ImportError as e:
@@ -63,36 +66,36 @@ def get_default_embeddings(dataset: TextData, model: str = 'miniLM', file_path: 
             ) from e
 
         async def _get_open_ai_embedding(text, model="text-embedding-ada-002"):
+            # Clean text:
             text = text.replace("\n", " ")
             text = text.replace("<br />", " ")
             text = clean_special_chars(text)
+
+            # Get embedding from openai:
             try:
-                embedding = openai.Embedding.create(input=[text], model=model)['data'][0]['embedding']
+                embedding = (await openai.Embedding.acreate(input=[text], model=model))['data'][0]['embedding']
             except:
+                # If server didn't respond, return zeros:
                 embedding = np.zeros(1536)
                 warnings.warn(f"Failed to get embedding for sample")
             return embedding
 
-        from datetime import datetime
+        async def run_many_openapi_request():
+            return await asyncio.gather(*[_get_open_ai_embedding(t) for t in dataset.text])
 
-        start = datetime.now()
+        # Check if the current os is windows. If so, use the correct event loop:
+        if os.name == 'nt':
+            asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-        embeddings = [[]] * len(dataset.text)
-        for i in range(dataset.n_samples): # Doesn't actually works
-            embeddings[i] = asyncio.run(_get_open_ai_embedding(dataset.text[i]))
-
+        embeddings = asyncio.run(run_many_openapi_request())
         embeddings = pd.DataFrame(embeddings, index=dataset.index)
-
-        print(f"Time to get embeddings: {datetime.now() - start}")
     else:
         raise ValueError(f"Invalid type {type}. Can be either 'miniLM' or 'open_ai'")
 
     if file_path:
-        embeddings.to_csv(file_path, index=dataset.index)
+        embeddings.to_csv(file_path, index=True)
 
     return embeddings
-
-
 
 
 def clean_special_chars(text):
