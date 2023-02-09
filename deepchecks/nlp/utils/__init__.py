1b1eddf2e02c961bb11929d708f45c130c0f27a1
Nadav-Barak <nadavbarak148@gmail.com>
2023-02-09 13:15:15 +0200

    CR comments

--- deepchecks/nlp/utils/__init__.py
+++ deepchecks/nlp/utils/__init__.py
@@ -1,5 +1,5 @@
 # ----------------------------------------------------------------------------
-# Copyright (C) 2021-2022 Deepchecks (https://www.deepchecks.com)
+# Copyright (C) 2021-2023 Deepchecks (https://www.deepchecks.com)
 #
 # This file is part of Deepchecks.
 # Deepchecks is distributed under the terms of the GNU Affero General
@@ -11,13 +11,13 @@
 """Utils package for nlp functionality."""
 
 from deepchecks.nlp.utils.embeddings_calculator import calculate_embeddings_for_text
-from deepchecks.nlp.utils.embeddings_display import (create_drift_display, create_outlier_display,
-                                                     create_performance_display)
+from deepchecks.nlp.utils.embeddings_display import create_drift_files, create_outlier_files, create_performance_files
 
 __all__ = [
     'calculate_embeddings_for_text',
-    'create_performance_display',
-    'create_drift_display',
-    'create_outlier_display',
+    'create_performance_files',
+    'create_drift_files',
+    'create_outlier_files',
 ]
+
 
--- deepchecks/nlp/utils/embeddings_display.py
+++ deepchecks/nlp/utils/embeddings_display.py
@@ -1,5 +1,5 @@
 # ----------------------------------------------------------------------------
-# Copyright (C) 2021-2022 Deepchecks (https://www.deepchecks.com)
+# Copyright (C) 2021-2023 Deepchecks (https://www.deepchecks.com)
 #
 # This file is part of Deepchecks.
 # Deepchecks is distributed under the terms of the GNU Affero General
@@ -28,16 +28,15 @@ from deepchecks.nlp.utils.embeddings_calculator import clean_special_chars
 from deepchecks.utils.dataframes import floatify_dataframe
 
 
-def create_performance_display(text: pd.Series, embeddings: np.ndarray, proba: np.ndarray, y_true: np.ndarray,
-                                 labels: List[str], path: str, sample_size: int = 10000, verbose: bool = False,
-                                 indexes_to_highlight: Dict[str, List[int]] = None):
+def create_performance_files(text: pd.Series, embeddings: np.ndarray, proba: np.ndarray, y_true: np.ndarray,
+                             labels: List[str], path: str, sample_size: int = 10000, verbose: bool = False,
+                             indexes_to_highlight: Dict[str, List[int]] = None):
     if list(sorted(labels)) != list(labels):
         raise DeepchecksValueError('Labels must be sorted in an alphanumeric order')
     if not os.path.exists(path):
         os.makedirs(path)
 
-    loss_per_sample = [log_loss([y_true], [y_proba], labels=sorted(labels)) for
-                       y_true, y_proba in zip(y_true, proba)]
+    loss_per_sample = [log_loss([y_true], [y_proba], labels=sorted(labels)) for y_true, y_proba in zip(y_true, proba)]
 
     text_dataframe = pd.DataFrame(text)
     text_dataframe.columns = ['text']
@@ -45,23 +44,25 @@ def create_performance_display(text: pd.Series, embeddings: np.ndarray, proba: n
     text_dataframe['label'] = y_true
     text_dataframe['prediction'] = [labels[np.argmax(x)] for x in proba]
     text_dataframe['model uncertainty'] = [(1 - np.max(x)) for x in proba]
-    text_dataframe['correct prediction'] = [x == y for x, y in zip(text_dataframe['label'], text_dataframe['prediction'])]
+    text_dataframe['correct prediction'] = [x == y for x, y in
+                                            zip(text_dataframe['label'], text_dataframe['prediction'])]
 
     if indexes_to_highlight is not None:
-        text_dataframe['highlight criteria'] = [_select_highlight_value(x, indexes_to_highlight) for x in text_dataframe.index]
+        text_dataframe['highlight criteria'] = [_select_highlight_value(x, indexes_to_highlight) for x in
+                                                text_dataframe.index]
     text_dataframe['text'] = text_dataframe['text'].apply(clean_special_chars)
     text_dataframe = text_dataframe[['text'] + [col for col in text_dataframe.columns if col != 'text']]
 
     # save values for display
     text_dataframe.to_csv(os.path.join(path, "performance_metadata.tsv"), sep="\t")
-    checkpoint = tf.train.Checkpoint(performance_embedding=tf.Variable(pd.DataFrame(embeddings, index=text_dataframe.index)))
+    checkpoint = tf.train.Checkpoint(
+        performance_embedding=tf.Variable(pd.DataFrame(embeddings, index=text_dataframe.index)))
     checkpoint.save(os.path.join(path, "performance_embedding.ckpt"))
 
 
-def create_outlier_display(text: pd.Series, embeddings: np.ndarray,
-                             path: str, nearest_neighbors_percent: float = 0.01, extent_parameter: int = 3,
-                             sample_size: int = 10000, verbose: bool = False,
-                             indexes_to_highlight: Dict[str, List[int]] = None):
+def create_outlier_files(text: pd.Series, embeddings: np.ndarray, path: str, nearest_neighbors_percent: float = 0.01,
+                         extent_parameter: int = 3, sample_size: int = 10000, verbose: bool = False,
+                         indexes_to_highlight: Dict[str, List[int]] = None):
     if not os.path.exists(path):
         os.makedirs(path)
     start_time = time.time()
@@ -79,7 +80,8 @@ def create_outlier_display(text: pd.Series, embeddings: np.ndarray,
     text_dataframe['outlier probability'] = prob_vector
 
     if indexes_to_highlight is not None:
-        text_dataframe['highlight criteria'] = [_select_highlight_value(x, indexes_to_highlight) for x in text_dataframe.index]
+        text_dataframe['highlight criteria'] = [_select_highlight_value(x, indexes_to_highlight) for x in
+                                                text_dataframe.index]
     # make text be the first column
     text_dataframe['text'] = text_dataframe['text'].apply(clean_special_chars)
     text_dataframe = text_dataframe[['text'] + [col for col in text_dataframe.columns if col != 'text']]
@@ -87,7 +89,8 @@ def create_outlier_display(text: pd.Series, embeddings: np.ndarray,
         print('finished calculating outlier probability score after {} seconds'.format(time.time() - start_time))
         print(f'Avg outlier score is {np.mean(prob_vector)}')
         if indexes_to_highlight is not None:
-            print(f'Avg outlier score in real outlier samples is {np.mean(prob_vector[text_dataframe.index.isin(indexes_to_highlight["real outlier samples"])])}')
+            print(
+                f'Avg outlier score in real outlier samples is {np.mean(prob_vector[text_dataframe.index.isin(indexes_to_highlight["real outlier samples"])])}')
 
     # save to file
     text_dataframe.to_csv(os.path.join(path, "outlier_metadata.tsv"), sep="\t")
@@ -96,23 +99,24 @@ def create_outlier_display(text: pd.Series, embeddings: np.ndarray,
     checkpoint = tf.train.Checkpoint(outlier_embedding=tf.Variable(embeddings_df))
     checkpoint.save(os.path.join(path, "outlier_embedding.ckpt"))
 
+
 def _select_highlight_value(index, indexes_to_highlight: Dict[str, List[int]]):
     for key, value in indexes_to_highlight.items():
         if index in value:
             return key
     return "other"
 
-def create_drift_display(train_text: pd.Series, test_text: pd.Series,
-                         train_embeddings: np.ndarray, test_embeddings: np.ndarray,
-                         path: str, sample_size: int = 2500, verbose: bool = False,
-                         indexes_to_highlight: Dict[str, List[int]] = None):
+
+def create_drift_files(train_text: pd.Series, test_text: pd.Series, train_embeddings: np.ndarray,
+                       test_embeddings: np.ndarray, path: str, sample_size: int = 2500, verbose: bool = False,
+                       indexes_to_highlight: Dict[str, List[int]] = None):
     if not os.path.exists(path):
         os.makedirs(path)
 
     train_dataframe, test_dataframe = pd.DataFrame(train_text), pd.DataFrame(test_text)
     train_dataframe.columns, test_dataframe.columns = ['text'], ['text']
-    train_embeddings = pd.DataFrame(train_embeddings, index = train_dataframe.index)
-    test_embeddings = pd.DataFrame(test_embeddings, index = test_dataframe.index)
+    train_embeddings = pd.DataFrame(train_embeddings, index=train_dataframe.index)
+    test_embeddings = pd.DataFrame(test_embeddings, index=test_dataframe.index)
 
     if len(train_dataframe) > sample_size:
         train_dataframe = train_dataframe.sample(sample_size)
@@ -130,11 +134,11 @@ def create_drift_display(train_text: pd.Series, test_text: pd.Series,
     domain_classifier = GradientBoostingClassifier(max_depth=3, subsample=0.8, min_samples_split=50, n_estimators=30)
     domain_classifier.fit(x_train, y_train)
 
-
     domain_classifier_auc = roc_auc_score(y_test, domain_classifier.predict_proba(x_test)[:, 1])
     drift_score = auc_to_drift_score(domain_classifier_auc)
     if verbose:
-        print('domain_classifier_auc on train is ', roc_auc_score(y_train, domain_classifier.predict_proba(x_train)[:, 1]))
+        print('domain_classifier_auc on train is ',
+              roc_auc_score(y_train, domain_classifier.predict_proba(x_train)[:, 1]))
         print('domain_classifier_auc on test is ', domain_classifier_auc)
         print(f'Domain classifier drift score is {drift_score:.2f}')
     train_dataframe['domain classifier proba'] = domain_classifier.predict_proba(train_embeddings)[:, 1]
@@ -153,7 +157,7 @@ def create_drift_display(train_text: pd.Series, test_text: pd.Series,
     all_data = all_data[['text'] + [col for col in all_data.columns if col != 'text']]
 
     # save data for display
-    all_data.to_csv(os.path.join(path,"drift_metadata.tsv"), sep="\t")
+    all_data.to_csv(os.path.join(path, "drift_metadata.tsv"), sep="\t")
     checkpoint = tf.train.Checkpoint(drift_embeddings=tf.Variable(all_embeddings))
     checkpoint.save(os.path.join(path, "drift_embeddings.ckpt"))
 
