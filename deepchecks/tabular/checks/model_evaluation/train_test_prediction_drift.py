ff6ec46a8bdf04e10cf186a2011bfc16898e64c1
Nir Hutnik <92314933+nirhutnik@users.noreply.github.com>
2023-02-09 18:51:17 +0200

    Update deepchecks/tabular/checks/model_evaluation/train_test_prediction_drift.py

--- deepchecks/tabular/checks/model_evaluation/train_test_prediction_drift.py
+++ deepchecks/tabular/checks/model_evaluation/train_test_prediction_drift.py
@@ -89,7 +89,7 @@ class TrainTestPredictionDrift(TrainTestCheck, ReduceMixin):
         If True, all categories will have an equal weight in the PSI score, with only the log of the ratio
         between the expected and actual category size being used. This is useful when we care about the relative
         difference between the distributions, but not about the absolute difference. Usable only when
-        categorical_drift_method = "psi"
+        categorical_drift_method = "PSI".
     ignore_na: bool, default True
         For categorical columns only. If True, ignores nones for categorical drift. If False, considers none as a
         separate category. For numerical columns we always ignore nones.
