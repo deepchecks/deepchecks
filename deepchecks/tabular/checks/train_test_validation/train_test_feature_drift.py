ae9a4d0e8fa3e1512080ec824f3cfbbf49d4d768
Itay Gabbay <itay@deepchecks.com>
2023-03-08 18:49:09 +0200

    Fixing docs build Fixing redirects

--- deepchecks/tabular/checks/train_test_validation/train_test_feature_drift.py
+++ deepchecks/tabular/checks/train_test_validation/train_test_feature_drift.py
@@ -22,6 +22,10 @@ from deepchecks.tabular.checks.train_test_validation.feature_drift import Featur
 class TrainTestFeatureDrift(FeatureDrift):
     """The TrainTestFeatureDrift check is deprecated and will be removed in the 0.14 version.
 
+    .. deprecated:: 0.14.0
+        `deepchecks.tabular.checks.TrainTestFeatureDrift is deprecated and will be removed in deepchecks 0.14 version.
+        Use `deepchecks.tabular.checks.FeatureDrift` instead.
+
     Please use the FeatureDrift check instead
     """
 
