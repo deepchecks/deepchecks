ed74240bead8f183447592f9eb260ccb199a6a47
JKL98ISR <jonatan.lib@gmail.com>
2023-03-15 18:49:25 +0200

    oop

---
+++ deepchecks/recommender/checks/label_popularity_drift.py
@@ -0,0 +1,131 @@
+# ----------------------------------------------------------------------------
+# Copyright (C) 2021-2023 Deepchecks (https://www.deepchecks.com)
+#
+# This file is part of Deepchecks.
+# Deepchecks is distributed under the terms of the GNU Affero General
+# Public License (version 3 or later).
+# You should have received a copy of the GNU Affero General Public License
+# along with Deepchecks.  If not, see <http://www.gnu.org/licenses/>.
+# ----------------------------------------------------------------------------
+#
+"""Module containing the popularity bias check."""
+import typing as t
+
+import pandas as pd
+
+from deepchecks.core import CheckResult
+from deepchecks.core.condition import ConditionCategory, ConditionResult
+
+from deepchecks.core.errors import DeepchecksValueError
+
+from deepchecks.recommender import Context
+from deepchecks.tabular import TrainTestCheck
+from deepchecks.utils.distribution.drift import calc_drift_and_plot
+
+
+class LabelPopularityDrift(TrainTestCheck):
+
+    def __init__(
+            self,
+            margin_quantile_filter: float = 0.025,
+            max_num_categories_for_drift: int = None,
+            min_category_size_ratio: float = 0.01,
+            max_num_categories_for_display: int = 10,
+            show_categories_by: str = 'largest_difference',
+            numerical_drift_method: str = 'KS',
+            categorical_drift_method: str = 'cramers_v',
+            balance_classes: bool = False,
+            ignore_na: bool = True,
+            aggregation_method: t.Optional[str] = 'max',
+            min_samples: t.Optional[int] = 10,
+            n_samples: int = 100_000,
+            random_state: int = 42,
+            **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.margin_quantile_filter = margin_quantile_filter
+        self.max_num_categories_for_drift = max_num_categories_for_drift
+        self.min_category_size_ratio = min_category_size_ratio
+        self.max_num_categories_for_display = max_num_categories_for_display
+        self.show_categories_by = show_categories_by
+        self.numerical_drift_method = numerical_drift_method
+        self.categorical_drift_method = categorical_drift_method
+        self.balance_classes = balance_classes
+        self.ignore_na = ignore_na
+        self.aggregation_method = aggregation_method
+        self.min_samples = min_samples
+        self.n_samples = n_samples
+        self.random_state = random_state
+        if self.aggregation_method not in ('weighted', 'mean', 'none', None, 'max'):
+            raise DeepchecksValueError('aggregation_method must be one of "weighted", "mean", "max", None')
+
+    def run_logic(self, context: Context, dataset_kind) -> CheckResult:
+        dataset = context.get_data_by_kind(dataset_kind).sample(self.n_samples, random_state=self.random_state)
+        model = context.model
+
+        labels = dataset.label_col.to_list()
+        predictions = model.predict(dataset.features_columns)
+        # flatten sequence of sequences to a single sequence
+        flattened_predictions = [item for sublist in predictions for item in sublist]
+
+        all_values = pd.Series(labels + flattened_predictions)
+        value_counts = all_values.value_counts(ascending=False)
+        value_counts.iloc[:] = list(range(len(value_counts)))
+        translation_dict = value_counts.to_dict()
+        labels = [translation_dict[label] for label in labels]
+        flattened_predictions = [translation_dict[prediction] for prediction in flattened_predictions]
+
+        drift_score, method, drift_display = calc_drift_and_plot(
+            train_column=pd.Series(labels),
+            test_column=pd.Series(flattened_predictions),
+            value_name='Item Popularity',
+            column_type='numerical',
+            plot_title='Popularity Bias',
+            margin_quantile_filter=self.margin_quantile_filter,
+            max_num_categories_for_drift=self.max_num_categories_for_drift,
+            min_category_size_ratio=self.min_category_size_ratio,
+            max_num_categories_for_display=self.max_num_categories_for_display,
+            show_categories_by=self.show_categories_by,
+            numerical_drift_method=self.numerical_drift_method,
+            categorical_drift_method=self.categorical_drift_method,
+            balance_classes=self.balance_classes,
+            ignore_na=self.ignore_na,
+            min_samples=self.min_samples,
+            raise_min_samples_error=True,
+            dataset_names=('Selected Items', 'Recommended Items'),
+            with_display=context.with_display,
+        )
+
+        return CheckResult(
+            value=drift_score,
+            header='Popularity Bias',
+            display=drift_display,
+        )
+
+    def add_condition_drift_score_less_than(self, max_allowed_score: float = 0.15):
+        """
+        Add condition - require drift score to be less than a certain threshold.
+
+        The industry standard for PSI limit is above 0.2.
+        There are no common industry standards for other drift methods, such as Cramer's V,
+        Kolmogorov-Smirnov and Earth Mover's Distance.
+        The threshold was lowered by 25% compared to feature drift defaults due to the higher importance of prediction
+        drift.
+
+        Parameters
+        ----------
+        max_allowed_categorical_score: float , default: 0.15
+            the max threshold for the categorical variable drift score
+        max_allowed_numeric_score: float ,  default: 0.15
+            the max threshold for the numeric variable drift score
+        Returns
+        -------
+        ConditionResult
+            False if any column has passed the max threshold, True otherwise
+        """
+
+        def condition(result: float) -> ConditionResult:
+            category = ConditionCategory.FAIL if result > max_allowed_score else ConditionCategory.PASS
+            return ConditionResult(category, f'The drift score was {result}')
+
+        return self.add_condition(f'drift score less than {max_allowed_score}', condition)
---
+++ deepchecks/recommender/checks/scatter_performance.py
@@ -0,0 +1,102 @@
+# ----------------------------------------------------------------------------
+# Copyright (C) 2021-2023 Deepchecks (https://www.deepchecks.com)
+#
+# This file is part of Deepchecks.
+# Deepchecks is distributed under the terms of the GNU Affero General
+# Public License (version 3 or later).
+# You should have received a copy of the GNU Affero General Public License
+# along with Deepchecks.  If not, see <http://www.gnu.org/licenses/>.
+# ----------------------------------------------------------------------------
+#
+"""Module containing the single dataset performance check."""
+from numbers import Number
+from typing import TYPE_CHECKING, Callable, Dict, List, Mapping, Optional, TypeVar, Union, cast
+
+import pandas as pd
+import plotly.graph_objects as go
+import plotly.express as px
+
+from deepchecks.core import CheckResult, ConditionCategory, ConditionResult
+from deepchecks.core.errors import DeepchecksValueError
+from deepchecks.core.reduce_classes import ReduceMetricClassMixin
+from deepchecks.tabular import Context
+from deepchecks.tabular.base_checks import SingleDatasetCheck
+from deepchecks.tabular.utils.task_type import TaskType
+from deepchecks.utils.docref import doclink
+from deepchecks.utils.strings import format_number
+
+if TYPE_CHECKING:
+    from deepchecks.core.checks import CheckConfig
+
+__all__ = ['ScatterPerformance']
+
+
+SDP = TypeVar('SDP', bound='ScatterPerformance')
+
+
+class ScatterPerformance(SingleDatasetCheck):
+    """Summarize given model performance on the train and test datasets based on selected scorers.
+
+    Parameters
+    ----------
+    scorers: Union[Mapping[str, Union[str, Callable]], List[str]], default: None
+        Scorers to override the default scorers, find more about the supported formats at
+        https://docs.deepchecks.com/stable/user-guide/general/metrics_guide.html
+    n_samples : int , default: 1_000_000
+        number of samples to use for this check.
+    random_state : int, default: 42
+        random seed for all check internals.
+    """
+
+    def __init__(self,
+                 scorers: Optional[Union[Mapping[str, Union[str, Callable]], List[str]]] = None,
+                 n_samples: int = 1_000_000,
+                 random_state: int = 42,
+                 **kwargs):
+        super().__init__(**kwargs)
+        self.scorers = scorers
+        self.n_samples = n_samples
+        self.random_state = random_state
+
+    def run_logic(self, context: Context, dataset_kind) -> CheckResult:
+        """Run check."""
+        dataset = context.get_data_by_kind(dataset_kind).sample(self.n_samples, random_state=self.random_state)
+        model = context.model
+        scorers = context.get_scorers(self.scorers, use_avg_defaults=False)[:2]
+
+        results_df = pd.DataFrame()
+        results_df[scorers[0].name] = scorers[0](model, dataset)
+        results_df[scorers[1].name] = scorers[1](model, dataset)
+
+        display = None
+        if context.with_display:
+            display = px.scatter(results_df, x=scorers[0].name, y=scorers[1].name)
+
+        return CheckResult(results_df, display=display)
+
+    def config(
+        self,
+        include_version: bool = True,
+        include_defaults: bool = True
+    ) -> 'CheckConfig':
+        """Return check configuration."""
+        if isinstance(self.scorers, dict):
+            for k, v in self.scorers.items():
+                if not isinstance(v, str):
+                    reference = doclink(
+                        'supported-metrics-by-string',
+                        template='For a list of built-in scorers please refer to {link}'
+                    )
+                    raise ValueError(
+                        'Only built-in scorers are allowed when serializing check instances. '
+                        f'{reference}. Scorer name: {k}'
+                    )
+        return super().config(include_version=include_version, include_defaults=include_defaults)
+
+    def reduce_output(self, check_result: CheckResult) -> Dict[str, float]:
+        """Return the values of the metrics for the dataset provided in a {metric: value} format."""
+        result = {}
+        for _, row in check_result.value.iterrows():
+            key = row['Metric'] if pd.isna(row.get('Class')) else (row['Metric'], str(row['Class']))
+            result[key] = row['Value']
+        return result
