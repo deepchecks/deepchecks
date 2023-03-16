dcce97f7ed486c531523432728afcca0c64d3dfc
nirhutnik <nirhutnik@gmail.com>
2023-03-16 11:02:47 +0200

    Added fix by oversampling to weak segments

--- deepchecks/tabular/checks/model_evaluation/weak_segments_performance.py
+++ deepchecks/tabular/checks/model_evaluation/weak_segments_performance.py
@@ -9,15 +9,18 @@
 # ----------------------------------------------------------------------------
 #
 """Module of weak segments performance check."""
-
-from typing import TYPE_CHECKING, Callable, Dict, List, Union
+import warnings
+from copy import copy
+from typing import TYPE_CHECKING, Callable, Dict, List, Union, Tuple
 
 import numpy as np
 import pandas as pd
+from imblearn.over_sampling import SMOTENC
 
 from deepchecks.core import CheckResult
 from deepchecks.core.check_result import DisplayMap
 from deepchecks.core.errors import DeepchecksNotSupportedError, DeepchecksProcessError, DeepchecksValueError
+from deepchecks.core.fix_classes import SingleDatasetCheckFixMixin
 from deepchecks.tabular import Context, SingleDatasetCheck
 from deepchecks.tabular.context import _DummyModel
 from deepchecks.tabular.utils.task_type import TaskType
@@ -26,13 +29,13 @@ from deepchecks.utils.performance.weak_segment_abstract import WeakSegmentAbstra
 from deepchecks.utils.single_sample_metrics import calculate_per_sample_loss
 from deepchecks.utils.typing import Hashable
 
-if TYPE_CHECKING:
-    from deepchecks.core.checks import CheckConfig
+# if TYPE_CHECKING:
+from deepchecks.core.checks import CheckConfig, DatasetKind
 
 __all__ = ['WeakSegmentsPerformance']
 
 
-class WeakSegmentsPerformance(SingleDatasetCheck, WeakSegmentAbstract):
+class WeakSegmentsPerformance(SingleDatasetCheck, WeakSegmentAbstract, SingleDatasetCheckFixMixin):
     """Search for segments with low performance scores.
 
     The check is designed to help you easily identify weak spots of your model and provide a deepdive analysis into
@@ -172,3 +175,130 @@ class WeakSegmentsPerformance(SingleDatasetCheck, WeakSegmentAbstract):
                         f'{reference}Scorer name: {k}'
                     )
         return super().config(include_version, include_defaults=include_defaults)
+
+    def fix_logic(self, context: Context, check_result, dataset_kind, oversample_by: str = 'duplicates',
+                  num_segment_to_fix: int = 1, oversample_factor: Union[float, str] = 'auto') -> Context:
+        """Run fix.
+
+        Parameters
+        ----------
+        context: Context
+            The context object to fix
+        check_result: CheckResult
+            The check result to fix
+        dataset_kind: DatasetKind
+            The dataset kind to fix
+        oversample_by: str
+            The method to use for oversampling. Can be 'duplicates' or 'smote'
+        num_segment_to_fix: int
+            The number of segments to fix
+        oversample_factor: Union[float, str]
+            The factor to use for oversampling. If 'auto' is given, the factor will be calculated automatically.
+        """
+        if oversample_factor == 'auto':
+            oversample_factor = 2  # TODO Temporarily set to 2, need to calculate the factor
+        elif oversample_factor < 1:
+            raise DeepchecksValueError('Oversample factor must be greater than 1')
+
+        # Fix will always be done on train dataset:
+        dataset = context.get_data_by_kind(DatasetKind.TRAIN)
+        data = dataset.data.copy()
+
+        weak_segments = check_result.value['weak_segments_list'].iloc[:num_segment_to_fix]
+
+        new_i = 0
+
+        for _, segment in weak_segments.iterrows():
+            feature_1, feature_2 = segment['Feature1'], segment['Feature2']
+            feature_1_range, feature_2_range = segment['Feature1 range'], segment['Feature2 range']
+
+            feature_1_samples = get_samples_by_range(s=data[feature_1], values_range=feature_1_range,
+                                                     is_categorical=feature_1 in dataset.cat_features)
+            if feature_2 is not None:
+                feature_2_samples = get_samples_by_range(s=data[feature_2], values_range=feature_2_range,
+                                                         is_categorical=feature_2 in dataset.cat_features)
+                relevant_samples = data.loc[feature_1_samples & feature_2_samples]
+            else:
+                relevant_samples = data.loc[feature_1_samples]
+
+            if relevant_samples.empty:
+                warnings.warn(f'No samples were found for segment, cannot fix.')
+            else:
+                if oversample_by == 'duplicates':
+                    additional_samples = relevant_samples.sample(frac=oversample_factor - 1, replace=True,
+                                                                 random_state=self.random_state)
+                    additional_samples.index = [f'duplicate_{i}' for i in range(new_i, new_i + len(additional_samples))]
+                    new_i += len(additional_samples)
+                elif oversample_by.lower() == 'smote':
+                    # SMOTENC requires a label, we'll give the label as whether the sample is from the weak segment
+                    # or not:
+                    irrelevant_samples = data[~data.index.isin(relevant_samples.index)]
+                    data_to_smote = pd.concat([relevant_samples, irrelevant_samples])
+                    label_to_smote = pd.Series([0] * relevant_samples.shape[0] + [1] * irrelevant_samples.shape[0],
+                                               index=data.index)
+
+                    n_samples_for_smote = int(len(relevant_samples) * oversample_factor)
+                    n_samples_to_add = n_samples_for_smote - len(relevant_samples)
+
+                    # SMOTE requires categorical features to be encoded as their location in the data:
+                    cat_features_for_smote = copy(dataset.cat_features)
+                    if context.task_type != TaskType.REGRESSION:
+                        cat_features_for_smote.append(dataset.label_name)
+                    cat_indexes_for_smote = [data_to_smote.columns.get_loc(cat_feature) for cat_feature in
+                                             cat_features_for_smote]
+
+                    smote = SMOTENC(categorical_features=cat_indexes_for_smote, random_state=self.random_state,
+                                    sampling_strategy={0: n_samples_for_smote})
+                    additional_samples = smote.fit_resample(data_to_smote, label_to_smote)[0][-n_samples_to_add:]
+                    additional_samples.index = [f'generated_{i}' for i in
+                                                range(new_i, new_i + n_samples_to_add)]
+                    new_i += n_samples_to_add
+
+            data = pd.concat([data, additional_samples])
+
+        context.set_dataset_by_kind(DatasetKind.TRAIN, dataset.copy(data))
+        return context
+
+    @property
+    def fix_params(self):
+        """Return fix params for display."""
+        return {'oversample_by': {'display': 'Oversample By',
+                                  'params': ['duplicates', 'smote'],
+                                  'params_display': ['Duplicating Samples', 'Using SMOTE'],
+                                  'params_description': ['Duplicate samples', 'Use SMOTE to generate new samples']},
+                'num_segment_to_fix': {'display': 'Number of Segments to Fix',
+                                       'params': int,
+                                       'params_display': 1,
+                                       'min_value': 1,
+                                       'params_description': ['Automatically calculate the number of segments to fix',
+                                                              'Fix 2 segments', 'Fix 3 segments', 'Fix 4 segments',
+                                                              ]},
+                'oversample_factor': {'display': 'Oversample Multiplier',
+                                      'params': ['auto', 2, 3, 4, 5],
+                                      'params_display': ['Auto', 'X2', 'X3', 'X4', 'X5'],
+                                      'params_description': ['Automatically calculate the number of segments to fix',
+                                                             'Oversample by 2', 'Oversample by 3', 'Oversample by 4',
+                                                             'Oversample by 5']}}
+
+    @property
+    def problem_description(self):
+        """Return problem description."""
+        return """Duplicate data samples are present in the dataset. This can lead to overfitting and
+                  decrease the performance of the model."""
+
+    @property
+    def manual_solution_description(self):
+        """Return manual solution description."""
+        return """Remove duplicate samples."""
+
+    @property
+    def automatic_solution_description(self):
+        """Return automatic solution description."""
+        return """Remove duplicate samples."""
+
+
+def get_samples_by_range(s: pd.Series, values_range: Union[List, Tuple], is_categorical: bool):
+    if is_categorical:
+        return s[s.isin(values_range)].index
+    else:
+        return s[s.between(*values_range)].index
