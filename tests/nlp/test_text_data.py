4494689b45409729b459d5795b2905e06edb0adc
nirhutnik <nirhutnik@gmail.com>
2023-03-23 12:47:51 +0200

    Added tests

--- tests/nlp/test_text_data.py
+++ tests/nlp/test_text_data.py
@@ -137,15 +137,58 @@ def test_head_functionality():
 
 def test_properties(text_classification_dataset_mock):
     # Arrange
-    dataset = text_classification_dataset_mock
+    dataset = text_classification_dataset_mock.copy()
 
     # Act & Assert
     assert_that(dataset._properties, equal_to(None))  # pylint: disable=protected-access
     dataset.calculate_default_properties(ignore_properties=['topic'])
     properties = dataset.properties
     assert_that(properties.shape[0], equal_to(3))
-    assert_that(properties.shape[1], equal_to(6))
+    assert_that(properties.shape[1], equal_to(7))
     assert_that(properties.columns,
-                contains_exactly('text_length', 'average_word_length', 'percentage_special_characters', 'language',
-                                 'sentiment', 'subjectivity'))
-    assert_that(properties.iloc[0].values, contains_exactly(22, 3.6, 0.0, 'en', 0.0, 0.0))
+                contains_exactly('text_length',  'average_word_length', 'percentage_special_characters',
+                                 'max_word_length', 'language', 'sentiment', 'subjectivity'))
+    assert_that(properties.iloc[0].values, contains_exactly(22, 3.6, 0.0, 9, 'en', 0.0, 0.0))
+
+
+def test_set_metadata(text_classification_dataset_mock):
+    # Arrange
+    dataset = text_classification_dataset_mock
+    metadata = pd.DataFrame({'first': [1, 2, 3], 'second': [4, 5, 6]})
+
+    assert_that(dataset._metadata, equal_to(None))  # pylint: disable=protected-access
+    assert_that(dataset._metadata_types, equal_to(None))  # pylint: disable=protected-access
+
+    # Act
+    dataset.set_metadata(metadata)
+
+    # Assert
+    assert_that((dataset.metadata != metadata).sum().sum(), equal_to(0))
+    assert_that(dataset.metadata_types, equal_to({'first': 'categorical', 'second': 'categorical'}))
+
+    dataset._metadata = None
+    dataset._metadata_types = None
+
+    dataset.set_metadata(metadata, metadata_types={'first': 'numeric', 'second': 'numeric'})
+    assert_that(dataset.metadata_types, equal_to({'first': 'numeric', 'second': 'numeric'}))
+
+
+def test_set_properties(text_classification_dataset_mock):
+    # Arrange
+    dataset = text_classification_dataset_mock
+    properties = pd.DataFrame({'text_length': [1, 2, 3], 'average_word_length': [4, 5, 6]})
+
+    assert_that(dataset._properties, equal_to(None))  # pylint: disable=protected-access
+    assert_that(dataset._properties_types, equal_to(None))  # pylint: disable=protected-access
+    # Act
+    dataset.set_properties(properties)
+
+    # Assert
+    assert_that((dataset.properties != properties).sum().sum(), equal_to(0))
+
+    dataset._properties = None
+    dataset._metadata_types = None
+
+    dataset.set_properties(properties, properties_types={'text_length': 'numeric', 'average_word_length': 'numeric'})
+    assert_that(dataset.properties_types, equal_to({'text_length': 'numeric', 'average_word_length': 'numeric'}))
+
