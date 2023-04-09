7c72748ecd37cb07d93111ece1bfebc4d6a60b6d
Matan Perlmutter <matan@deepchecks.com>
2023-04-09 11:02:26 +0300

    Improve column type inference

--- deepchecks/tabular/dataset.py
+++ deepchecks/tabular/dataset.py
@@ -537,23 +537,6 @@ class Dataset:
         return self.copy(train_df), self.copy(test_df)
 
     @staticmethod
-    def _infer_label_type(label_col: pd.Series):
-        if is_categorical(label_col, max_categorical_ratio=0.05):
-            if label_col.nunique(dropna=True) > 2:
-                if infer_dtype(label_col) == 'integer' \
-                        and label_col.nunique() >= 5:
-                    get_logger().warning(
-                        'Integer label has many unique values. In this case, deepchecks automatically '
-                        'infers label to be multiclass. If your label is a regression label and not multiclass, '
-                        'please use "label_type=\'regression\'" when initializing your Dataset.'
-                    )
-                return TaskType.MULTICLASS
-            else:
-                return TaskType.BINARY
-        else:
-            return TaskType.REGRESSION
-
-    @staticmethod
     def _infer_categorical_features(
             df: pd.DataFrame,
             max_categorical_ratio: float,
