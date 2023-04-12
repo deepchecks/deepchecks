d286a1cbdd416a4e006e117e3d584e685c7150e8
Yurii Romanyshyn <yurii.romanyshyn@starnavi.io>
2023-04-12 16:55:27 +0300

    code style fixes

--- deepchecks/nlp/metric_utils/token_classification.py
+++ deepchecks/nlp/metric_utils/token_classification.py
@@ -24,7 +24,8 @@ DEFAULT_AVG_SCORER_NAMES = ('f1_macro', 'recall_macro', 'precision_macro')
 DEFAULT_PER_CLASS_SCORER_NAMES = tuple()
 
 # see issue DEE-473
-#https://linear.app/deepchecks/issue/DEE-473/incorrectly-inferred-model-classes-for-token-classification-task
+# https://linear.app/deepchecks/issue/DEE-473/incorrectly-inferred-model-classes-for-token-classification-task
+#
 # DEFAULT_PER_CLASS_SCORER_NAMES = ('f1_per_class',)
 
 
