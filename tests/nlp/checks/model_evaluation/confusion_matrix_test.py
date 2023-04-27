fde36ab836fbbd0dfef771120f0cb5155e672d41
Kishore Sampath <kishore@gitaa.in>
2023-04-27 23:06:47 +0530

    Added tests

--- tests/nlp/checks/model_evaluation/confusion_matrix_test.py
+++ tests/nlp/checks/model_evaluation/confusion_matrix_test.py
@@ -11,8 +11,11 @@
 """Test for the nlp SingleDatasetPerformance check"""
 
 from hamcrest import assert_that, close_to, equal_to
+from deepchecks.core.condition import ConditionCategory
 
 from deepchecks.nlp.checks.model_evaluation import ConfusionMatrixReport
+from deepchecks.utils.strings import format_number
+from tests.base.utils import equal_condition_result
 
 
 def test_defaults(text_classification_dataset_mock):
@@ -68,3 +71,65 @@ def test_run_tweet_emotion(tweet_emotion_train_test_textdata, tweet_emotion_trai
     # Assert
     assert_that(result.value[0][0], close_to(1160, 0.001))
     assert_that(result.value.shape[0], close_to(4, 0.001))
+
+
+def test_condition_misclassified_samples_lower_than_raises_error(tweet_emotion_train_test_textdata,
+                                                                 tweet_emotion_train_test_predictions):
+    
+    # Arrange
+    _, test_ds = tweet_emotion_train_test_textdata
+    _, test_preds = tweet_emotion_train_test_predictions
+
+    check = ConfusionMatrixReport() \
+            .add_condition_misclassified_samples_lower_than_condition(misclassified_samples_threshold=-0.1) \
+            .add_condition_misclassified_samples_lower_than_condition(misclassified_samples_threshold=1.1)
+    
+    result = check.run(test_ds, predictions=test_preds)
+
+    # Assert
+    assert_that(result.conditions_results[0], equal_condition_result(
+        is_pass=False,
+        name=f'Misclassified cell size lower than {format_number(-0.1 * 100)}% of the total samples',
+        details='Exception in condition: ValidationError: Condition requires the parameter "misclassified_samples_threshold" '
+                'to be between 0 and 1 inclusive but got -0.1',
+        category=ConditionCategory.ERROR
+    ))
+
+    assert_that(result.conditions_results[1], equal_condition_result(
+        is_pass=False,
+        name=f'Misclassified cell size lower than {format_number(1.1 * 100)}% of the total samples',
+        details='Exception in condition: ValidationError: Condition requires the parameter "misclassified_samples_threshold" '
+                'to be between 0 and 1 inclusive but got 1.1',
+        category=ConditionCategory.ERROR
+    ))
+
+
+def test_condition_misclassified_samples_lower_than(tweet_emotion_train_test_textdata,
+                                                    tweet_emotion_train_test_predictions):
+    
+    # Arrange
+    _, test_ds = tweet_emotion_train_test_textdata
+    _, test_preds = tweet_emotion_train_test_predictions
+
+    check = ConfusionMatrixReport() \
+            .add_condition_misclassified_samples_lower_than_condition(misclassified_samples_threshold=0.1) \
+            .add_condition_misclassified_samples_lower_than_condition(misclassified_samples_threshold=0.01)
+    
+    # Act
+    result = check.run(test_ds, predictions=test_preds)
+
+    # Assert
+    assert_that(result.conditions_results[0], equal_condition_result(
+        is_pass=True,
+        name=f'Misclassified cell size lower than {format_number(0.1 * 100)}% of the total samples',
+        details='Number of samples in each of the misclassified cells in the confusion matrix is '
+                f'lesser than the threshold ({0.1 * len(test_ds)}) based on the ' \
+                'given misclassified_samples_threshold ratio'
+    ))
+
+    assert_that(result.conditions_results[1], equal_condition_result(
+        is_pass=False,
+        name=f'Misclassified cell size lower than {format_number(0.01 * 100)}% of the total samples',
+        details='Found a cell with 23 misclassified samples which is greater than the threshold '
+                f'({0.01 * len(test_ds)}) based on the given misclassified_samples_threshold ratio'
+    ))
