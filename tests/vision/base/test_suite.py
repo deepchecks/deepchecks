f9720af1b7541918a8d8be4dc0f715d59206bab1
Matan Perlmutter <matan@deepchecks.com>
2023-04-27 18:19:19 +0300

    fix indent

--- tests/vision/base/test_suite.py
+++ tests/vision/base/test_suite.py
@@ -251,7 +251,8 @@ def test_full_suite_execution_coco_torch(coco_visiondata_train, coco_visiondata_
         length = get_expected_results_length(suite, args)
         validate_suite_result(result, length)
 
- def test_full_suite_execution_coco_tf(tf_coco_visiondata_train, tf_coco_visiondata_test):
+
+def test_full_suite_execution_coco_tf(tf_coco_visiondata_train, tf_coco_visiondata_test):
      suite = full_suite(imaginery_kwarg='just to make sure all checks have kwargs in the init')
      arguments = (
          dict(train_dataset=tf_coco_visiondata_train, test_dataset=tf_coco_visiondata_test),
