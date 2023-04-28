3f8c2ecf0f7e58f85968187a70159043092b1d88
kishore-s-15 <skishore2602.dev@gmail.com>
2023-04-28 10:36:19 +0530

    Added tests

--- tests/tabular/checks/model_evaluation/confusion_matrix_report_test.py
+++ tests/tabular/checks/model_evaluation/confusion_matrix_report_test.py
@@ -12,9 +12,11 @@
 import numpy as np
 from hamcrest import assert_that, calling, greater_than, has_length, raises
 
+from deepchecks.core.condition import ConditionCategory
 from deepchecks.core.errors import DeepchecksNotSupportedError, DeepchecksValueError, ModelValidationError
 from deepchecks.tabular.checks.model_evaluation import ConfusionMatrixReport
-
+from deepchecks.utils.strings import format_number
+from tests.base.utils import equal_condition_result
 
 def test_dataset_wrong_input():
     bad_dataset = 'wrong_input'
@@ -79,3 +81,60 @@ def test_model_info_object_not_normalize(iris_labeled_dataset, iris_adaboost):
     for i in range(len(result)):
         for j in range(len(result[i])):
             assert isinstance(result[i][j], np.int64)
+
+
+def test_condition_misclassified_samples_lower_than_raises_error(iris_split_dataset_and_model):
+    # Arrange
+    _, test, clf = iris_split_dataset_and_model
+
+    # Act
+    check = ConfusionMatrixReport() \
+            .add_condition_misclassified_samples_lower_than_condition(misclassified_samples_threshold=-0.1) \
+            .add_condition_misclassified_samples_lower_than_condition(misclassified_samples_threshold=1.1)
+
+    result = check.run(test, clf)
+
+    # Assert
+    assert_that(result.conditions_results[0], equal_condition_result(
+        is_pass=False,
+        name=f'Misclassified cell size lower than {format_number(-0.1 * 100)}% of the total samples',
+        details='Exception in condition: ValidationError: Condition requires the parameter "misclassified_samples_threshold" '
+                'to be between 0 and 1 inclusive but got -0.1',
+        category=ConditionCategory.ERROR
+    ))
+
+    assert_that(result.conditions_results[1], equal_condition_result(
+        is_pass=False,
+        name=f'Misclassified cell size lower than {format_number(1.1 * 100)}% of the total samples',
+        details='Exception in condition: ValidationError: Condition requires the parameter "misclassified_samples_threshold" '
+                'to be between 0 and 1 inclusive but got 1.1',
+        category=ConditionCategory.ERROR
+    ))
+
+
+def test_condition_misclassified_samples_lower_than(iris_split_dataset_and_model):
+    # Arrange
+    _, test, clf = iris_split_dataset_and_model
+
+    # Act
+    check = ConfusionMatrixReport() \
+            .add_condition_misclassified_samples_lower_than_condition(misclassified_samples_threshold=0.1) \
+            .add_condition_misclassified_samples_lower_than_condition(misclassified_samples_threshold=0.05)
+
+    result = check.run(test, clf)
+
+    # Assert
+    assert_that(result.conditions_results[0], equal_condition_result(
+        is_pass=True,
+        name=f'Misclassified cell size lower than {format_number(0.1 * 100)}% of the total samples',
+        details='Number of samples in each of the misclassified cells in the confusion matrix is '
+                f'lesser than the threshold ({0.1 * len(test)}) based on the ' \
+                'given misclassified_samples_threshold ratio'
+    ))
+
+    assert_that(result.conditions_results[1], equal_condition_result(
+        is_pass=False,
+        name=f'Misclassified cell size lower than {format_number(0.05 * 100)}% of the total samples',
+        details='Found a cell with 4 misclassified samples which is greater than the threshold '
+                f'({0.05 * len(test)}) based on the given misclassified_samples_threshold ratio'
+    ))
