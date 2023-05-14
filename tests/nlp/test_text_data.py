97512efa8cb6a00586c6a1aeba8d97f8005d59d8
nirhutnik <nirhutnik@gmail.com>
2023-05-14 16:09:39 +0300

    Fixed a small bug

--- tests/nlp/test_text_data.py
+++ tests/nlp/test_text_data.py
@@ -186,7 +186,7 @@ def test_properties(text_classification_dataset_mock):
     dataset.calculate_default_properties(ignore_properties=['topic'] + LONG_RUN_PROPERTIES)
     properties = dataset.properties
     assert_that(properties.shape[0], equal_to(3))
-    assert_that(properties.shape[1], equal_to(9))
+    assert_that(properties.shape[1], equal_to(10))
     assert_that(properties.columns,
                 contains_exactly('Text Length', 'Average Word Length', 'Max Word Length', '% Special Characters',
                                  'Sentiment', 'Subjectivity', 'Lexical Density', 'Readability Score',
