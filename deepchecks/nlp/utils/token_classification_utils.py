0c5f821394e0c7edc636ca4f65166b7534edc91a
nirhutnik <nirhutnik@gmail.com>
2023-05-23 23:52:23 +0300

    fixed pylint

--- deepchecks/nlp/utils/token_classification_utils.py
+++ deepchecks/nlp/utils/token_classification_utils.py
@@ -23,7 +23,6 @@ __all__ = [
 
 def clean_iob_prefixes(labels) -> np.array:
     """Remove the initial character of IOB labels (B- and I- and such) if they exist."""
-
     return np.array([label[2:] if label[:2] in ['B-', 'I-', 'O-'] else label for label in labels])
 
 
