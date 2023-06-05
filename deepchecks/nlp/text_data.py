ac7b35dd88e5b56b0416eaf7502f09732c58c1be
Noam Bressler <noamzbr@gmail.com>
2023-06-05 21:11:17 +0300

    Embed long documents with openai (#2573)
    * enable device for properties and chunk text when passing to openai embeddings
    
    * add tiktoken and move transformers to nlp reqs
    
    * Define handling options for long documents

--- deepchecks/nlp/text_data.py
+++ deepchecks/nlp/text_data.py
@@ -302,7 +302,9 @@ class TextData:
             )
         return self._embeddings
 
-    def calculate_builtin_embeddings(self, model: str = 'miniLM', file_path: str = 'embeddings.npy'):
+    def calculate_builtin_embeddings(self, model: str = 'miniLM', file_path: str = 'embeddings.npy',
+                                     device: t.Optional[str] = None, long_sample_behaviour: str = 'average+warn',
+                                     open_ai_batch_size: int = 500):
         """Calculate the built-in embeddings of the dataset.
 
         Parameters
@@ -313,11 +315,28 @@ class TextData:
             'open_ai': using the ADA model in the open_ai library. Requires an API key.
         file_path : str, default: 'embeddings.npy'
             The path to save the embeddings to.
+        device : str, default: None
+            The device to use for calculating the embeddings. If None, the default device will be used.
+        long_sample_behaviour : str, default 'average+warn'
+            How to handle long samples. Averaging is done as described in
+            https://github.com/openai/openai-cookbook/blob/main/examples/Embedding_long_inputs.ipynb
+            Currently, applies only to the 'open_ai' model, as the 'miniLM' model can handle long samples.
+
+            Options are:
+                - 'average+warn' (default): average the embeddings of the chunks and warn if the sample is too long.
+                - 'average': average the embeddings of the chunks.
+                - 'truncate': truncate the sample to the maximum length.
+                - 'raise': raise an error if the sample is too long.
+                - 'nan': return an embedding vector of nans for each sample that is too long.
+        open_ai_batch_size : int, default 500
+            The amount of samples to send to open ai in each batch. Reduce if getting errors from open ai.
         """
         if self._embeddings is not None:
             warnings.warn('Embeddings already exist, overwriting them', UserWarning)
 
-        self._embeddings = calculate_builtin_embeddings(text=self.text, model=model, file_path=file_path)
+        self._embeddings = calculate_builtin_embeddings(text=self.text, model=model, file_path=file_path, device=device,
+                                                        long_sample_behaviour=long_sample_behaviour,
+                                                        open_ai_batch_size=open_ai_batch_size)
 
     def set_embeddings(self, embeddings: np.ndarray, verbose: bool = True):
         """Set the embeddings of the dataset.
