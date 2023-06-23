c4c90e62290a9c5d855279683d793e17fcd72276
AIT ALI YAHIA Rayane <aitaliyahia.rayane.pro@gmail.com>
2023-06-23 15:16:40 +0200

    Update __init__.py

--- deepchecks/recommender/checks/__init__.py
+++ deepchecks/recommender/checks/__init__.py
@@ -8,18 +8,20 @@
 # along with Deepchecks.  If not, see <http://www.gnu.org/licenses/>.
 # ----------------------------------------------------------------------------
 #
-
+from .date_train_test_leakage_overlap import DateTrainTestLeakageOverlap
+from .cold_start_detection import ColdStartDetection
 from .label_popularity_drift import LabelPopularityDrift
 from .operations_amount_segment_performance import OperationsAmountSegmentPerformance
 from .popularity_bias import PopularityBias
 from .prediction_popularity_drift import PredictionPopularityDrift
 from .sample_performance import SamplePerformance
-from .scatter_performance import ScatterPerformance
-
+from .user_session_length_drift import UserSessionDrift
 __all__ = ['LabelPopularityDrift',
            'OperationsAmountSegmentPerformance',
            'PopularityBias',
            'PredictionPopularityDrift',
-           'ScatterPerformance',
            'SamplePerformance',
+           'DateTrainTestLeakageOverlap',
+           'ColdStartDetection',
+           'UserSessionDrift'
            ]
