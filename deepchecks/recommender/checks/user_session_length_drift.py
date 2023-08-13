f9e1f6c21cdfdb3cc113bcd1744f3c7b3629b1c9
AIT ALI YAHIA Rayane <aitaliyahia.rayane.pro@gmail.com>
2023-08-13 03:20:56 +0200

    fix variable name : change session_length by nb_interactions

--- deepchecks/recommender/checks/user_session_length_drift.py
+++ deepchecks/recommender/checks/user_session_length_drift.py
@@ -14,20 +14,21 @@ import typing as t
 import pandas as pd
 
 from deepchecks.core import CheckResult
-from deepchecks.core.condition import ConditionCategory, ConditionResult
 from deepchecks.core.errors import DeepchecksValueError
 from deepchecks.recommender import Context
 from deepchecks.tabular import TrainTestCheck
 from deepchecks.utils.distribution.drift import calc_drift_and_plot
-from deepchecks.utils.validation import is_sequence_not_str
 
 __all__ = ['UserSessionDrift']
 
 class UserSessionDrift(TrainTestCheck):
-    """
-    
-    Check for user session length drift in train and test sets. This check compares the distribution of user session lengths between the train and test sets to identify potential drift or discrepancies. It helps assess whether the two sets exhibit similar patterns in terms of user session lengths or if there are significant differences that could introduce drift in the recommender system.
-    
+    """ Check for user session length drift in train and test sets.
+
+    This check compares the distribution of user session lengths between the train and test sets.
+    It helps to identify potential drift or discrepancies, but also assess whether the two sets
+    exhibit similar patterns in terms of user session lengths or if there are significant
+    differences that could introduce drift in the recommender system.
+
     """
     def __init__(
             self,
@@ -79,13 +80,13 @@ class UserSessionDrift(TrainTestCheck):
         user_col = train_dataset.user_index_name
         assert user_col == test_dataset.user_index_name
 
-        train_user_session_length = train_dataset.data[user_col].value_counts().tolist()
-        test_user_session_length = test_dataset.data[user_col].value_counts().tolist()
+        train_users_nb_interactions = train_dataset.data[user_col].value_counts().tolist()
+        test_users_nb_interactions = test_dataset.data[user_col].value_counts().tolist()
 
 
-        drift_score, method, drift_display = calc_drift_and_plot(
-            train_column=pd.Series(train_user_session_length),
-            test_column=pd.Series(test_user_session_length),
+        drift_score, _, drift_display = calc_drift_and_plot(
+            train_column=pd.Series(train_users_nb_interactions),
+            test_column=pd.Series(test_users_nb_interactions),
             value_name='Session Length',
             column_type='numerical',
             plot_title='Session Length Drift',
@@ -104,36 +105,4 @@ class UserSessionDrift(TrainTestCheck):
             with_display=context.with_display,
         )
 
-        return CheckResult(
-            value=drift_score,
-            header='Session Length drift',
-            display=drift_display,
-        )
-
-    def add_condition_drift_score_less_than(self, max_allowed_score: float = 0.15):
-        """
-        Add condition - require drift score to be less than a certain threshold.
-
-        The industry standard for PSI limit is above 0.2.
-        There are no common industry standards for other drift methods, such as Cramer's V,
-        Kolmogorov-Smirnov and Earth Mover's Distance.
-        The threshold was lowered by 25% compared to feature drift defaults due to the higher importance of prediction
-        drift.
-
-        Parameters
-        ----------
-        max_allowed_categorical_score: float , default: 0.15
-            the max threshold for the categorical variable drift score
-        max_allowed_numeric_score: float ,  default: 0.15
-            the max threshold for the numeric variable drift score
-        Returns
-        -------
-        ConditionResult
-            False if any column has passed the max threshold, True otherwise
-        """
-
-        def condition(result: float) -> ConditionResult:
-            category = ConditionCategory.FAIL if result > max_allowed_score else ConditionCategory.PASS
-            return ConditionResult(category, f'The drift score was {result}')
-
-        return self.add_condition(f'drift score less than {max_allowed_score}', condition)
+        return CheckResult(value=drift_score,header='Session Length drift',display=drift_display)
