d106baecba420e3f12e42dfaf75c11c61f4fc2a7
AIT ALI YAHIA Rayane <aitaliyahia.rayane.pro@gmail.com>
2023-08-13 03:25:23 +0200

    pylint | logic
    - pass pylint > 8.5.
    - Inherit from SingleDatasetCheck instead of TrainTestCheck: the idea is that we don't have label for training, we use all training point to fit a model in an unsupervised manner. Therefore, we will compare the drift between the popularity of training instances, and validation labels, instead of training labels and validation labels.

--- deepchecks/recommender/checks/label_popularity_drift.py
+++ deepchecks/recommender/checks/label_popularity_drift.py
@@ -8,27 +8,23 @@
 # along with Deepchecks.  If not, see <http://www.gnu.org/licenses/>.
 # ----------------------------------------------------------------------------
 #
-"""Module containing the popularity drift check."""
+"""Module containing the label popularity drift check."""
 import typing as t
-
 import pandas as pd
 
 from deepchecks.core import CheckResult
-from deepchecks.core.condition import ConditionCategory, ConditionResult
 from deepchecks.core.errors import DeepchecksValueError
 from deepchecks.recommender import Context
-from deepchecks.tabular import TrainTestCheck
+from deepchecks.tabular import SingleDatasetCheck
 from deepchecks.utils.distribution.drift import calc_drift_and_plot
-from deepchecks.utils.validation import is_sequence_not_str
 
 __all__ = ['LabelPopularityDrift']
 
-class LabelPopularityDrift(TrainTestCheck):
-    """
-    Calculate label popularity drift between train dataset and test dataset, using statistical measures.
+class LabelPopularityDrift(SingleDatasetCheck):
+    """ Compute popularity drift between train points and test labels, using statistical measures.
 
-    Check calculates a drift score for the label in test dataset, by comparing its distribution to the train
-    dataset.
+    Check calculates a drift score for the popularity of the trained instances, by comparing its
+    distribution to the true labels.
 
     For numerical columns, we use the Kolmogorov-Smirnov statistic.
     See https://en.wikipedia.org/wiki/Kolmogorov%E2%80%93Smirnov_test
@@ -40,54 +36,70 @@ class LabelPopularityDrift(TrainTestCheck):
     We also support Population Stability Index (PSI).
     See https://www.lexjansen.com/wuss/2017/47_Final_Paper_PDF.pdf.
 
-    For categorical labels, it is recommended to use Cramer's V, unless your variable includes categories with a
-    small number of samples (common practice is categories with less than 5 samples).
-    However, in cases of a variable with many categories with few samples, it is still recommended to use Cramer's V.
+    For categorical predictions, it is recommended to use Cramer's V, unless your variable i
+    ncludes categories with a small number of samples (common practice is categories with less than 5 samples).
+    However, in cases of a variable with many categories with few samples,
+    it is still recommended to use Cramer's V.
 
-    **Note:** In case of highly imbalanced classes, it is recommended to use Cramer's V, together with setting
-    the ``balance_classes`` parameter to ``True``.
+    **Note:** In case of highly imbalanced classes, it is recommended to use Cramer's V,
+    together with setting the ``balance_classes`` parameter to ``True``.
+    This also requires setting the ``drift_mode`` parameter to
+    ``auto`` (default) or ``'prediction'``.
+
 
     Parameters
     ----------
     margin_quantile_filter: float, default: 0.025
-        float in range [0,0.5), representing which margins (high and low quantiles) of the distribution will be filtered
-        out of the EMD calculation. This is done in order for extreme values not to affect the calculation
+        float in range [0,0.5), representing which margins (high and low quantiles)
+        of the distribution will be filtered out of the EMD calculation.
+        This is done in order for extreme values not to affect the calculation
         disproportionally. This filter is applied to both distributions, in both margins.
     min_category_size_ratio: float, default 0.01
-        minimum size ratio for categories. Categories with size ratio lower than this number are binned
-        into an "Other" category. Ignored if balance_classes=True.
+        minimum size ratio for categories. Categories with size ratio lower than this number
+        are binned into an "Other" category. Ignored if balance_classes=True.
     max_num_categories_for_drift: int, default: None
-        Only for classification. Max number of allowed categories. If there are more,
-        they are binned into an "Other" category.
+        Only relevant if drift is calculated for classification predictions.
+        Max number of allowed categories. If there are more, they are binned into
+        an "Other" category.
     max_num_categories_for_display: int, default: 10
         Max number of categories to show in plot.
     show_categories_by: str, default: 'largest_difference'
-        Specify which categories to show for categorical features' graphs, as the number of shown categories is limited
-        by max_num_categories_for_display. Possible values:
-        - 'train_largest': Show the largest train categories.
-        - 'test_largest': Show the largest test categories.
+        Specify which categories to show for categorical features' graphs,
+        as the number of shown categories is limited by max_num_categories_for_display.
+        Possible values:
+        - 'train_largest': Show the largest prediction categories.
+        - 'test_largest': Show the largest labels categories.
         - 'largest_difference': Show the largest difference between categories.
     numerical_drift_method: str, default: "KS"
         decides which method to use on numerical variables. Possible values are:
         "EMD" for Earth Mover's Distance (EMD), "KS" for Kolmogorov-Smirnov (KS).
-    categorical_drift_method: str, default: "cramers_v"
-        decides which method to use on categorical variables. Possible values are:
-        "cramers_v" for Cramer's V, "PSI" for Population Stability Index (PSI).
     balance_classes: bool, default: False
-        If True, all categories will have an equal weight in the Cramer's V score. This is useful when the categorical
-        variable is highly imbalanced, and we want to be alerted on changes in proportion to the category size,
-        and not only to the entire dataset. Must have categorical_drift_method = "cramers_v".
+        If True, all categories will have an equal weight in the Cramer's V score.
+        This is useful when the categorical variable is highly imbalanced, and we want
+        to be alerted on changes in proportion to the category size, and not only to the
+        entire dataset. Must have categorical_drift_method = "cramers_v" and
+        drift_mode = "auto" or "prediction".
         If True, the variable frequency plot will be created with a log scale in the y-axis.
-    ignore_na: bool, default False
-        For categorical columns only. If True, ignores nones for categorical drift. If False, considers none as a
-        separate category. For numerical columns we always ignore nones.
+    ignore_na: bool, default True
+        For categorical columns only. If True, ignores nones for categorical drift.
+        If False, considers none as a separate category.
+        For numerical columns we always ignore nones.
+    aggregation_method: t.Optional[str], default: "max"
+        Argument for the reduce_output functionality, decides how to aggregate the drift scores
+        of different classes (for classification tasks) into a single score,
+        when drift is computed on the class probabilities. Possible values are:
+        'max': Maximum of all the class drift scores.
+        'weighted': Weighted mean based on the class sizes in the prediction data set.
+        'mean': Mean of all drift scores.
+        None: No averaging. Return a dict with a drift score for each class.
     min_samples : int , default: 10
-        Minimum number of samples required to calculate the drift score. If there are not enough samples for either
-        train or test, the check will raise a ``NotEnoughSamplesError`` exception.
+        Minimum number of samples required to calculate the drift score. If there are not enough
+        samples for either predictions or labels,
+        the check will raise a ``NotEnoughSamplesError`` exception.
     n_samples : int , default: 100_000
-        Number of samples to use for drift computation and plot.
-    random_state : int , default: 42
-        Random seed for sampling.
+        number of samples to use for this check.
+    random_state : int, default: 42
+        random seed for all check internals.
     """
     def __init__(
             self,
@@ -123,33 +135,35 @@ class LabelPopularityDrift(TrainTestCheck):
         if self.aggregation_method not in ('weighted', 'mean', 'none', None, 'max'):
             raise DeepchecksValueError('aggregation_method must be one of "weighted", "mean", "max", None')
 
-    def run_logic(self, context: Context) -> CheckResult:
+    def run_logic(self, context: Context, dataset_kind) -> CheckResult:
         """Calculate drift for all columns.
 
         Returns
         -------
         CheckResult
             value: drift score.
-            display: label distribution graph, comparing the train and test distributions.
+            display: distribution graph, comparing the predictions and true labels distributions.
         """
-        train_labels = context._y_pred_train
-        test_labels = context._y_pred_test
 
+        test_labels = context.get_data_by_kind(dataset_kind).label_col
+        sample_size = min(self.n_samples, len(test_labels))
 
-        # Compute item popularity for the training dataset
+        test_labels = test_labels.sample(sample_size, random_state=self.random_state)
+
         interaction_dataset  = context._interaction_dataset
         item_id = interaction_dataset.item_index_name
         item_popularity = interaction_dataset.data[item_id].value_counts().to_dict()
+        train_items = interaction_dataset.data[item_id].unique()
 
-        train_labels_popularity = [item_popularity[item] for sublist in train_labels for item in sublist]
-        test_labels_popularity =  [item_popularity[item] for sublist in test_labels for item in sublist]
+        train_popularity = [item_popularity[item] for item in train_items]
+        label_popularity = [item_popularity[item] for sublist in test_labels for item in sublist if item in item_popularity]
 
-        drift_score, method, drift_display = calc_drift_and_plot(
-            train_column=pd.Series(train_labels_popularity),
-            test_column=pd.Series(test_labels_popularity),
-            value_name='Label Popularity',
+        drift_score, _, drift_display = calc_drift_and_plot(
+            train_column=pd.Series(train_popularity),
+            test_column=pd.Series(label_popularity),
+            value_name='Prediction Popularity',
             column_type='numerical',
-            plot_title='Label Popularity Drift',
+            plot_title='Prediction Popularity Drift',
             margin_quantile_filter=self.margin_quantile_filter,
             max_num_categories_for_drift=self.max_num_categories_for_drift,
             min_category_size_ratio=self.min_category_size_ratio,
@@ -161,8 +175,8 @@ class LabelPopularityDrift(TrainTestCheck):
             ignore_na=self.ignore_na,
             min_samples=self.min_samples,
             raise_min_samples_error=True,
-            dataset_names=('Train', 'Test'),
-            with_display=context.with_display,
+            dataset_names=('Prediction', 'True Labels'),
+            with_display=context.with_display
         )
 
         return CheckResult(
@@ -170,31 +184,3 @@ class LabelPopularityDrift(TrainTestCheck):
             header='Label Popularity Drift',
             display=drift_display,
         )
-
-    def add_condition_drift_score_less_than(self, max_allowed_score: float = 0.15):
-        """
-        Add condition - require drift score to be less than a certain threshold.
-
-        The industry standard for PSI limit is above 0.2.
-        There are no common industry standards for other drift methods, such as Cramer's V,
-        Kolmogorov-Smirnov and Earth Mover's Distance.
-        The threshold was lowered by 25% compared to feature drift defaults due to the higher importance of prediction
-        drift.
-
-        Parameters
-        ----------
-        max_allowed_categorical_score: float , default: 0.15
-            the max threshold for the categorical variable drift score
-        max_allowed_numeric_score: float ,  default: 0.15
-            the max threshold for the numeric variable drift score
-        Returns
-        -------
-        ConditionResult
-            False if any column has passed the max threshold, True otherwise
-        """
-
-        def condition(result: float) -> ConditionResult:
-            category = ConditionCategory.FAIL if result > max_allowed_score else ConditionCategory.PASS
-            return ConditionResult(category, f'The drift score was {result}')
-
-        return self.add_condition(f'drift score less than {max_allowed_score}', condition)
