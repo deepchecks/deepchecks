fdef4ca433024bdb390aaf04120cef5de867ea3b
shiritdvorkin <shiritdvir@gmail.com>
2023-08-20 16:11:43 +0300

    Adding a doc to the new check - frequent substring

--- docs/source/checks/nlp/data_integrity/plot_frequent_substrings.py
+++ docs/source/checks/nlp/data_integrity/plot_frequent_substrings.py
@@ -9,7 +9,7 @@ This notebook provides an overview for using and understanding the frequent subs
 
 **Structure:**
 
-* `Why check for frequent substrings? <#why-check-for-text-data-duplicates>`__
+* `Why check for frequent substrings? <#why-check-for-frequent-substrings>`__
 * `Create TextData <#create-textdata>`__
 * `Run the Check <#run-the-check>`__
 * `Define a Condition <#define-a-condition>`__
@@ -17,8 +17,15 @@ This notebook provides an overview for using and understanding the frequent subs
 Why check for frequent substrings?
 ===================================
 
-The ``FrequentSubstrings`` check finds frequent substrings of varying lengths (n-grams) in the Dataset.
-Frequent substrings may be an indicator for a problem in the data pipeline that requires attention.
+The purpose of the "FrequentSubstrings" check is to identify recurring substrings within the Dataset.
+These commonly occurring substrings can signal potential issues within the data pipeline that demand consideration.
+Furthermore, these substrings might impact the model's performance and,
+in certain scenarios, it might be necessary to eliminate them from the dataset.
+
+Substrings of varying lengths (n-grams) are extracted from the dataset text samples.
+The frequencies of these n-grams are calculated and only substrings exceeding a defined minimum length are retained.
+The substrings are then sorted by their frequencies and the most frequent substrings are identified.
+Finally, the substrings with the highest frequency and those surpassing a significance level are displayed.
 
 Create TextData
 ===============
@@ -50,7 +57,7 @@ FrequentSubstrings().run(dataset)
 # Define a Condition
 # ==================
 #
-# Now, we define a condition that enforces the ratio of frequent substrings to be smaller than 0.05
+# Now, we define a condition that enforces that ratio of frequent substrings will be smaller than 0.05
 # for all frequent substrings in the data. A condition is deepchecks' way to validate model and data quality,
 # and let you know if anything goes wrong.
 
