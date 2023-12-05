aa61f9fec2b451e149cbc9adc9aa7bb897e0b45a
JKL98ISR <jonatan.lib@gmail.com>
2023-12-05 16:37:36 +0200

    fix_test

--- deepchecks/utils/dataframes.py
+++ deepchecks/utils/dataframes.py
@@ -43,7 +43,7 @@ def default_fill_na_per_column_type(df: pd.DataFrame, cat_features: t.Optional[t
 
 def default_fill_na_series(col: pd.Series, is_cat_column: t.Optional[bool] = None) -> t.Optional[pd.Series]:
     """Fill NaN values based on column type if possible otherwise returns None."""
-    if is_cat_column and 'None' not in col.astype('object').unique():
+    if is_cat_column and 'None' not in col.astype('object').dropna().unique():
         return col.astype('object').fillna('None')
     elif is_numeric_dtype(col):
         return col.astype('float64').fillna(np.nan)
